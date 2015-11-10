eac73f4c5d658db48bfc2732cb72dc74e1aba413
Seth Rosetter <seth.rosetter@gmail.com>
2015-11-09 20:33:12 -0800

    Support smarter logging

--- provision/fetch_sg_logs.py
+++ provision/fetch_sg_logs.py
@@ -5,22 +5,22 @@ import time
 from ansible_runner import run_ansible_playbook
 
 
-def fetch_sync_gateway_logs():
+def fetch_sync_gateway_logs(prefix):
 
+    print("\n\n>>> Pulling logs")
     # fetch logs from sync_gateway instances
     run_ansible_playbook("fetch-sync-gateway-logs.yml")
 
-    # zip logs, rename, and copy to desktop
+    # zip logs and timestamp
     if os.path.isdir("/tmp/sg_logs"):
+
         date_time = time.strftime("%Y-%m-%d-%H-%M-%S")
-        name = "/tmp/{}-sglogs".format(date_time)
+        name = "/tmp/{}-{}-sglogs".format(prefix, date_time)
 
         shutil.make_archive(name, "zip", "/tmp/sg_logs")
-        home = os.environ["HOME"]
 
-        shutil.copy("{}.zip".format(name), "{}/Desktop".format(home))
         shutil.rmtree("/tmp/sg_logs")
-        print(">>> Copied {}.zip to {}/Desktop".format(name, home))
+        print(">>> sync_gateway logs copied here {}\n".format(name))
 
 
 if __name__ == "__main__":
