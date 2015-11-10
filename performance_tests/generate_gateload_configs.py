727657d70a9ecf46f07f84bee1a3cd8951c86f0c
Traun Leyden <tleyden@couchbase.com>
2015-11-09 19:11:07 -0800

    Rework generate_gateload_configs for static inventory

---
+++ performance_tests/generate_gateload_configs.py
@@ -0,0 +1,122 @@
+
+"""
+
+This attempts to automate the the Gateload config creation
+
+* Find a list of ip's of all the sync gateway machines 
+* Find the ip of the writer and remove from the list 
+* Find a list of ip's of all the gateload machines
+* Assert that list_sync_gateways and list_gateloads are of the same length
+* Assign each gateload a sync gateway
+* Assign each gateload a user offset (iterate over gateloads and bump by 13K)
+* For each gateload
+  * Generate gateload config from a template
+    * Use assigned Sync Gateway ip
+    * Use assigned user offset
+  * Upload gateload config to gateload machine
+
+
+"""
+
+import subprocess
+import json
+import os
+from jinja2 import Template
+
+def sync_gateways():
+    """
+    Get the list of sync gateway private ip addresses
+    """
+    tag = "sync_gateways"
+    public_ips = []
+    instance_list = public_ip_addresses_for_tag(tag)
+    print "sync gw instance_list: {}".format(instance_list)
+    for public_ip in instance_list:
+        if len(public_ip) == 0:
+            continue
+        print "sync gw public ip: {}".format(public_ip)
+        public_ips.append(public_ip)
+
+    return public_ips
+
+
+def public_ip_addresses_for_tag(tag):
+    cmd = "ansible {0} --list-hosts -i ../temp_ansible_hosts".format(tag)
+    result = subprocess.check_output(cmd, shell=True)
+    print result
+    instance_list = result.split("\n")
+    instance_list = [x.strip() for x in instance_list]
+    instance_list = [x for x in instance_list if x]  # filter empty items 
+    return instance_list
+
+def gateloads():
+    tag = "load_generators"
+    gateloads = []
+    instance_list = public_ip_addresses_for_tag(tag)
+
+    for public_ip in instance_list:
+        if len(public_ip) == 0:
+            continue
+        print "gateload public ip: {}".format(public_ip)
+        gateloads.append(public_ip)
+
+    return gateloads
+
+def render_gateload_template(sync_gateway_private_ip, user_offset, number_of_pullers, number_of_pushers):
+        # run template to produce file
+        gateload_config = open("files/gateload_config.json")
+        template = Template(gateload_config.read())
+        rendered = template.render(
+            sync_gateway_private_ip=sync_gateway_private_ip,
+            user_offset=user_offset,
+            number_of_pullers=number_of_pullers,
+            number_of_pushers=number_of_pushers
+        )
+        return rendered 
+
+def upload_gateload_config(gateload_ec2_id, sync_gateway_private_ip, user_offset, number_of_pullers, number_of_pushers):
+    
+    rendered = render_gateload_template(
+        sync_gateway_private_ip,
+        user_offset,
+        number_of_pullers,
+        number_of_pushers
+    )
+    print rendered
+
+    outfile = os.path.join("/tmp", gateload_ec2_id) 
+    with open(outfile, 'w') as f:
+        f.write(rendered)
+    print "Wrote to file: {}".format(outfile)
+
+    # transfer file to remote host
+    cmd = 'ansible {} -i ../temp_ansible_hosts -m copy -a "src={} dest=/home/centos/gateload_config.json" --user centos'.format(gateload_ec2_id, outfile)
+    result = subprocess.check_output(cmd, shell=True)
+    print "File transfer result: {}".format(result)
+
+
+def main(number_of_pullers, number_of_pushers):
+
+    sync_gateway_ips = sync_gateways()
+
+    gateload_ips = gateloads()
+
+    for idx, gateload_ip in enumerate(gateload_ips):
+        # calculate the user offset
+        user_offset = idx * 13000 
+        # assign a sync gateway to this gateload, get its ip
+        sync_gateway_private_ip = sync_gateway_ips[idx]
+
+        upload_gateload_config(
+            gateload_ip,
+            sync_gateway_private_ip,
+            user_offset,
+            number_of_pullers,
+            number_of_pushers
+        )
+
+    print "Finished successfully"
+
+if __name__ == "__main__":
+    main(100, 100)
+
