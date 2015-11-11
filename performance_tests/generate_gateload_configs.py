f21c6df8c5e48090bcfcf076fe5f70e60d9b9856
Traun Leyden <tleyden@couchbase.com>
2015-11-10 16:29:37 -0800

    Rework gateload config generation based on ansible api

--- performance_tests/generate_gateload_configs.py
+++ performance_tests/generate_gateload_configs.py
@@ -24,93 +24,75 @@ import os
 from jinja2 import Template
 import ansible.inventory
 
-def sync_gateways():
-    """
-    Get the list of sync gateway private ip addresses
-    """
-    tag = "sync_gateways"
-    public_ips = []
-    instance_list = public_ip_addresses_for_tag(tag)
-    print "sync gw instance_list: {}".format(instance_list)
-    for public_ip in instance_list:
-        if len(public_ip) == 0:
-            continue
-        print "sync gw public ip: {}".format(public_ip)
-        public_ips.append(public_ip)
 
-    return public_ips
-
-
-def public_ip_addresses_for_tag(tag):
+def hosts_for_tag(tag):
     hostfile = "../../../temp_ansible_hosts"
     i = ansible.inventory.Inventory(host_list=hostfile)
     hosts = i.get_group(tag).get_hosts()
-    return [host.get_variables()['ansible_ssh_host'] for host in hosts]
+    return [host.get_variables() for host in hosts]
 
 def gateloads():
     tag = "load_generators"
-    gateloads = []
-    instance_list = public_ip_addresses_for_tag(tag)
+    return hosts_for_tag(tag)
 
-    for public_ip in instance_list:
-        if len(public_ip) == 0:
-            continue
-        print "gateload public ip: {}".format(public_ip)
-        gateloads.append(public_ip)
+def sync_gateways():
+    tag = "sync_gateways"
+    return hosts_for_tag(tag)
 
-    return gateloads
-
-def render_gateload_template(sync_gateway_private_ip, user_offset, number_of_pullers, number_of_pushers):
+def render_gateload_template(sync_gateway, user_offset, number_of_pullers, number_of_pushers):
         # run template to produce file
         gateload_config = open("files/gateload_config.json")
         template = Template(gateload_config.read())
         rendered = template.render(
-            sync_gateway_private_ip=sync_gateway_private_ip,
+            sync_gateway_private_ip=sync_gateway['ansible_ssh_host'],
             user_offset=user_offset,
             number_of_pullers=number_of_pullers,
             number_of_pushers=number_of_pushers
         )
         return rendered 
 
-def upload_gateload_config(gateload_ec2_id, sync_gateway_private_ip, user_offset, number_of_pullers, number_of_pushers):
+def upload_gateload_config(gateload, sync_gateway, user_offset, number_of_pullers, number_of_pushers):
+
+    gateload_inventory_hostname = gateload['inventory_hostname']    
     
     rendered = render_gateload_template(
-        sync_gateway_private_ip,
+        sync_gateway,
         user_offset,
         number_of_pullers,
         number_of_pushers
     )
     print rendered
 
-    outfile = os.path.join("/tmp", gateload_ec2_id) 
+    outfile = os.path.join("/tmp", gateload_inventory_hostname) 
     with open(outfile, 'w') as f:
         f.write(rendered)
     print "Wrote to file: {}".format(outfile)
 
     # transfer file to remote host
-    cmd = 'ansible {} -i ../../../temp_ansible_hosts -m copy -a "src={} dest=/home/centos/gateload_config.json" --user centos'.format(gateload_ec2_id, outfile)
+
+    cmd = 'ansible {} -i ../../../temp_ansible_hosts -m copy -a "src={} dest=/home/centos/gateload_config.json" --user centos'.format(gateload_inventory_hostname, outfile)
     result = subprocess.check_output(cmd, shell=True)
     print "File transfer result: {}".format(result)
 
 
 def main(number_of_pullers, number_of_pushers):
 
-    sync_gateway_ips = sync_gateways()
+    sync_gateway_hosts = sync_gateways()
 
-    gateload_ips = gateloads()
+    gateload_hosts = gateloads()
 
-    for idx, gateload_ip in enumerate(gateload_ips):
+    for idx, gateload in enumerate(gateload_hosts):
 
         # calculate the user offset
         total_num_users = number_of_pullers + number_of_pushers
         user_offset = idx * total_num_users
 
         # assign a sync gateway to this gateload, get its ip
-        sync_gateway_private_ip = sync_gateway_ips[idx]
+        sync_gateway = sync_gateway_hosts[idx]
 
         upload_gateload_config(
-            gateload_ip,
-            sync_gateway_private_ip,
+            gateload,
+            sync_gateway,
             user_offset,
             number_of_pullers,
             number_of_pushers
