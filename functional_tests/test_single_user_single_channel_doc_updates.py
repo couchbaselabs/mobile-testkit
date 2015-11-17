f9071db20749dfcdfd0e958b1f9d41aa0ab7d2bb
Ashvinder Singh <ashvinder@Ashvinders-MacBook-Pro.local>
2015-11-17 08:59:06 -0800

    add parameterization support

--- functional_tests/test_single_user_single_channel_doc_updates.py
+++ functional_tests/test_single_user_single_channel_doc_updates.py
@@ -1,21 +1,25 @@
 import time
-
 from lib.user import User
 from lib.admin import Admin
-
 from fixtures import cluster
+import pytest
+
 
 # Scenario-2:
 # Single User Single Channel: Create Unique docs and update docs verify all num docs present in changes feed.
 # Verify all revisions in changes feed
 # https://docs.google.com/spreadsheets/d/1nlba3SsWagDrnAep3rDZHXHIDmRH_FFDeTaYJms_55k/edit#gid=598127796
 
-def test_update_docs_multiple_users_multiple_channels(cluster):
+@pytest.mark.sanity
+@pytest.mark.distributed_index
+@pytest.mark.parametrize("num_docs", [100])
+@pytest.mark.parametrize("num_revisions", [100])
+def test_single_user_single_channel_doc_updates(cluster, num_docs, num_revisions):
 
     start = time.time()
     cluster.reset(config="sync_gateway_default_functional_tests.json")
-    num_docs = 100
-    num_revisions = 10
+    num_docs = num_docs
+    num_revisions = num_revisions
     username = "User-1"
     password = "password"
     channels = ["channel-1"]
