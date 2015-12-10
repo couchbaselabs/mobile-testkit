45c114468aa8bca496d5f2699694abe845f5aa79
Ashvinder Singh <ashvinder@Ashvinders-MacBook-Pro.local>
2015-12-10 03:20:18 -0800

    webhooks support

---
+++ lib/web_server.py
@@ -0,0 +1,55 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+import urlparse
+import time
+import json
+from BaseHTTPServer import HTTPServer
+import threading
+from lib import settings
+import logging
+log = logging.getLogger(settings.LOGGER)
+
+
+server_received_data = []
+
+class HttpHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        log.info('Received GET request')
+        self.send_response(200)
+        self.send_header('Last-Modified', self.date_time_string(time.time()))
+        self.end_headers()
+        self.wfile.write('Response body\n')
+        return
+
+    def do_POST(self):
+        log.info('Received POST request')
+        content_len = int(self.headers.getheader('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        data = json.loads(post_body)
+        log.info("Received {} data in Post request".format(data))
+        server_received_data.append(data)
+        log.info("Appended POST data payload to server_received_data")
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        return
+
+
+class WebServer(object):
+    def __init__(self, port=8080):
+        self.port = port
+        self.server = HTTPServer(('',port),HttpHandler)
+
+    def start(self):
+        thread = threading.Thread(target=self.server.serve_forever)
+        thread.daemon = True
+        try:
+            thread.start()
+        except Exception as e:
+            raise ValueError("Caught exception could not launch webserver thread",e)
+
+    def stop(self):
+        self.server.shutdown()
+
+    def get_data(self):
+        return server_received_data
