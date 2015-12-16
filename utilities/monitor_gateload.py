02ec468fd5d9f76e1da498303cc5ecf1173772c3
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-16 15:33:20 -0800

    Add perf monitoring scripts

---
+++ utilities/monitor_gateload.py
@@ -0,0 +1,23 @@
+import sys
+import subprocess
+
+from optparse import OptionParser
+
+
+if __name__ == "__main__":
+    usage = """
+    usage: python monitor_gateload.py -e "111.11.111.111:4985,222.22.222.222:4985"
+    """
+
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-e", "", action="store", type="string", dest="endpoints", default=None, help="ips running gateload")
+    (opts, args) = parser.parse_args(sys.argv[1:])
+
+    if opts.endpoints is None:
+        print("Please specify '-e' endpoints to monitor")
+        sys.exit(1)
+
+    vars = "gateload.ops.PushToSubscriberInteractive.p95, gateload.ops.PushToSubscriberInteractive.p99,gateload.total_doc_pulled,gateload.total_doc_pushed"
+
+    subprocess.call(["expvarmon", "-ports={}".format(opts.endpoints), "-vars={}".format(vars)])
