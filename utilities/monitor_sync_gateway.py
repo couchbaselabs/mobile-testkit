9e64940ef4649942b14155df257051fa5159d0e4
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-17 13:26:42 -0800

    Grab ips from provisioning config for expvarmon

--- utilities/monitor_sync_gateway.py
+++ utilities/monitor_sync_gateway.py
@@ -1,23 +1,21 @@
-import sys
 import subprocess
+from provisioning_config_parser import hosts_for_tag
 
 from optparse import OptionParser
 
-
 if __name__ == "__main__":
     usage = """
-    usage: python monitor_sync_gateway.py -e "111.11.111.111:4985,222.22.222.222:4985"
+    usage: python monitor_sync_gateway.py"
     """
 
-    parser = OptionParser(usage=usage)
+    # Get sync_gateway ips from ansible inventory
+    sgs_host_vars = hosts_for_tag("sync_gateways")
+    sgs = [sgv["ansible_ssh_host"] for sgv in sgs_host_vars]
 
-    parser.add_option("-e", "", action="store", type="string", dest="endpoints", default=None, help="ips running sync_gateway")
-    (opts, args) = parser.parse_args(sys.argv[1:])
-
-    if opts.endpoints is None:
-        print("Please specify '-e' endpoints to monitor")
-        sys.exit(1)
+    print("Monitoring sync_gateways: {}".format(sgs))
+    sgs_with_port = [sg + ":4985" for sg in sgs]
+    sgs_joined = ",".join(sgs_with_port)
 
     vars = "mem:memstats.Alloc,mem:memstats.Sys,mem:memstats.HeapAlloc,mem:memstats.HeapInuse,memstats.NumGC,memstats.PauseTotalNs,memstats.PauseNs,syncGateway_db.channelChangesFeeds,syncGateway_db.document_gets,syncGateway_db.revisionCache_adds,syncGateway_db.revs_added"
 
-    subprocess.call(["expvarmon", "-ports={}".format(opts.endpoints), "-endpoint=/_expvar", "-vars={}".format(vars)])
+    subprocess.call(["expvarmon", "-ports={}".format(sgs_joined), "-endpoint=/_expvar", "-vars={}".format(vars)])
