b52a8c3de5be35100371777219f33fc3cb846e95
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-18 15:26:25 -0800

    Cleanup and add validation for monitor scripts

--- utilities/monitor_gateload.py
+++ utilities/monitor_gateload.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 from provisioning_config_parser import hosts_for_tag
 
 
@@ -11,12 +12,25 @@ if __name__ == "__main__":
     lgs_host_vars = hosts_for_tag("load_generators")
     lgs = [lg["ansible_ssh_host"] for lg in lgs_host_vars]
 
-    print("Monitoring sync_gateways: {}".format(lgs))
+    if len(lgs) == 0:
+        print("No gateloads to monitor in 'provisioning_config'")
+        sys.exit(1)
+
+    print("Monitoring gateloads: {}".format(lgs))
     lgs_with_port = [lg + ":9876" for lg in lgs]
     lgs_joined = ",".join(lgs_with_port)
 
-    vars = "gateload.ops.PushToSubscriberInteractive.p95,gateload.ops.PushToSubscriberInteractive.p99,gateload.total_doc_pulled,gateload.total_doc_pushed,gateload.user_active,gateload.user_awake"
+    expvars = [
+        "gateload.ops.PushToSubscriberInteractive.p95",
+        "gateload.ops.PushToSubscriberInteractive.p99",
+        "gateload.total_doc_pulled",
+        "gateload.total_doc_pushed",
+        "gateload.user_active",
+        "gateload.user_awake"
+    ]
+
+    expvars_combined = ",".join(expvars)
 
     subprocess.call(["expvarmon",
                      "-ports={}".format(lgs_joined),
-                     "-vars={}".format(vars)])
+                     "-vars={}".format(expvars_combined)])
