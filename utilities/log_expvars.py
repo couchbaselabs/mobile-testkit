8b5b2f73ae3c7987f8e1ad4cea9c33f4543a8a84
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-18 15:12:46 -0800

    Minor expvar log cleanup

--- utilities/log_expvars.py
+++ utilities/log_expvars.py
@@ -5,6 +5,35 @@ import requests
 from provisioning_config_parser import hosts_for_tag
 
 
+def write_endpoint_expvars(file, endpoint):
+    try:
+        resp = requests.get("http://{}".format(endpoint))
+        print(resp.url)
+        file.write("\n############## VARS #############\n")
+        file.write("Date / Time: {}\n".format(datetime.datetime.now()))
+        file.write("Endpoint: {}\n".format(endpoint))
+
+        expvars = resp.json()
+
+        p95 = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p95"]
+        p99 = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p99"]
+        total_doc_pushed = expvars["gateload"]["total_doc_pushed"]
+        total_doc_pulled = expvars["gateload"]["total_doc_pulled"]
+        user_active = expvars["gateload"]["user_active"]
+        user_awake = expvars["gateload"]["user_awake"]
+
+        file.write("P95: {}\n".format(p95))
+        file.write("P99: {}\n".format(p99))
+        file.write("total_doc_pushed: {}\n".format(total_doc_pushed))
+        file.write("total_doc_pulled: {}\n".format(total_doc_pulled))
+        file.write("user_active: {}\n".format(user_active))
+        file.write("user_awake: {}\n".format(user_awake))
+
+    except Exception as e:
+        print("Failed to connect: {}".format(e))
+        file.write("!! Failed to connect to endpoint: {}\n".format(endpoint))
+
+
 def log_expvars():
     usage = """
     usage: log_expvars.py"
@@ -38,32 +67,7 @@ def log_expvars():
         while test_time < 1200:
 
             for endpoint in endpoints:
-                try:
-                    resp = requests.get("http://{}".format(endpoint))
-                    print(resp.url)
-                    f.write("\n############## VARS #############\n")
-                    f.write("Date / Time: {}\n".format(datetime.datetime.now()))
-                    f.write("Endpoint: {}\n".format(endpoint))
-
-                    expvars = resp.json()
-
-                    p95 = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p95"]
-                    p99 = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p99"]
-                    total_doc_pushed = expvars["gateload"]["total_doc_pushed"]
-                    total_doc_pulled = expvars["gateload"]["total_doc_pulled"]
-                    user_active = expvars["gateload"]["user_active"]
-                    user_awake = expvars["gateload"]["user_awake"]
-
-                    f.write("P95: {}\n".format(p95))
-                    f.write("P99: {}\n".format(p99))
-                    f.write("total_doc_pushed: {}\n".format(total_doc_pushed))
-                    f.write("total_doc_pulled: {}\n".format(total_doc_pulled))
-                    f.write("user_active: {}\n".format(user_active))
-                    f.write("user_awake: {}\n".format(user_awake))
-
-                except Exception as e:
-                    print("Failed to connect: {}".format(e))
-                    f.write("!! Failed to connect to endpoint: {}\n".format(endpoint))
+                write_endpoint_expvars(f, endpoint)
 
             print("Elapsed: {}".format(test_time))
             f.write("\n\n\n".format(endpoint))
