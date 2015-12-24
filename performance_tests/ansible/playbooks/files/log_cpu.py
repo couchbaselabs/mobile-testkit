16ef8fe1fad7a04784ff110643c2139d3711024e
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-24 14:38:11 -0800

    Writing CPU stats

--- performance_tests/ansible/playbooks/files/log_cpu.py
+++ performance_tests/ansible/playbooks/files/log_cpu.py
@@ -14,6 +14,21 @@ with open("cpu_stats", "w") as f:
     # collect cpu stats while gateload process is running
     while is_running("gateload"):
 
-        f.write(datetime.datetime.now())
-        print(psutil.cpu_percent())
+        print("Datetime: {}".format(datetime.datetime.now()))
+        print("-------------------------------")
+        print("CPU times: {}".format(psutil.cpu_times()))
+        print("CPU times (per CPU): {}".format(psutil.cpu_times(percpu=True)))
+        print("CPU percent: {}".format(psutil.cpu_percent(interval=1)))
+        print("CPU percent (per CPU): {}".format(psutil.cpu_percent(interval=1, percpu=True)))
+
+        f.write("Datetime: {}\n".format(datetime.datetime.now()))
+        f.write("-------------------------------\n")
+        f.write("CPU times: {}\n".format(psutil.cpu_times()))
+        f.write("CPU times (per CPU): {}\n".format(psutil.cpu_times(percpu=True)))
+        f.write("CPU percent: {}\n".format(psutil.cpu_percent(interval=1)))
+        f.write("CPU percent (per CPU): {}\n".format(psutil.cpu_percent(interval=1, percpu=True)))
+
+        f.write("\n\n")
+        # Wait a minute
+        time.sleep(1)
 
