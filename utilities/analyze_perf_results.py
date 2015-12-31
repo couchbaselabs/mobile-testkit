628f0be617f0178ac9d31996b98b49fd2e8a845e
Seth Rosetter <seth.rosetter@gmail.com>
2015-12-31 11:30:31 -0800

    Capture full gateload and sg expvars, filter when analyzing perf results

--- utilities/analyze_perf_results.py
+++ utilities/analyze_perf_results.py
@@ -1,11 +1,13 @@
 import json
 import datetime
 import os
+import sys
 import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
+from optparse import OptionParser
+from collections import OrderedDict
 
-matplotlib.rcParams.update({'font.size': 8})
+matplotlib.rcParams.update({'font.size': 6})
 
 
 def keys_present(keys, dictionary):
@@ -16,11 +18,12 @@ def keys_present(keys, dictionary):
     return True
 
 
-def plot_expvars(figure, json_file_name):
+def plot_gateload_expvars(figure, json_file_name):
 
-    obj = dict()
+    print("Plotting gateload expvars ...")
+
     with open(json_file_name, "r") as f:
-        obj = json.loads(f.read())
+        obj = json.loads(f.read(), object_pairs_hook=OrderedDict)
 
     datetimes = []
     p95 = []
@@ -28,31 +31,58 @@ def plot_expvars(figure, json_file_name):
     docs_pushed = []
     docs_pulled = []
 
-    for entry in obj.keys():
-        # only plot entry with all vars wer are looking for
-        if keys_present(["p95", "p99", "total_doc_pushed", "total_doc_pulled"], obj[entry]):
-            datetimes.append(datetime.datetime.strptime(entry, "%Y-%m-%d %H:%M:%S.%f"))
-            p95.append(obj[entry]["p95"])
-            p99.append(obj[entry]["p99"])
-            docs_pushed.append(obj[entry]["total_doc_pushed"])
-            docs_pulled.append(obj[entry]["total_doc_pulled"])
+    for timestamp in obj:
+
+        # only plot if p95 and p99 exist in expvars
+        if "PushToSubscriberInteractive" in obj[timestamp]["expvars"]["gateload"]["ops"]:
+            datetimes.append(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f"))
+            p95.append(obj[timestamp]["expvars"]["gateload"]["ops"]["PushToSubscriberInteractive"]["p95"])
+            p99.append(obj[timestamp]["expvars"]["gateload"]["ops"]["PushToSubscriberInteractive"]["p99"])
+            docs_pushed.append(obj[timestamp]["expvars"]["gateload"]["total_doc_pushed"])
+            docs_pulled.append(obj[timestamp]["expvars"]["gateload"]["total_doc_pulled"])
 
     # Plot p95 / p99
     ax1 = figure.add_subplot(211)
-    ax1.set_title(" p95 (blue) / p99 ns (g)")
+    ax1.set_title("PushToSubscriberInteractive: p95 (blue) / p99 ns (green)")
     ax1.plot(datetimes, p95, "bs", datetimes, p99, "g^")
     # for i, j in zip(datetimes, p95):
     #     ax1.annotate(str(j), xy=(i, j))
 
     # Plot docs pushed / docs pulled
     ax2 = figure.add_subplot(212)
-    ax2.set_title("docs pushed (red) / docs pulled (yellow)")
+    ax2.set_title("total_doc_pushed (red) / total_doc_pulled (yellow)")
     ax2.plot(datetimes, docs_pushed, "rs", datetimes, docs_pulled, "y^")
 
     figure.autofmt_xdate()
 
 
+def plot_sync_gateway_expvars(figure, json_file_name):
+
+    print("Plotting sync_gateway expvars ...")
+
+    with open(json_file_name, "r") as f:
+        obj = json.loads(f.read(), object_pairs_hook=OrderedDict)
+
+    datetimes = []
+    memstats_alloc = []
+    memstats_sys = []
+
+    for timestamp in obj:
+        datetimes.append(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f"))
+        memstats_alloc.append(obj[timestamp]["expvars"]["memstats"]["Alloc"])
+        memstats_sys.append(obj[timestamp]["expvars"]["memstats"]["Sys"])
+
+    # Plot Alloc / Sys
+    ax1 = figure.add_subplot(111)
+    ax1.set_title("memstats.Alloc (blue) / memstats.Sys (green)")
+    ax1.plot(datetimes, memstats_alloc, "bs", datetimes, memstats_sys, "g^")
+
+    figure.autofmt_xdate()
+
+
 def plot_machine_stats(figure, folder_path):
+
+    print("Plotting machine stats ...")
 
     folders = [x[0] for x in os.walk(folder_path)]
 
@@ -70,8 +100,7 @@ def plot_machine_stats(figure, folder_path):
             machine_stats[name] = obj
 
     ax3 = figure.add_subplot(111)
-    ax3.set_title("CPU percent (red=sg1, green=sg2, blue=sg3)")
-
+    ax3.set_title("CPU percent (sg1=blue, sg2=green, sg3=red, sg4=cyan, sg5=magenta, sg6=yellow, else=black)")
 
     for machine in machine_stats:
         entity = machine_stats[machine]
@@ -86,13 +115,19 @@ def plot_machine_stats(figure, folder_path):
 
         # Hack to show different machines in different color
         if machine == "sg1":
-            color = "r"
+            color = "b"
         elif machine == "sg2":
             color = "g"
         elif machine == "sg3":
-            color = "b"
-        else:
+            color = "r"
+        elif machine == "sg4":
+            color = "c"
+        elif machine == "sg5":
+            color = "m"
+        elif machine == "sg6":
             color = "y"
+        else:
+            color = "k"
 
         ax3.plot(datetimes, cpu_percents, "{}o".format(color))
 
@@ -100,19 +135,44 @@ def plot_machine_stats(figure, folder_path):
 
 
 def analze_perf_results(test_id):
-    fig1 = plt.figure()
-    fig1.text(0.5, 0.04, 'Gateload Expvars', ha='center', va='center')
+
+    print("Generating graphs for {}".format(test_id))
 
     # Generate plot of gateload expvars
-    plot_expvars(fig1, "performance_results/{}/expvars.json".format(test_id))
+    fig1 = plt.figure()
+    fig1.text(0.5, 0.04, 'Gateload Expvars', ha='center', va='center')
+    plot_gateload_expvars(fig1, "performance_results/{}/gateload_expvars.json".format(test_id))
+    plt.savefig("performance_results/{}/gateload_expvars.png".format(test_id), dpi=300)
 
-    plt.savefig("performance_results/{}/expvars.png".format(test_id), dpi=300)
-
+    # Generate plot of sync_gateway expvars
     fig2 = plt.figure()
-    fig2.text(0.5, 0.04, 'sync_gateway CPU', ha='center', va='center')
+    fig2.text(0.5, 0.04, 'sync_gateway expvars', ha='center', va='center')
+    plot_sync_gateway_expvars(fig2, "performance_results/{}/sync_gateway_expvars.json".format(test_id))
+    plt.savefig("performance_results/{}/sync_gateway_expvars.png".format(test_id), dpi=300)
 
     # Generate plot of machine stats
-    plot_machine_stats(fig2, "performance_results/{}/perf_logs/".format(test_id))
+    fig3 = plt.figure()
+    fig3.text(0.5, 0.04, 'sync_gateway CPU', ha='center', va='center')
+    plot_machine_stats(fig3, "performance_results/{}/perf_logs/".format(test_id))
+    plt.savefig("performance_results/{}/sync_gateway_machine_stats.png".format(test_id), dpi=300)
 
-    # Save png
-    plt.savefig("performance_results/{}/sg_machines.png".format(test_id), dpi=300)
+if __name__ == "__main__":
+    usage = """usage: analyze_perf_results.py
+    --test-id=<test-id>
+    """
+
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("", "--test-id",
+                      action="store", type="string", dest="test_id", default=None,
+                      help="Test id to generate graphs for")
+
+    arg_parameters = sys.argv[1:]
+
+    (opts, args) = parser.parse_args(arg_parameters)
+
+    if opts.test_id is None:
+        print("You must provide a test identifier to run the test")
+        sys.exit(1)
+
+    analze_perf_results(opts.test_id)
--- utilities/log_expvars.py
+++ utilities/log_expvars.py
@@ -3,36 +3,14 @@ import datetime
 import requests
 import json
 
+from collections import OrderedDict
+
 from requests.exceptions import ConnectionError
 
 from provisioning_config_parser import hosts_for_tag
 
 
-def write_gateload_expvars(results_obj, endpoint):
-
-        resp = requests.get("http://{}".format(endpoint))
-        resp.raise_for_status()
-        expvars = resp.json()
-
-        now = "{}".format(datetime.datetime.utcnow())
-        results_obj[now] = dict()
-
-        try:
-            results_obj[now]["endpoint"] = endpoint
-            results_obj[now]["p95"] = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p95"]
-            results_obj[now]["p99"] = expvars["gateload"]["ops"]["PushToSubscriberInteractive"]["p99"]
-            results_obj[now]["total_doc_pushed"] = expvars["gateload"]["total_doc_pushed"]
-            results_obj[now]["total_doc_pulled"] = expvars["gateload"]["total_doc_pulled"]
-            results_obj[now]["user_active"] = expvars["gateload"]["user_active"]
-            results_obj[now]["user_awake"] = expvars["gateload"]["user_awake"]
-
-        except Exception as e:
-            # P95 and P99 may have not been calculated as of yet
-            print("Failed to connect: {}".format(e))
-            results_obj[now]["error"] = "!! Failed to connect to endpoint: {}\n".format(endpoint)
-
-
-def write_sync_gateway_expvars(results_obj, endpoint):
+def write_expvars(results_obj, endpoint):
 
         resp = requests.get("http://{}".format(endpoint))
         resp.raise_for_status()
@@ -41,12 +19,7 @@ def write_sync_gateway_expvars(results_obj, endpoint):
         now = "{}".format(datetime.datetime.utcnow())
         results_obj[now] = {
             "endpoint": endpoint,
-            "memstats": {
-                "Alloc": expvars["memstats"]["Alloc"],
-                "Sys": expvars["memstats"]["Sys"],
-                "HeapAlloc": expvars["memstats"]["HeapAlloc"],
-                "HeapSys": expvars["memstats"]["HeapSys"]
-            }
+            "expvars": expvars
         }
 
 
@@ -69,33 +42,32 @@ def log_expvars(folder_name):
     print("Monitoring sync_gateways: {}".format(sgs))
     sgs_expvar_endpoints = [sg + ":4985/_expvar" for sg in sgs]
 
+    start_time = time.time()
 
-    date_time = time.strftime("%Y-%m-%d-%H-%M-%S")
-    target_test_filename = "performance_results/{}/expvars.json".format(folder_name)
+    gateload_results = OrderedDict()
+    syncgateway_results = OrderedDict()
 
-    with open(target_test_filename, "w") as f:
+    gateload_is_running = True
+    while gateload_is_running:
 
-        start_time = time.time()
+        # Caputure expvars for gateloads
+        for endpoint in lgs_expvar_endpoints:
+            try:
+                write_expvars(gateload_results, endpoint)
+            except ConnectionError as he:
+                # connection to gateload expvars has been closed
+                print(he)
+                gateload_is_running = False
 
-        results = dict()
+        # Caputure expvars for sync_gateways
+        for endpoint in sgs_expvar_endpoints:
+            write_expvars(syncgateway_results, endpoint)
 
-        gateload_is_running = True
-        while gateload_is_running:
+        print("Elapsed: {}".format(time.time() - start_time))
+        time.sleep(30)
 
-            # Caputure expvars for gateloads
-            for endpoint in lgs_expvar_endpoints:
-                try:
-                    write_gateload_expvars(results, endpoint)
-                except ConnectionError as he:
-                    # connection to gateload expvars has been closed
-                    print(he)
-                    gateload_is_running = False
+    with open("performance_results/{}/gateload_expvars.json".format(folder_name), "w") as f:
+        f.write(json.dumps(gateload_results))
 
-            # Caputure expvars for sync_gateways
-            for endpoint in sgs_expvar_endpoints:
-                write_sync_gateway_expvars(results, endpoint)
-
-            print("Elapsed: {}".format(time.time() - start_time))
-            time.sleep(30)
-
-        f.write(json.dumps(results, indent=4))
+    with open("performance_results/{}/sync_gateway_expvars.json".format(folder_name), "w") as f:
+        f.write(json.dumps(syncgateway_results))
