32edabe277f9879155232b567a6aab1e9e385f24
Traun Leyden <tleyden@couchbase.com>
2016-01-06 16:43:56 -0800

    Fixes #192
    https://github.com/couchbaselabs/sync-gateway-testcluster/issues/192

--- lib/cluster.py
+++ lib/cluster.py
@@ -2,12 +2,14 @@ import os
 import re
 import sys
 import json
+import time
 
 import jinja2
 import ansible.inventory
 
 from lib.syncgateway import SyncGateway
 from lib.server import Server
+from lib.admin import Admin
 from provision.ansible_runner import run_ansible_playbook
 
 
@@ -52,7 +54,7 @@ class Cluster:
         num_index_readers = len(self.sync_gateways) - len(self.sync_gateway_writers)
         if num_index_readers == 0:
             raise Exception("Functional tests require at least 1 index reader")
-        
+
     
     def reset(self, config):
 
@@ -119,6 +121,110 @@ class Cluster:
             stop_on_fail=False
         )
         assert(status == 0)
+
+        # Validate CBGT
+        if not self.validate_cbgt_pindex_distribution_retry():
+            self.save_cbgt_diagnostics()
+            raise Exception("Failed to validate CBGT Pindex distribution")
+
+        print(">>> Detected valid CBGT Pindex distribution")
+
+    def save_cbgt_diagnostics(self):
+        
+        # CBGT REST Admin API endpoint
+        for sync_gateway_writer in self.sync_gateway_writers:
+
+            adminApi = Admin(sync_gateway_writer)
+            cbgt_diagnostics = adminApi.get_cbgt_diagnostics()
+            cbgt_cfg = adminApi.get_cbgt_cfg()
+        
+            # dump raw diagnostics
+            pretty_print_json = json.dumps(cbgt_diagnostics, sort_keys=True, indent=4, separators=(',', ': '))
+            print("SG {} CBGT diagnostic output: {}".format(sync_gateway_writer, pretty_print_json))
+        
+    def validate_cbgt_pindex_distribution_retry(self):
+        """
+        Validates the CBGT pindex distribution by looking for nodes that don't have
+        any pindexes assigned to it
+        """
+        for i in xrange(10):
+            is_valid = self.validate_cbgt_pindex_distribution()
+            if is_valid:
+               return True
+            else:
+                print("Could not validate CBGT Pindex distribution.  Will retry after sleeping ..")
+                time.sleep(5)
+
+        return False 
+
+    def validate_cbgt_pindex_distribution(self):
+
+        # build a map of node -> num_pindexes
+        node_defs_pindex_counts = {}
+
+        # CBGT REST Admin API endpoint 
+        adminApi = Admin(self.sync_gateways[0])
+        cbgt_cfg = adminApi.get_cbgt_cfg()
+
+        # loop over the planpindexes and update the count for the node where it lives
+        # this will end up with a dictionary like:
+        #  {'74c818f04b99b169': 32, '11886131c807a30e': 32}  (each node uuid has 32 pindexes)
+        plan_pindexes = cbgt_cfg["planPIndexes"]["planPIndexes"]
+        for data_bucket_key, data_bucket_val in plan_pindexes.iteritems():
+
+            # get the nodes where this pindex lives
+            nodes = data_bucket_val["nodes"]
+            # it should only live on one node.  if not, abort.
+            if len(nodes) > 1:
+                raise Exception("Unexpected: a CBGT Pindex was assigned to more than one node")
+            # loop over the nodes where this pindex lives and increment the count
+            for node in nodes:
+
+                # add a key for this node if we don't already have one
+                if not node_defs_pindex_counts.has_key(node):
+                    node_defs_pindex_counts[node] = 0
+                    
+                current_pindex_count = node_defs_pindex_counts[node]
+                current_pindex_count += 1
+                node_defs_pindex_counts[node] = current_pindex_count
+
+
+        print("CBGT node to pindex counts: {}".format(node_defs_pindex_counts))
+        
+        # make sure number of unique node uuids is equal to the number of sync gateway writers
+        if len(node_defs_pindex_counts) != len(self.sync_gateway_writers):
+            print("CBGT len(unique_node_uuids) != len(self.sync_gateway_writers) ({} != {})".format(
+                len(node_defs_pindex_counts),
+                len(self.sync_gateway_writers)
+            ))
+            return False 
+
+        # make sure that all of the nodes have approx the same number of pindexes assigneed to them
+        i = 0
+        num_pindex_first_node = 0 
+        for node_def_uuid, num_pindexes in node_defs_pindex_counts.iteritems():
+
+            if i == 0:
+                # it's the first node we've looked at, just record number of pindexes and continue 
+                num_pindex_first_node = num_pindexes
+                i += 1
+                continue
+
+            # ok, it's the 2nd+ node, make sure the delta with the first node is less than or equal to 1
+            # (the reason we can't compare for equality is that sometimes the pindexes can't be
+            # divided evenly across the cluster)
+            delta = abs(num_pindex_first_node - num_pindexes)
+            if delta > 1:
+                print("CBGT Sync Gateway node {} has {} pindexes, but other node has {} pindexes.".format(
+                    node_def_uuid,
+                    num_pindexes,
+                    num_pindex_first_node
+                ))
+                return False
+                
+    
+        return True
+        
 
     def __repr__(self):
         s = "\n\n"
