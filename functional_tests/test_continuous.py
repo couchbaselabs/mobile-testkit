e6e81c5b8da9a2fcd05bd6e55d49bcc404bb8a7d
Seth Rosetter <seth.rosetter@gmail.com>
2016-01-12 12:01:07 -0800

    Bump continuous back to 100 workers so it has enough threads to run tests, remove exceptions handling, futures should succeed or fail test if they do not.

--- functional_tests/test_continuous.py
+++ functional_tests/test_continuous.py
@@ -42,30 +42,26 @@ def test_continuous_changes_parametrized(cluster, conf, num_users, num_docs, num
     abc_doc_pusher = admin.register_user(target=cluster.sync_gateways[2], db="db", name="abc_doc_pusher", password="password", channels=["ABC"])
     doc_terminator = admin.register_user(target=cluster.sync_gateways[2], db="db", name="doc_terminator", password="password", channels=["TERMINATE"])
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=lib.settings.MAX_REQUEST_WORKERS) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
 
         futures = {executor.submit(user.start_continuous_changes_tracking, termination_doc_id="killcontinuous"): user.name for user in users}
         futures[executor.submit(abc_doc_pusher.add_docs, num_docs)] = "doc_pusher"
 
         for future in concurrent.futures.as_completed(futures):
-            try:
-                task_name = futures[future]
+            task_name = futures[future]
 
-                # Send termination doc to seth continuous changes feed subscriber
-                if task_name == "doc_pusher":
-                    abc_doc_pusher.update_docs(num_revs_per_doc=num_revisions)
+            # Send termination doc to seth continuous changes feed subscriber
+            if task_name == "doc_pusher":
+                abc_doc_pusher.update_docs(num_revs_per_doc=num_revisions)
 
-                    time.sleep(10)
+                time.sleep(10)
 
-                    doc_terminator.add_doc("killcontinuous")
-                elif task_name.startswith("user"):
-                    # When the user has continuous _changes feed closed, return the docs and verify the user got all the channel docs
-                    docs_in_changes = future.result()
-                    # Expect number of docs + the termination doc + _user doc
-                    verify_same_docs(expected_num_docs=num_docs, doc_dict_one=docs_in_changes, doc_dict_two=abc_doc_pusher.cache)
-
-            except Exception as e:
-                print("Futures: error: {}".format(e))
+                doc_terminator.add_doc("killcontinuous")
+            elif task_name.startswith("user"):
+                # When the user has continuous _changes feed closed, return the docs and verify the user got all the channel docs
+                docs_in_changes = future.result()
+                # Expect number of docs + the termination doc + _user doc
+                verify_same_docs(expected_num_docs=num_docs, doc_dict_one=docs_in_changes, doc_dict_two=abc_doc_pusher.cache)
 
     # Expect number of docs + the termination doc
     verify_changes(abc_doc_pusher, expected_num_docs=num_docs, expected_num_revisions=num_revisions, expected_docs=abc_doc_pusher.cache)
@@ -95,25 +91,21 @@ def test_continuous_changes_sanity(cluster, conf, num_docs, num_revisions):
 
     docs_in_changes = dict()
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=lib.settings.MAX_REQUEST_WORKERS) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
 
         futures = dict()
         futures[executor.submit(seth.start_continuous_changes_tracking, termination_doc_id="killcontinuous")] = "continuous"
         futures[executor.submit(abc_doc_pusher.add_docs, num_docs)] = "doc_pusher"
 
         for future in concurrent.futures.as_completed(futures):
-            try:
-                task_name = futures[future]
+            task_name = futures[future]
 
-                # Send termination doc to seth continuous changes feed subscriber
-                if task_name == "doc_pusher":
-                    abc_doc_pusher.update_docs(num_revs_per_doc=num_revisions)
-                    doc_terminator.add_doc("killcontinuous")
-                elif task_name == "continuous":
-                    docs_in_changes = future.result()
-
-            except Exception as e:
-                print("Futures: error: {}".format(e))
+            # Send termination doc to seth continuous changes feed subscriber
+            if task_name == "doc_pusher":
+                abc_doc_pusher.update_docs(num_revs_per_doc=num_revisions)
+                doc_terminator.add_doc("killcontinuous")
+            elif task_name == "continuous":
+                docs_in_changes = future.result()
 
     # Expect number of docs + the termination doc
     verify_changes(abc_doc_pusher, expected_num_docs=num_docs, expected_num_revisions=num_revisions, expected_docs=abc_doc_pusher.cache)
