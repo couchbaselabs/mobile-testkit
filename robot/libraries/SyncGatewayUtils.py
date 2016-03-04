e3ef95a058878de0fa87c50f45dc2232bb41709c
Seth Rosetter <seth.rosetter@gmail.com>
2016-03-03 16:17:07 -0800

    Use resource file for paths

--- robot/libraries/SyncGatewayUtils.py
+++ robot/libraries/SyncGatewayUtils.py
@@ -18,7 +18,10 @@ class SyncGatewayUtils:
         print("Installing {} sync_gateway on {}".format(version, platform))
 
         url = "http://latestbuilds.hq.couchbase.com/couchbase-sync-gateway/1.2.0/1.2.0-83/couchbase-sync-gateway-enterprise_1.2.0-83_x86_64.tar.gz"
-        os.chdir("resources/sync_gateway")
+
+        print os.getcwd()
+
+        os.chdir("resources/artifacts/sync_gateway")
 
         r = requests.get(url)
 
@@ -28,13 +31,18 @@ class SyncGatewayUtils:
         with tarfile.open(self.file_name) as tar_f:
             tar_f.extractall()
 
-        os.chdir("../../")
+        os.chdir("../../../")
+
+        print os.getcwd()
 
     def uninstall_local_sync_gateway(self, platform):
-        os.chdir("resources/sync_gateway")
+
+        os.chdir("resources/artifacts/sync_gateway")
+
         os.remove(self.file_name)
         shutil.rmtree("couchbase-sync-gateway")
-        os.chdir("../../")
+
+        os.chdir("../../../")
 
     def get_sync_gateway_document_count(self, db):
         docs = self.admin.get_all_docs(db)
