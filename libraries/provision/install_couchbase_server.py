5bdf9c4c3f5734159c09191d70ed3a53a6d28a0b
Seth Rosetter <seth.rosetter@gmail.com>
2016-04-21 12:24:29 -0700

    Support dev builds in install_couchbase_server.py

--- libraries/provision/install_couchbase_server.py
+++ libraries/provision/install_couchbase_server.py
@@ -9,62 +9,69 @@ class CouchbaseServerConfig:
 
     def __init__(self, version):
 
-        self._valid_versions = [
+        version_build = version.split("-")
+        self.version = version_build[0]
+        if len(version_build) == 2:
+            # Build number is included
+            self.build = version_build[1]
+        else:
+            self.build = None
+
+        self.released_versions = [
             "3.1.0",
             "3.1.1",
             "3.1.2",
+            "3.1.3",
+            "3.1.4",
+            "3.1.5",
             "4.0.0",
             "4.1.0"
         ]
 
-        self._version = version
+    def get_baseurl_package(self):
 
-    #  https://s3.amazonaws.com/packages.couchbase.com/releases/4.1.0/couchbase-server-enterprise-4.1.0-centos7.x86_64.rpm
-    def _base_url_package_for_server(self, version):
-        if self.is_valid:
-            base_url = "https://s3.amazonaws.com/packages.couchbase.com/releases/{}".format(version)
-            package_name = "couchbase-server-enterprise-{}-centos7.x86_64.rpm".format(version)
-            return base_url, package_name
+        if self.build is None:
+            if self.version not in self.released_versions:
+                raise ValueError("Version not found in released versions: {}".format(self.version))
+
+            # Download the server release from s3
+            base_url = "https://s3.amazonaws.com/packages.couchbase.com/releases/{}".format(self.version)
+            package_name = "couchbase-server-enterprise-{}-centos7.x86_64.rpm".format(self.version)
+
         else:
-            print "Server package url not found. Make sure to specify a version / build."
-            raise ValueError("Invalid server url")
 
-    @property
-    def version(self):
-        return self._version
+            # Get dev server package from latestbuilds
+            if self.version.startswith("4.1"):
+                # http://172.23.120.24/builds/latestbuilds/couchbase-server/sherlock/5914/couchbase-server-enterprise-4.1.1-5914-centos7.x86_64.rpm
+                base_url = "http://172.23.120.24/builds/latestbuilds/couchbase-server/sherlock/{}".format(self.build)
+                package_name = "couchbase-server-enterprise-{}-{}-centos7.x86_64.rpm".format(self.version, self.build)
+            elif self.version.startswith("4.5"):
+                # http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/2151/couchbase-server-enterprise-4.5.0-2151-centos7.x86_64.rpm
+                base_url = "http://172.23.120.24/builds/latestbuilds/couchbase-server/watson/{}".format(self.build)
+                package_name = "couchbase-server-enterprise-{}-{}-centos7.x86_64.rpm".format(self.version, self.build)
+            else:
+                raise ValueError("Unable to resolve dev build for version: {}-{}".format(self.version, self.build))
+
+        return base_url, package_name
 
     def __str__(self):
         output = "\n  Couchbase Server configuration\n"
         output += "  ------------------------------------------\n"
-        output += "  version:      {}\n".format(self._version)
+        output += "  version:      {}\n".format(self.version)
         return output
-
-    def server_base_url_and_package(self):
-        return self._base_url_package_for_server(self._version)
-
-    def is_valid(self):
-        if self._version not in self._valid_versions:
-            print "Make sure your server version is one of the following: {}".format(self._valid_versions)
-            return False
-        return True
 
 
 def install_couchbase_server(couchbase_server_config):
 
     print(couchbase_server_config)
 
-    if not couchbase_server_config.is_valid():
-        print "Invalid server provisioning configuration. Exiting ..."
-        sys.exit(1)
-
-    server_base_url, server_package_name = couchbase_server_config.server_base_url_and_package()
-
     ansible_runner = AnsibleRunner()
 
+    server_baseurl, server_package_name = couchbase_server_config.get_baseurl_package()
     status = ansible_runner.run_ansible_playbook(
         "install-couchbase-server-package.yml",
         "couchbase_server_package_base_url={0} couchbase_server_package_name={1}".format(
-            server_base_url,
+            server_baseurl,
             server_package_name
         ),
         stop_on_fail=False
