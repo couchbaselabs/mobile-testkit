792c45f914bcc92c5eda694297198624ebb057cc
Seth Rosetter <seth.rosetter@gmail.com>
2016-05-19 13:50:43 -0700

    Remove unused local keywords, use cluster install approach across repo

--- keywords/SyncGateway.py
+++ keywords/SyncGateway.py
@@ -20,51 +20,6 @@ class SyncGateway:
     def __init__(self):
         self._session = Session()
 
-    def download_local_sync_gateway(self):
-
-        # Check if package is already downloaded and return if it is preset
-        if os.path.isdir("{}/{}".format(BINARY_DIR, self.extracted_file_name)):
-            logging.info("Package exists: {}. Skipping download".format(self.extracted_file_name))
-            return
-
-        print("Installing {} sync_gateway".format(self._version_build))
-
-        version, build = version_and_build(self._version_build)
-        if version == "1.1.1":
-            url = "{}/couchbase-sync-gateway/release/{}/{}/couchbase-sync-gateway-enterprise_{}_x86_64.tar.gz".format(
-                LATEST_BUILDS,
-                version,
-                self._version_build,
-                self._version_build)
-        else:
-            url = "{}/couchbase-sync-gateway/{}/{}/couchbase-sync-gateway-enterprise_{}_x86_64.tar.gz".format(
-                LATEST_BUILDS,
-                version,
-                self._version_build,
-                self._version_build)
-
-        # Download and write package
-        r = requests.get(url)
-        file_name = "{}/{}.tar.gz".format(BINARY_DIR, self.extracted_file_name)
-
-        with open(file_name, "wb") as f:
-            f.write(r.content)
-
-        # Extract package
-        with tarfile.open(file_name) as tar_f:
-            tar_f.extractall(path="{}/{}".format(BINARY_DIR, self.extracted_file_name))
-
-        # Remove .tar.gz and return to root directory
-        os.remove(file_name)
-
-    def remove_local_sync_gateway(self):
-        logging.info("Removing {}".format(self.extracted_file_name))
-        shutil.rmtree("deps/binaries/{}".format(self.extracted_file_name))
-
-    def get_local_sync_gateway_binary_path(self):
-        sync_gateway_binary_path = "{}/{}/couchbase-sync-gateway/bin/sync_gateway".format(BINARY_DIR, self.extracted_file_name)
-        logging.info("sync_gateway binary path: {}".format(sync_gateway_binary_path))
-        return sync_gateway_binary_path
     def verify_sync_gateway_launched(self, host, port, admin_port):
 
         url = "http://{}:{}".format(host, port)
