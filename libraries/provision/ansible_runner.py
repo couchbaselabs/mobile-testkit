315c35bcb226933d31a3aff64350ca77a6fb493e
Traun Leyden <tleyden@couchbase.com>
2016-05-23 16:32:47 -0700

    Rough first draft of running ansible via api instead of fork
    Basic stuff seems to work.  Lots of cleanup needed still.
    
    https://github.com/couchbaselabs/mobile-testkit/issues/438

--- libraries/provision/ansible_runner.py
+++ libraries/provision/ansible_runner.py
@@ -1,7 +1,9 @@
 import os
 import sys
 import subprocess
-
+from ansible_python_runner import Runner
+import re
+import logging
 
 class AnsibleRunner:
 
@@ -10,24 +12,53 @@ class AnsibleRunner:
 
     def run_ansible_playbook(self, script_name, extra_vars=None, stop_on_fail=True):
 
-        # Need to cd here to pick up dynamic inventory
-        os.chdir("libraries/provision/ansible/playbooks")
+        print "run_ansible_playbook called with script_name: {} extra_vars: {}".format(script_name, extra_vars)
+        print "os.path.realpath(__file__): {}".format(os.path.realpath(__file__))
 
-        if not os.path.isfile(script_name):
-            print("Could not locate ansible script {0} in {1}".format(script_name, os.getcwd()))
-            sys.exit(1)
+        # parse key=value pairs into dictionary
+        # shamelessly copied n pasted from http://bit.ly/1syhZ31
+        run_data = dict(re.findall(r'(\S+)=(".*?"|\S+)', extra_vars))
 
-        if extra_vars is not None:
-            status = subprocess.call(["ansible-playbook", "-i", "../../../../{}".format(self.provisiong_config), script_name, "--extra-vars", extra_vars])
-        else:
-            status = subprocess.call(["ansible-playbook", "-i", "../../../../{}".format(self.provisiong_config), script_name])
+        print "extra_vars parsed into run_data: {}".format(run_data)
 
-        if status != 0 and stop_on_fail:
-            sys.exit(1)
+        # inventory_filename, playbook, run_data, verbosity = 0):
+        # inventory_filename = "../../{}".format(self.provisiong_config)
+        print "provisiong_config: {}".format(self.provisiong_config)
+        inventory_filename = "/Users/tleyden/Development/mobile-testkit/{}".format(self.provisiong_config)
 
-        os.chdir("../../../../")
+        playbook_filename = "/Users/tleyden/Development/mobile-testkit/libraries/provision/ansible/playbooks/{}".format(script_name)
 
-        return status
+        print("creating runner with inventory_filename: {}".format(inventory_filename))
+
+        runner = Runner(
+            inventory_filename=inventory_filename,
+            playbook = playbook_filename,
+            run_data = run_data,
+            verbosity = 10
+        )
+
+        print("calling runner")
+        stats = runner.run()
+
+        """
+        stats fields:
+
+        self.processed = {}
+        self.failures  = {}
+        self.ok        = {}
+        self.dark      = {}
+        self.changed   = {}
+        self.skipped   = {}
+        """
+        print("stats.processed: {}".format(stats.processed))
+        print("stats.failures: {}".format(stats.failures))
+        print("stats.ok: {}".format(stats.ok))
+        print("stats.dark: {}".format(stats.dark))
+        print("stats.changed: {}".format(stats.changed))
+        print("stats.skipped: {}".format(stats.skipped))
+
+        return 0  # TODO: if ansible command failed, return an error ..
+
 
     def run_targeted_ansible_playbook(self, script_name, target_name, extra_vars=None, stop_on_fail=True):
 
