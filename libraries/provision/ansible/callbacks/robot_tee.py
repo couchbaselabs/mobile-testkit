a32661d6247e1821984f6b0be8b73d7536154930
Traun Leyden <tleyden@couchbase.com>
2016-05-24 20:06:55 -0700

    comment out the stdout "passthrough" stuff since it's losing info
    for the moment, it won't appear in ansible console .. will need to re-enable w/ better wrapping

--- libraries/provision/ansible/callbacks/robot_tee.py
+++ libraries/provision/ansible/callbacks/robot_tee.py
@@ -13,45 +13,52 @@ class CallbackModule(CallbackModule_default):
         self.last_task = None
         self.shown_title = False
 
-    def v2_playbook_on_task_start(self, task, is_conditional):
-        console("TASK[{}] *********************************************** ".format(task.name))
-        self.last_task = task
-        self.shown_title = False
-
-    def display_task_banner(self):
-        if not self.shown_title:
-            self.super_ref.v2_playbook_on_task_start(self.last_task, None)
-            self.shown_title = True
-
-    def v2_runner_on_failed(self, result, ignore_errors=False):
-        self.display_task_banner()
-        self.super_ref.v2_runner_on_failed(result, ignore_errors)
-
-    def v2_runner_on_ok(self, result):
-        if result._result.get('changed', False):
-            self.display_task_banner()
-            self.super_ref.v2_runner_on_ok(result)
-        else:
-            pass
-
-    def v2_runner_on_unreachable(self, result):
-        self.display_task_banner()
-        self.super_ref.v2_runner_on_unreachable(result)
-
-    def v2_runner_on_skipped(self, result):
-        pass
-
-    def v2_playbook_on_include(self, included_file):
-        pass
-
-    def v2_playbook_item_on_ok(self, result):
-        console("v2_playbook_item_on_ok: {}".format(str(result)))
-        self.display_task_banner()
-        self.super_ref.v2_playbook_item_on_ok(result)
-
-    def v2_playbook_item_on_skipped(self, result):
-        pass
-
-    def v2_playbook_item_on_failed(self, result):
-        self.display_task_banner()
-        self.super_ref.v2_playbook_item_on_failed(result)
+    # def v2_playbook_on_task_start(self, task, is_conditional):
+    #     console("TASK[{}] *********************************************** ".format(task.name))
+    #     self.last_task = task
+    #     self.shown_title = False
+    #
+    # def display_task_banner(self):
+    #     if not self.shown_title:
+    #         self.super_ref.v2_playbook_on_task_start(self.last_task, None)
+    #         self.shown_title = True
+    #
+    # def v2_runner_on_failed(self, result, ignore_errors=False):
+    #     console("v2_runner_on_failed: {}".format(str(result)))
+    #     self.display_task_banner()
+    #     self.super_ref.v2_runner_on_failed(result, ignore_errors)
+    #
+    # def v2_runner_on_ok(self, result):
+    #     console("v2_runner_on_ok: {}".format(str(result)))
+    #     if result._result.get('changed', False):
+    #         self.display_task_banner()
+    #         self.super_ref.v2_runner_on_ok(result)
+    #     else:
+    #         pass
+    #
+    # def v2_runner_on_unreachable(self, result):
+    #     console("v2_runner_on_unreachable: {}".format(str(result)))
+    #     self.display_task_banner()
+    #     self.super_ref.v2_runner_on_unreachable(result)
+    #
+    # def v2_runner_on_skipped(self, result):
+    #     console("v2_runner_on_skipped: {}".format(str(result)))
+    #     pass
+    #
+    # def v2_playbook_on_include(self, included_file):
+    #     console("v2_playbook_on_include: {}".format(str(included_file)))
+    #     pass
+    #
+    # def v2_playbook_item_on_ok(self, result):
+    #     console("v2_playbook_item_on_ok: {}".format(str(result)))
+    #     self.display_task_banner()
+    #     self.super_ref.v2_playbook_item_on_ok(result)
+    #
+    # def v2_playbook_item_on_skipped(self, result):
+    #     console("v2_playbook_item_on_skipped: {}".format(str(result)))
+    #     pass
+    #
+    # def v2_playbook_item_on_failed(self, result):
+    #     console("v2_playbook_item_on_failed: {}".format(str(result)))
+    #     self.display_task_banner()
+    #     self.super_ref.v2_playbook_item_on_failed(result)
