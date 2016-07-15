276034acd77630958d40722c3beae6077451a9ee
Traun Leyden <tleyden@couchbase.com>
2016-07-15 07:17:27 -0700

    Rework to use boto directly

--- libraries/provision/generate_pools_json_from_aws.py
+++ libraries/provision/generate_pools_json_from_aws.py
@@ -1,85 +1,23 @@
 
-import ec2
-import ansible.inventory
 from optparse import OptionParser
 import sys
 import json
 import time
+from boto import cloudformation
+from boto import ec2
 
 # This generates a pool.json file from the current AWS EC2 inventory.
 # The pool.json file can then be used to generate cluster configs under resources/cluster_configs
 
-def write_to_file(cloud_formation_stack_dns_addresses, filename):
-    """
-    {
-        "ips": [
-	        "ec2-54-242-119-83.compute-1.amazonaws.com",
-	        "ec2-54-242-119-84.compute-1.amazonaws.com",
-        ]
-    }
-    """
-    output_dictionary = {"ips": cloud_formation_stack_dns_addresses}
+DEFAULT_REGION="us-east-1"
 
-    with open(filename, 'w') as target:
-        target.truncate()
-        target.write(json.dumps(output_dictionary, sort_keys=True, indent=4, separators=(',', ': ')))
-
-def lookup_dns_names(inventory, cloud_formation_stack_ip_addresses):
-    """
-    Given
-
-    [u'54.242.119.83', u'54.234.207.138'..]
-
-    Return
-
-    [u'ec2-54-242-119-83.compute-1.amazonaws.com', etc..]
-
-    """
-
-    return [inventory["_meta"]["hostvars"][ip_address]["ec2_public_dns_name"] for ip_address in cloud_formation_stack_ip_addresses]
-
-def wait_until_ip_addresses_in_running_state(ip_addresses, ec2Inventory):
-    for ip_address in ip_addresses:
-        print("Checking to see if {} in running state".format(ip_address))
-        running = False
-        for i in xrange(10):
-            state = ec2Inventory.inventory["_meta"]["hostvars"][ip_address]["ec2_state"]
-            if state != "running":
-                print("Not in running state: {}.  Updating cache and retrying".format(ip_address))
-                ec2Inventory.do_api_calls_update_cache()
-            else:
-                running = True
-                break
-        if not running:
-            raise Exception("IP {} didn't go into running state after several retries".format(ip_address))
-
-
-def filter_cloud_formation(cloud_formation_stack_ip_addresses, ip_addresses):
-    """
-    Given a list of ip addresses that are in the cloud_formation_stack, eg:
-
-    ["ip1","ip2","ip3"]
-
-    and the list of ip_addresses we want to filter:
-
-    ["ip3", "ip4"]
-
-    which may represent Couchbase Server ip addresses across multiple AWS cloudformation stacks
-
-    Return the ip addresses that are in the given cloudformation, eg    :
-
-    ["ip3"]
-
-    """
-    return filter(lambda x: x in cloud_formation_stack_ip_addresses, ip_addresses)
-
-if __name__=="__main__":
+def main():
 
     usage = """usage: python generate_ansible_inventory_from_aws.py
-    --stackname=<aws_cloudformation_stack_name>
-    --targetfile=<ansible_inventory_target_file_path>
-    --dumpinventory=true|false
-    """
+        --stackname=<aws_cloudformation_stack_name>
+        --targetfile=<ansible_inventory_target_file_path>
+        --dumpinventory=true|false
+        """
 
     parser = OptionParser(usage=usage)
 
@@ -103,81 +41,97 @@ if __name__=="__main__":
         print("You must specify --stackname=<stack_name>")
         sys.exit(1)
 
-    print "Getting inventory from AWS ... (may take a few minutes)"
-    ec2Inventory = ec2.Ec2Inventory()
+    pool_dns_addresses = generate_pools(opts.stackname, opts.dumpinventory)
 
-    # print "Refreshing inventory from AWS ... (may take a few minutes)"
-    ec2Inventory.do_api_calls_update_cache()
-
-    # the inventory will contain all the hosts for the given cloudformation stack
-    # under a key that looks like tag_aws_cloudformation_stack_name_TLeydenTestCluster
-    cloud_formation_stack_key = "tag_aws_cloudformation_stack_name_{}".format(opts.stackname)
-    got_stack_ip_addresses = False
-    for i in xrange(10):
-        try:
-            print("Getting stack ip addresses for {}".format(cloud_formation_stack_key))
-            cloud_formation_stack_ip_addresses = ec2Inventory.inventory[cloud_formation_stack_key]
-            got_stack_ip_addresses = True
-            break
-        except Exception as e:
-            print("Couldn't get stack ip addresses due to exception: {}.  Going to retry in a few seconds".format(e))
-            print "Getting inventory from AWS ... (may take a few minutes)"
-            ec2Inventory.do_api_calls_update_cache()
-    if not got_stack_ip_addresses:
-        raise Exception("Could not find ip addresses for {}".format(cloud_formation_stack_key))
-
-    saw_all_cloudformation_instances = False
-    for i in xrange(10):
-
-        # get all hosts for that cloudformation stack + uniquify
-        if opts.dumpinventory:
-            print "Inventory: {}".format(json.dumps(ec2Inventory.inventory, indent=4))
-
-        # it's important that these are in the _same order_ as they are generated in generate_clusters_from_pool.py
-        # so that things "line up" and the right machines are used for the role type as specified in the
-        # initial call to create_and_instantiate_cluster.py.  It's a bit of a hack, and very brittle, and should
-        # be reworked to explicitly record the machine size somewhere, or assert that the vm's spun up
-        # all have the identical sizes.
-        couchbase_server_ip_addresses = []
-        if "tag_Type_couchbaseserver" in ec2Inventory.inventory:
-            couchbase_server_ip_addresses = ec2Inventory.inventory["tag_Type_couchbaseserver"]
-            couchbase_server_ip_addresses = filter_cloud_formation(cloud_formation_stack_ip_addresses, couchbase_server_ip_addresses)
-        print("couchbase_server_ip_addresses: {}".format(couchbase_server_ip_addresses))
-
-        sync_gateway_ip_addresses = []
-        if "tag_Type_syncgateway" in ec2Inventory.inventory:
-            sync_gateway_ip_addresses = ec2Inventory.inventory["tag_Type_syncgateway"]
-            sync_gateway_ip_addresses = filter_cloud_formation(cloud_formation_stack_ip_addresses, sync_gateway_ip_addresses)
-        print("sync_gateway_ip_addresses: {}".format(sync_gateway_ip_addresses))
-
-        load_generator_ip_addresses = []
-        if "tag_Type_gateload" in ec2Inventory.inventory:
-            load_generator_ip_addresses = ec2Inventory.inventory["tag_Type_gateload"]
-            load_generator_ip_addresses = filter_cloud_formation(cloud_formation_stack_ip_addresses, load_generator_ip_addresses)
-        print("load_generator_ip_addresses: {}".format(load_generator_ip_addresses))
-
-        # get ip addresses of [u'54.242.119.83', u'54.234.207.138', u'54.209.218.97', u'54.242.55.56', u'54.242.29.78', u'52.90.171.161', u'54.175.81.204']
-        pool_ip_addresses = couchbase_server_ip_addresses + sync_gateway_ip_addresses + load_generator_ip_addresses
-        print("pool_ip_addresses: {}".format(pool_ip_addresses))
-
-        if len(pool_ip_addresses) == len(cloud_formation_stack_ip_addresses):
-            saw_all_cloudformation_instances = True
-            break
-        else:
-            print "Waiting for more instances to show up.  Getting inventory from AWS ... (may take a few minutes)"
-            ec2Inventory.do_api_calls_update_cache()
-
-    if not saw_all_cloudformation_instances:
-        raise Exception("Did not see all cloudformation instances: {}".format(cloud_formation_stack_ip_addresses))
-
-    print("Waiting until all instances are in the running state")
-    wait_until_ip_addresses_in_running_state(pool_ip_addresses, ec2Inventory)
-    print("All instances are now in the running state")
-    
-    # convert to dns names
-    pool_dns_addresses = lookup_dns_names(ec2Inventory.inventory, pool_ip_addresses)
+    print("pool_dns_addresses: {}".format(pool_dns_addresses))
 
     write_to_file(pool_dns_addresses, opts.targetfile)
 
     print "Generated {}".format(opts.targetfile)
 
+def generate_pools(stackname, dumpinventory):
+
+    # find all ec2 instance ids for stack
+    instance_ids_for_stack = get_instance_ids_for_stack(stackname)
+
+    # get list of instance objects from ids
+    instances_for_stack = lookup_instances_from_ids(instance_ids_for_stack)
+
+    # wait until stack ec2 instances are all in running state
+    wait_until_state(instances_for_stack, instance_ids_for_stack, "running")
+
+    # get public_dns_name for all instances
+    return get_public_dns_names(instances_for_stack)
+
+def get_instance_ids_for_stack(stackname):
+    """
+    For a given cloudformation stack, return all of the instance ids, eg
+    ["i-f1430877", "i-g3430877", ...]
+    """
+    instance_ids_for_stack = []
+    region = cloudformation.connect_to_region(DEFAULT_REGION)
+    stack_resources = region.describe_stack_resources(stackname)
+    for stack_resource in stack_resources:
+        if stack_resource.resource_type == "AWS::EC2::Instance":
+            instance_ids_for_stack.append(stack_resource.physical_resource_id)
+    return instance_ids_for_stack
+
+def lookup_instances_from_ids(instance_ids):
+    """
+    Given an array of instance ids, lookup the instance objects
+    which will have all the metadata
+    """
+    instances = []
+    region = ec2.connect_to_region(DEFAULT_REGION)
+    reservations = region.get_all_instances(instance_ids)
+    for reservation in reservations:
+        for instance in reservation.instances:
+            instances.append(instance)
+    return instances
+
+def wait_until_state(instances, instance_ids, state):
+
+    """
+    Wait until all instances are in the given state.  The instance_ids are
+    passed in case the instance objects need to be refreshed from AWS
+    """
+    for x in xrange(50):
+
+        all_instances_in_state = True
+        for instance in instances:
+            if instance.state != state:
+                all_instances_in_state = False
+
+        # if all instances were in the given state, we're done
+        if all_instances_in_state:
+            return
+
+        # otherwise ..
+        print("Not all instances in {} were in state {}.  Waiting and will retry.. Iteration: {}".format(instance_ids, state, x))
+        time.sleep(5)
+        instances = lookup_instances_from_ids(instance_ids)
+
+    raise Exception("Gave up waiting for instances {} to reach state {}".format(instance_ids, state))
+
+def get_public_dns_names(instances):
+
+    return [instance.public_dns_name for instance in instances]
+
+def write_to_file(cloud_formation_stack_dns_addresses, filename):
+    """
+    {
+        "ips": [
+	        "ec2-54-242-119-83.compute-1.amazonaws.com",
+	        "ec2-54-242-119-84.compute-1.amazonaws.com",
+        ]
+    }
+    """
+    output_dictionary = {"ips": cloud_formation_stack_dns_addresses}
+
+    with open(filename, 'w') as target:
+        target.truncate()
+        target.write(json.dumps(output_dictionary, sort_keys=True, indent=4, separators=(',', ': ')))
+
+if __name__=="__main__":
+
+    main()
