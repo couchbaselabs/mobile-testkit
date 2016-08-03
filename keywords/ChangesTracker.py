86750d8fb86ebc7cedf0d65ea72a671706324cce
Seth Rosetter <seth.rosetter@gmail.com>
2016-08-02 17:24:16 -0700

    Add ChangeTracker for async control for changes tests, add sanity load balancer test

---
+++ keywords/ChangesTracker.py
@@ -0,0 +1,108 @@
+import json
+import time
+import logging
+
+import requests
+
+from keywords.MobileRestClient import get_auth_type
+from keywords.constants import AuthType
+from keywords.utils import log_r
+from keywords.utils import log_info
+
+class ChangesTracker:
+
+    def __init__(self, url, db, auth):
+        self.processed_changes = {}
+        self.endpoint = "{}/{}".format(url, db)
+        self.auth = auth
+
+        self.cancel = False
+
+    def process_changes(self, results):
+        """
+        Add each doc from longpoll changes results to the processed changes list in the following format:
+        { "doc_id": [ {"rev": "rev1"}, {"rev", "rev2"}, ...] }
+        """
+        for doc in results:
+            if len(doc["changes"]) > 0:
+                if doc["id"] in self.processed_changes:
+                    # doc has already been seen in the changes feed,
+                    # append new changes to revs accociated with that id
+                    revs_list = self.processed_changes[doc["id"]]
+                    revs_list = revs_list.extend(doc["changes"])
+                    self.processed_changes[doc["id"]] = revs_list
+                else:
+                    # Stored the doc with the list of rev changes
+                    self.processed_changes[doc["id"]] = doc["changes"]
+
+    def start(self):
+
+        auth_type = get_auth_type(self.auth)
+        current_seq_num = 0
+
+        while not self.cancel:
+
+            log_info("self.cancel: {}".format(self.cancel))
+
+            data = {
+                "feed": "longpoll",
+                "style": "all_docs",
+                "timeout": 1000,
+                "since": current_seq_num
+            }
+
+            log_info("Making Request")
+
+            if auth_type == AuthType.session:
+                resp = requests.post("{}/_changes".format(self.endpoint), data=json.dumps(data), cookies=dict(SyncGatewaySession=self.auth[1]), stream=True)
+            elif auth_type == AuthType.http_basic:
+                resp = requests.post("{}/_changes".format(self.endpoint), data=json.dumps(data), auth=self.auth, stream=True)
+            else:
+                resp = requests.post("{}/_changes".format(self.endpoint), data=json.dumps(data), stream=True)
+
+            log_r(resp)
+            resp.raise_for_status()
+            resp_obj = resp.json()
+
+            # log_info("RESP_OBJ: {}".format(resp_obj))
+
+            self.process_changes(resp_obj["results"])
+            current_seq_num = resp_obj["last_seq"]
+
+    def stop(self):
+        log_info("Closing _changes feed ...")
+        self.cancel = True
+
+    def wait_until(self, expected_docs, timeout=30):
+
+        start = time.time()
+        while True:
+            if time.time() - start > timeout:
+                log_info("ChangeTracker.wait_until: TIMEOUT")
+                return False
+
+            # Check that the expected docs exist in the the
+            missing_docs = []
+            for doc in expected_docs:
+                if doc["id"] not in self.processed_changes:
+                    # doc_id not found in changes
+                    missing_docs.append(doc)
+                else:
+                    rev_found = False
+                    for rev in self.processed_changes[doc["id"]]:
+                        if rev["rev"] == doc["rev"]:
+                            # Found rev in changes feed, continue with the next dox
+                            log_info("FOUND id: {}, rev: {}".format(doc["id"], doc["rev"]))
+                            rev_found = True
+
+                    if not rev_found:
+                        missing_docs.append(doc)
+
+            if len(missing_docs) == 0:
+                log_info(":) Saw all docs in the changes feed for ({})!".format(self.auth))
+
+                return True
+
+            log_info("Docs missing from changes feed: {}".format(missing_docs))
+
+            time.sleep(1)
--- testsuites/syncgateway/functional/2sg_1cbs_1lbs/load_balancer_scenarios.py
+++ testsuites/syncgateway/functional/2sg_1cbs_1lbs/load_balancer_scenarios.py
@@ -1,26 +1,51 @@
+import time
+import concurrent.futures
+
 from keywords.utils import log_info
 from keywords.MobileRestClient import MobileRestClient
+from keywords.ChangesTracker import ChangesTracker
+
 
 def test_load_balance_sanity(cluster_config):
     log_info(cluster_config)
 
     admin_sg_one = cluster_config["sync_gateways"][0]["admin"]
+    sg_one_url = cluster_config["sync_gateways"][0]["public"]
     lb_url = cluster_config["load_balancers"][0]
 
     sg_db = "db"
-    num_docs = 20000
+    num_docs = 1000
     sg_user_name = "seth"
     sg_user_password = "password"
+    channels = ["ABC", "CBS"]
 
     client = MobileRestClient()
 
-    user = client.create_user(admin_sg_one, sg_db, sg_user_name, sg_user_password)
+    user = client.create_user(admin_sg_one, sg_db, sg_user_name, sg_user_password, channels=channels)
     session = client.create_session(admin_sg_one, sg_db, sg_user_name)
 
     log_info(user)
     log_info(session)
 
     log_info("Adding docs to the load balancer ...")
-    docs = client.add_docs(lb_url, sg_db, num_docs, "test_doc", auth=session)
 
-    log_info(docs)
+    ct = ChangesTracker(url=lb_url, db=sg_db, auth=session)
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        log_info("Starting ...")
+        executor.submit(ct.start)
+        log_info("Adding docs ...")
+        add_docs_task = executor.submit(client.add_docs, lb_url, sg_db, num_docs, "test_doc", channels=channels, auth=session)
+
+        docs = add_docs_task.result()
+
+        log_info("Adding docs done")
+        wait_for_changes = executor.submit(ct.wait_until, docs)
+
+        if wait_for_changes.result():
+            log_info("Stopping ...")
+            log_info("Found all docs ...")
+            executor.submit(ct.stop)
+        else:
+           executor.submit(ct.stop)
+           raise Exception("Could not find all changes in feed before timeout!!")
