368317cb3598e1a415c1bd88438956de76de8f2f
Seth Rosetter <seth.rosetter@gmail.com>
2016-08-22 09:43:43 -0700

    Add support for provisioning CBS 4.6 and Sync Gateway 1.3.1

--- libraries/provision/install_sync_gateway.py
+++ libraries/provision/install_sync_gateway.py
@@ -22,7 +22,8 @@ class SyncGatewayConfig:
             "1.1.1",
             "1.2.0",
             "1.2.1",
-            "1.3.0"
+            "1.3.0",
+            "1.3.1"
         ]
 
         self.commit = commit
