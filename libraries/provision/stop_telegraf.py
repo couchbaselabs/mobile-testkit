389287258743181658b0177bde6a6a2de925f0b2
Andrew Reslan <andrew@couchbase.com>
2016-09-15 16:44:13 +0100

    Adds stats collection to SG erf cluster and write as time-series data to influxdb

---
+++ libraries/provision/stop_telegraf.py
@@ -0,0 +1,17 @@
+import os
+
+from ansible_runner import AnsibleRunner
+
+
+if __name__ == "__main__":
+    usage = "usage: python stop_telegraf.py"
+
+    try:
+        cluster_config = os.environ["CLUSTER_CONFIG"]
+    except KeyError as ke:
+        print ("Make sure CLUSTER_CONFIG is defined and pointing to the configuration you would like to run against")
+        raise KeyError("CLUSTER_CONFIG not defined. Unable to stop telegraf collectors.")
+
+    ansible_runner = AnsibleRunner()
+    status = ansible_runner.run_ansible_playbook("stop-telegraf.yml")
+    assert status == 0, "Failed to stop telegraf collectors"
