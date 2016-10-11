bab8b79ecb01f67099d130cf0aa86096230792e4
Seth Rosetter <seth.rosetter@gmail.com>
2016-10-11 13:26:52 -0700

    Split into multiple lines

--- keywords/MobileRestClient.py
+++ keywords/MobileRestClient.py
@@ -991,7 +991,9 @@ class MobileRestClient:
         auth_type = get_auth_type(auth)
 
         if auth_type == AuthType.session:
-            resp = self._session.post("{}/{}/_bulk_docs".format(url, db), data=json.dumps(request_body), cookies=dict(SyncGatewaySession=auth[1]))
+            resp = self._session.post("{}/{}/_bulk_docs".format(url, db),
+                                      data=json.dumps(request_body),
+                                      cookies=dict(SyncGatewaySession=auth[1]))
         elif auth_type == AuthType.http_basic:
             resp = self._session.post("{}/{}/_bulk_docs".format(url, db), data=json.dumps(request_body), auth=auth)
         else:
