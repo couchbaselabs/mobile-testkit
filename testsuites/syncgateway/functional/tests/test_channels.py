6a7720311c7483e3caa7ffb9efbd9177ef565a55
Seth Rosetter <seth.rosetter@gmail.com>
2016-12-07 15:58:36 -0800

    Add test for channel remove / add for docs

--- testsuites/syncgateway/functional/tests/test_channels.py
+++ testsuites/syncgateway/functional/tests/test_channels.py
@@ -1,9 +1,13 @@
+import time
+
 import pytest
 
 from keywords.utils import log_info
 from libraries.testkit.cluster import Cluster
 from keywords.MobileRestClient import MobileRestClient
 from keywords.SyncGateway import sync_gateway_config_path_for_mode
+
+import keywords.exceptions
 
 
 @pytest.mark.sanity
@@ -32,7 +36,7 @@ def test_remove_add_channels_to_doc(params_from_base_test_setup, sg_conf_name):
     # TODO convert this to userinfo.UserInfo once backfill tests are merged
     admin_user_name = "admin"
     admin_user_pass = "pass"
-    admin_user_channels = ["A"]
+    admin_user_channels = ["A", "B"]
 
     a_user_name = "a_user"
     a_user_pass = "pass"
@@ -60,27 +64,93 @@ def test_remove_add_channels_to_doc(params_from_base_test_setup, sg_conf_name):
     # 'a_user' should get 50 'a_doc_*' doc and 1 '_user/a_user' doc
     assert len(a_user_changes["results"]) == 51
 
-    # Use admin user to update the docs to remove 'A' from the channels property on the doc
-    client.update_docs(url=sg_url, db=sg_db, docs=a_docs, number_updates=1, auth=admin_user_auth, channels=[])
+    ###########################
+    # Remove Channels from doc
+    ###########################
 
-    # TODO: remove sleep
-    import time
-    time.sleep(5)
+    # Copy a_docs_id_rev to dictionary to scratch off values
+    remove_docs_scratch_off = a_docs_id_rev.copy()
+    assert len(remove_docs_scratch_off) == 50
 
-    # Get changes for 'a_user'
-    a_user_changes = client.get_changes(url=sg_url, db=sg_db, since=a_user_changes["last_seq"], auth=a_user_auth, feed="normal")
+    # Use admin user to update the docs to remove 'A' from the channels property on the doc and add 'B'
+    client.update_docs(url=sg_url, db=sg_db, docs=a_docs, number_updates=1, auth=admin_user_auth, channels=['B'])
 
-    # Loop over changes found and perform the following
-    #   1. Check that the docs is flagged with 'removed'
-    #   2. Cross off the doc fromt the the 'a_docs_id_rev'
-    for change in a_user_changes["results"]:
+    # Longpoll loop requires due to the delay that changes take to permeate to the client
+    changes_timeout = 10
+    start = time.time()
+    last_seq = a_user_changes["last_seq"]
+    while True:
 
-        assert change["removed"] == ["A"]
-        # This will blow up if any change is not found in that dictionary
-        del a_docs_id_rev[change["id"]]
+        # If take longer than 10 seconds, fail the test
+        if time.time() - start > changes_timeout:
+            raise keywords.exceptions.TimeoutException("Could not find all expected docs in changs feed")
 
-    # Make sure all docs are crossed off
-    assert a_docs_id_rev == {}
+        # We found everything, exit loop!
+        if remove_docs_scratch_off == {}:
+            log_info("All expected docs found to be removed")
+            break
 
-    import pdb
-    pdb.set_trace()
+        # Get changes for 'a_user' from last_seq
+        a_user_changes = client.get_changes(url=sg_url, db=sg_db, since=last_seq, auth=a_user_auth, timeout=10)
+        assert len(a_user_changes["results"]) > 0
+
+        # Loop over changes found and perform the following
+        #   1. Check that the docs is flagged with 'removed'
+        #   2. Cross off the doc fromt the the 'remove_docs_scratch_off'
+        for change in a_user_changes["results"]:
+            assert change["removed"] == ["A"]
+            assert change["changes"][0]["rev"].startswith("2-")
+            # This will blow up if any change is not found in that dictionary
+            del remove_docs_scratch_off[change["id"]]
+
+        # Update last_seq
+        last_seq = a_user_changes["last_seq"]
+
+    # Issue changes request from 'last_seq' and verify that the changes are up to date and returns no results
+    a_user_changes = client.get_changes(url=sg_url, db=sg_db, since=last_seq, auth=a_user_auth, feed="normal")
+    assert len(a_user_changes["results"]) == 0
+
+    #########################
+    # Add Channels to doc
+    #########################
+
+    # Copy the a_docs_id_rev dictionary for scratch ovee
+    add_docs_scratch_off = a_docs_id_rev.copy()
+    assert len(add_docs_scratch_off) == 50
+
+    # Use admin user to update the docs to add ['A'] back to document channels
+    client.update_docs(url=sg_url, db=sg_db, docs=a_docs, number_updates=1, auth=admin_user_auth, channels=["A"])
+
+    # Longpoll loop requires due to the delay that changes take to permeate to the client
+    changes_timeout = 10
+    start = time.time()
+    last_seq = a_user_changes["last_seq"]
+    while True:
+
+        # If take longer than 10 seconds, fail the test
+        if time.time() - start > changes_timeout:
+            raise keywords.exceptions.TimeoutException("Could not find all expected docs in changs feed")
+
+        # We found everything, exit loop!
+        if add_docs_scratch_off == {}:
+            log_info("All expected docs found to be removed")
+            break
+
+        # Get changes for 'a_user' from last_seq
+        a_user_changes = client.get_changes(url=sg_url, db=sg_db, since=last_seq, auth=a_user_auth, timeout=10)
+        assert len(a_user_changes["results"]) > 0
+
+        # Loop over changes found and perform the following
+        #   1. Check that the docs has a 3rd gen rev prefix
+        #   2. Cross off the doc fromt the the 'add_docs_scratch_off'
+        for change in a_user_changes["results"]:
+            assert change["changes"][0]["rev"].startswith("3-")
+            # This will blow up if any change is not found in that dictionary
+            del add_docs_scratch_off[change["id"]]
+
+        # Update last_seq
+        last_seq = a_user_changes["last_seq"]
+
+    # Issue changes request from 'last_seq' and verify that the changes are up to date and returns no results
+    a_user_changes = client.get_changes(url=sg_url, db=sg_db, since=last_seq, auth=a_user_auth, feed="normal")
+    assert len(a_user_changes["results"]) == 0
