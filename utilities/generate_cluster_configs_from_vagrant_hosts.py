2b1e20c451912d9511a97b4d771c60e9fb97e742
raghusarangapani <raghu.sarangapani@couchbase.com>
2017-01-04 13:44:12 -0800

    Adding the changes for vagrant ethernet public network (#832)
    * Adding the changes for vagrant ethernet public network
    
    * Addressing Seth’s comments
    
    * Adding more sanity checks
    
    * Fixed indentation errors
    
    * Moved the validation back into the generate_cluster_configs_from_vagrant method
    
    * Fixed typos

--- utilities/generate_cluster_configs_from_vagrant_hosts.py
+++ utilities/generate_cluster_configs_from_vagrant_hosts.py
@@ -10,26 +10,34 @@ import os
 from libraries.utilities.generate_clusters_from_pool import generate_clusters_from_pool
 
 
-def generate_cluster_configs_from_vagrant(private_network, public_network):
+def generate_cluster_configs_from_vagrant(private_network, public_network, public_network_ethernet):
     """
     1. Gets the status for a running vagrant vm set.
     2. Uses the host name to look up the ip allocated to each vagrant vm instance
     3. Uses this IP list to build a pool.json file and generate the cluster configurations
     """
 
-    if private_network and public_network:
-        raise ProvisioningError("Invalid private_network and public_network flags")
+    # Check if only one of the options is set, private_network or public_network or public_network_ethernet
+    if private_network and (public_network or public_network_ethernet):
+        raise ProvisioningError("Invalid private_network and public_network/public_network_ethernet flags")
+    elif public_network and (private_network or public_network_ethernet):
+        raise ProvisioningError("Invalid public_network and private_network/public_network_ethernet flags")
+    elif public_network_ethernet and (public_network or private_network):
+        raise ProvisioningError("Invalid public_network_ethernet and public_network/private_network flags")
 
-    if not private_network and not public_network:
-        raise ProvisioningError("Invalid private_network and public_network flags")
+    # Check if none of the options are set
+    if not private_network and not public_network and not public_network_ethernet:
+        raise ProvisioningError("Invalid private_network, public_network and public_network_ethernet flags")
 
     cwd = os.getcwd()
 
     # Change directory to where the appropriate Vagrantfile lives
     if private_network:
         os.chdir("vagrant/private_network")
-    else:
+    elif public_network:
         os.chdir("vagrant/public_network")
+    else:
+        os.chdir("vagrant/public_network_ethernet")
 
     v = vagrant.Vagrant()
     status = v.status()
@@ -73,6 +81,11 @@ if __name__ == "__main__":
 
        usage: python generate_cluster_configs_from_vagrant_hosts.py
        --public-network
+
+       or
+
+       usage: python generate_cluster_configs_from_vagrant_hosts.py
+       --public-network-ethernet
        """
 
     parser = OptionParser(usage=usage)
@@ -85,8 +98,12 @@ if __name__ == "__main__":
                       action="store_true", dest="public_network", default=False,
                       help="Use Vagrant public network (Bridged)")
 
+    parser.add_option("", "--public-network-ethernet",
+                      action="store_true", dest="public_network_ethernet", default=False,
+                      help="Use Vagrant public ethernet network (Bridged)")
+
     arg_parameters = sys.argv[1:]
 
     (opts, args) = parser.parse_args(arg_parameters)
 
-    generate_cluster_configs_from_vagrant(opts.private_network, opts.public_network)
+    generate_cluster_configs_from_vagrant(opts.private_network, opts.public_network, opts.public_network_ethernet)
