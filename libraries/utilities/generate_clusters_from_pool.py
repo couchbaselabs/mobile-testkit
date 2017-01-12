55ab6c1bc90d6340a6d57afd6713d244299b614b
Seth Rosetter <seth.rosetter@gmail.com>
2017-01-12 10:57:54 -0800

    Remove dupe cluster def

--- libraries/utilities/generate_clusters_from_pool.py
+++ libraries/utilities/generate_clusters_from_pool.py
@@ -411,7 +411,6 @@ def generate_clusters_from_pool(pool_file):
         ClusterDef("1sg_1ac_1cbs_1lgs", num_sgs=1, num_acs=1, num_cbs=1, num_lgs=1, num_lbs=0),
         ClusterDef("2sg_2ac_3cbs_1lgs", num_sgs=2, num_acs=2, num_cbs=3, num_lgs=1, num_lbs=0),
         ClusterDef("2sg_2ac_3cbs_2lgs", num_sgs=2, num_acs=2, num_cbs=3, num_lgs=2, num_lbs=0),
-        ClusterDef("4sg_2ac_3cbs_4lgs", num_sgs=4, num_acs=2, num_cbs=3, num_lgs=4, num_lbs=0),
         # Perf Mini Matrix
         ClusterDef("1sg_1ac_3cbs_1lgs", num_sgs=1, num_acs=1, num_cbs=3, num_lgs=1, num_lbs=0),
         ClusterDef("1sg_2ac_3cbs_1lgs", num_sgs=1, num_acs=2, num_cbs=3, num_lgs=1, num_lbs=0),
