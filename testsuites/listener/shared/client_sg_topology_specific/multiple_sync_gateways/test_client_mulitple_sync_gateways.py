a59a7913b642e25779f3560d442d8e56a51f053b
Seth Rosetter <seth.rosetter@gmail.com>
2017-01-19 17:51:42 -0800

    Simplify client_multiple sgs test

--- testsuites/listener/shared/client_sg_topology_specific/multiple_sync_gateways/test_client_mulitple_sync_gateways.py
+++ testsuites/listener/shared/client_sg_topology_specific/multiple_sync_gateways/test_client_mulitple_sync_gateways.py
@@ -50,7 +50,7 @@ def test_listener_two_sync_gateways(setup_client_syncgateway_test):
     log_info("Couchbase Server url: {}".format(cb_server_url))
 
     c = cluster.Cluster(cluster_config)
-    sg_config_path = sync_gateway_config_path_for_mode("listener_tests/listener_tests", sg_mode)
+    sg_config_path = sync_gateway_config_path_for_mode("listener_tests/multiple_sync_gateways", sg_mode)
     c.reset(sg_config_path=sg_config_path)
 
     ls_db_one = "ls_db1"
@@ -66,79 +66,11 @@ def test_listener_two_sync_gateways(setup_client_syncgateway_test):
 
     client = MobileRestClient()
 
-    # Delete dbs on sync_gateways
-    client.delete_databases(url=sg_one_admin_url)
-    client.delete_databases(url=sg_two_admin_url)
+    # Delete sg_db2 on sync_gateway 1
+    client.delete_database(url=sg_one_admin_url, name=sg_db_two)
 
-    if sg_mode == "di":
-        accel_url = topology["sg_accels"][0]
-        client.delete_databases(url=accel_url)
-
-    # Create backing stores for each db
-    cb_server = CouchbaseServer(cb_server_url)
-    cb_server.delete_buckets()
-
-    cb_server.create_buckets(["data-bucket-1", "data-bucket-2", "index-bucket-1", "index-bucket-2"])
-
-    if sg_mode == "di":
-
-        # Create dbs on sync_gateway
-        client.create_database(
-            url=sg_one_admin_url,
-            sync_gateway_mode=sg_mode,
-            name=sg_db_one,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-1",
-            index_bucket_name="index-bucket-1",
-            is_index_writer=False
-        )
-        client.create_database(
-            url=sg_two_admin_url,
-            name=sg_db_two,
-            sync_gateway_mode=sg_mode,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-2",
-            index_bucket_name="index-bucket-2",
-            is_index_writer=False
-        )
-
-        # Create dbs on sg_accel
-        client.create_database(
-            url=accel_url,
-            sync_gateway_mode=sg_mode,
-            name=sg_db_one,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-1",
-            index_bucket_name="index-bucket-1",
-            is_index_writer=True
-        )
-        client.create_database(
-            url=accel_url,
-            name=sg_db_two,
-            sync_gateway_mode=sg_mode,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-2",
-            index_bucket_name="index-bucket-2",
-            is_index_writer=True
-        )
-
-    else:
-
-        # Create dbs on sync_gateway
-        client.create_database(
-            url=sg_one_admin_url,
-            sync_gateway_mode=sg_mode,
-            name=sg_db_one,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-1"
-        )
-        client.create_database(
-            url=sg_two_admin_url,
-            name=sg_db_two,
-            sync_gateway_mode=sg_mode,
-            server_url=cb_server_url,
-            bucket_name="data-bucket-2",
-        )
+    # Delete sg_db1 on sync_gateway 2
+    client.delete_database(url=sg_two_admin_url, name=sg_db_one)
 
     import pdb
     pdb.set_trace()
