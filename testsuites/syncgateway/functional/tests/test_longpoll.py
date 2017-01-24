21a5bba7de1724daab89b4c27ff2b117e27c73d7
Seth Rosetter <seth.rosetter@gmail.com>
2017-01-23 18:35:37 -0800

    Fix test longpoll tests

--- testsuites/syncgateway/functional/tests/test_longpoll.py
+++ testsuites/syncgateway/functional/tests/test_longpoll.py
@@ -405,9 +405,14 @@ def test_longpoll_awaken_channels(params_from_base_test_setup, sg_conf_name):
     ############################################################
 
     # Get starting sequence of docs, use the last seq to progress past any _user docs.
-    adam_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="normal", auth=adam_auth)
-    traun_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="normal", auth=traun_auth)
-    andy_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="normal", auth=andy_auth)
+    adam_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="longpoll", auth=adam_auth)
+    traun_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="longpoll", auth=traun_auth)
+    andy_changes = client.get_changes(url=sg_url, db=sg_db, since=0, feed="longpoll", auth=andy_auth)
+
+    # Make sure _user docs shows up in the changes feed
+    assert len(adam_changes["results"]) == 1 and adam_changes["results"][0]["id"] == "_user/adam"
+    assert len(traun_changes["results"]) == 1 and traun_changes["results"][0]["id"] == "_user/traun"
+    assert len(andy_changes["results"]) == 1 and andy_changes["results"][0]["id"] == "_user/andy"
 
     with concurrent.futures.ProcessPoolExecutor() as ex:
 
