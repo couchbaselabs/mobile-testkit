84c38a838acffada2284deb77fc68322f0b4e1d2
Seth Rosetter <seth.rosetter@gmail.com>
2017-01-26 16:53:55 -0800

    Add new perf cluster config

--- libraries/utilities/generate_clusters_from_pool.py
+++ libraries/utilities/generate_clusters_from_pool.py
@@ -420,6 +420,7 @@ def generate_clusters_from_pool(pool_file):
         ClusterDef("3sg_2ac_3cbs_3lgs", num_sgs=3, num_acs=2, num_cbs=3, num_lgs=3, num_lbs=0),
         ClusterDef("4sg_1ac_3cbs_4lgs", num_sgs=4, num_acs=1, num_cbs=3, num_lgs=4, num_lbs=0),
         ClusterDef("4sg_2ac_3cbs_4lgs", num_sgs=4, num_acs=2, num_cbs=3, num_lgs=4, num_lbs=0),
+        ClusterDef("4sg_8ac_3cbs_4lgs", num_sgs=4, num_acs=8, num_cbs=3, num_lgs=4, num_lbs=0),
         ClusterDef("8sg_1ac_3cbs_8lgs", num_sgs=8, num_acs=1, num_cbs=3, num_lgs=8, num_lbs=0),
         ClusterDef("8sg_2ac_3cbs_8lgs", num_sgs=8, num_acs=2, num_cbs=3, num_lgs=8, num_lbs=0),
         # End Perf Mini Matrix
