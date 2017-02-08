dc9913a02bdf3853a62a17fcfdb34482e4754344
Traun Leyden <tleyden@couchbase.com>
2017-02-07 20:49:23 -0800

    install_aws_credentials working w/ cli params

--- libraries/provision/install_aws_credentials.py
+++ libraries/provision/install_aws_credentials.py
@@ -5,10 +5,13 @@ from keywords.exceptions import ProvisioningError
 
 from keywords.utils import log_info
 
+import argparse
+
+
 # Install aws credentials onto machine in /root/.aws/credentials file.
 # Currently used by awslogs, which is a cloudwatch logs forwarder.
 
-def install_aws_credentials(cluster_config):
+def install_aws_credentials(cluster_config, aws_access_key_id, aws_secret_access_key):
 
     log_info("Installing aws credentials for cluster_config: {}".format(cluster_config))
 
@@ -17,8 +20,8 @@ def install_aws_credentials(cluster_config):
     status = ansible_runner.run_ansible_playbook(
         "install-aws-credentials.yml",
         extra_vars={
-            "aws_access_key_id": "foo",
-            "aws_secret_access_key": "bar",
+            "aws_access_key_id": aws_access_key_id,
+            "aws_secret_access_key": aws_secret_access_key,
         },
     )
     if status != 0:
@@ -26,7 +29,13 @@ def install_aws_credentials(cluster_config):
 
 
 if __name__ == "__main__":
-    usage = "usage: python install_aws_credentials.py"
+
+    usage = "usage: python install_aws_credentials.py --aws-access-key-id AKIAIYOURKEY --aws-secret-access-key uaLqkyoursecret"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--aws-access-key-id", help="aws access key id", required=True)
+    parser.add_argument("--aws-secret-access-key", help="aws secret access key", required=True)
+    args = parser.parse_args()
 
     try:
         cluster_conf = os.environ["CLUSTER_CONFIG"]
@@ -34,4 +43,4 @@ if __name__ == "__main__":
         print ("Make sure CLUSTER_CONFIG is defined and pointing to the configuration you would like to provision")
         raise KeyError("CLUSTER_CONFIG not defined. Unable to provision cluster.")
 
-    install_aws_credentials(cluster_conf)
+    install_aws_credentials(cluster_conf, args.aws_access_key_id, args.aws_secret_access_key)
