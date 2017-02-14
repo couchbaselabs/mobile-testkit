c9c309b6da57cf3d05f7bc995b02ed3fd9def900
Traun Leyden <tleyden@couchbase.com>
2017-02-13 16:20:19 -0800

    Retry sync gw profile collection several times
    In http://uberjenkins.sc.couchbase.com/view/Performance/job/sync-gateway-perf-test-copy-ro/73/, got this error
    
    15:57:03 Traceback (most recent call last):
    15:57:03   File "testsuites/syncgateway/performance/run_gateload_perf_test.py", line 203, in <module>
    15:57:03     gateload_params=gateload_params_from_args
    15:57:03   File "testsuites/syncgateway/performance/run_gateload_perf_test.py", line 96, in run_gateload_perf_test
    15:57:03     raise Exception("It appears that gateload did not finish successfully.  Check logs for details")

--- libraries/utilities/fetch_sync_gateway_profile.py
+++ libraries/utilities/fetch_sync_gateway_profile.py
@@ -1,30 +1,43 @@
 import os.path
 import shutil
+import time
 from libraries.provision.ansible_runner import AnsibleRunner
 from keywords.utils import log_info
 
 
 def fetch_sync_gateway_profile(cluster_config, folder_name):
 
-    try:
+    max_num_attempts = 20
 
-        ansible_runner = AnsibleRunner(config=cluster_config)
+    for attempt_number in max_num_attempts:
 
-        print("\n")
+        try:
 
-        print("Pulling sync_gateway profile ...")
-        # fetch logs from sync_gateway instances
-        status = ansible_runner.run_ansible_playbook("fetch-sync-gateway-profile.yml")
-        if status != 0:
-            raise Exception("Failed to fetch sync_gateway profile")
+            ansible_runner = AnsibleRunner(config=cluster_config)
 
-        # zip logs and timestamp
-        if os.path.isdir("/tmp/sync_gateway_profile"):
+            print("\n")
 
-            # Move perf logs to performance_results
-            shutil.move("/tmp/sync_gateway_profile", "testsuites/syncgateway/performance/results/{}/".format(folder_name))
+            print("Pulling sync_gateway profile.  Attempt #{}".format(attempt_number))
+            # fetch logs from sync_gateway instances
+            status = ansible_runner.run_ansible_playbook("fetch-sync-gateway-profile.yml")
+            if status != 0:
+                raise Exception("Failed to fetch sync_gateway profile")
 
-        print("\n")
+            # zip logs and timestamp
+            if os.path.isdir("/tmp/sync_gateway_profile"):
 
-    except Exception as e:
-        log_info("Exception trying to collect sync gateway profile: {}", e)
+                # Move perf logs to performance_results
+                shutil.move("/tmp/sync_gateway_profile", "testsuites/syncgateway/performance/results/{}/".format(folder_name))
+
+            print("\n")
+            return
+
+        except Exception as e:
+            log_info("Exception trying to collect sync gateway profile: {}", e)
+            if attempt_number < (max_num_attempts - 1):
+                log_info("Going to retry.  So far {}/{} attempts".format(attempt_number, max_num_attempts))
+                time.sleep(30)
+            else:
+                log_info("Exhausted attempts.  Giving up")
+
+        attempt_number += 1
