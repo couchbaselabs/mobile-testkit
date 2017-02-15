51b093cf0352ceee9f091c4f416b46ddbcdd1141
Seth Rosetter <seth.rosetter@gmail.com>
2017-02-15 11:45:58 -0800

    Feature/addition cluster configs (#931)
    * Add additional cluster topologies
    
    * Add the rest of the configs
    
    * Add a couple 6 server confs
    
    * Add one more config and make sure generate_clusters_from_pools test passes
    
    * Fix comment

--- mobile_testkit_tests/test_generate_clusters_from_pool.py
+++ mobile_testkit_tests/test_generate_clusters_from_pool.py
@@ -91,8 +91,8 @@ def test_generate_clusters_from_pool_ip_to_node(cleanup_folder, cluster_conf, nu
     generate_clusters_from_pool(ip_to_node_type_pool_file)
 
     # Verification
-    # mock_pool_ip_to_node_type.json will generate 50 ansible+json
-    assert len([name for name in os.listdir(cluster_config_dir)]) == 50
+    # mock_pool_ip_to_node_type.json will generate 42 ansible+json
+    assert len([name for name in os.listdir(cluster_config_dir)]) == 42
 
     # We will check 2 files for content 1cbs/1cbs.json
     config = configparser.ConfigParser()
