4a86cf6ed4164e44b664b6b503f4a502922cc84f
Traun Leyden <traun.leyden@gmail.com>
2017-02-16 17:58:36 -0800

    Collect inuse and alloc space heap profiles (#965)

--- libraries/provision/ansible/playbooks/files/sync_gateway_collect_profile.py
+++ libraries/provision/ansible/playbooks/files/sync_gateway_collect_profile.py
@@ -24,7 +24,7 @@ def run_command(command):
         raise Exception("{0} failed".format(command))
 
 
-def collect_profiles(results_directory, sg_binary_path, profile_types, format_types):
+def collect_profiles(results_directory, sg_binary_path, profile_types, format_types, heap_val_selection_types):
 
     # make sure raw profile gz files end up in results dir
     os.environ["PPROF_TMPDIR"] = results_directory
@@ -32,17 +32,32 @@ def collect_profiles(results_directory, sg_binary_path, profile_types, format_ty
     for profile_type in profile_types:
         for format_type in format_types:
             print("Collecting {0} profile in format {1}".format(profile_type, format_type))
-            out_filename = "{0}.{1}".format(profile_type, format_type)
-            dest_path = os.path.join(results_directory, out_filename)
-            cmd = "go tool pprof -{0} {1} {2}/{3} > {4}".format(
-                format_type,
-                sg_binary_path,
-                sg_pprof_url,
-                profile_type,
-                dest_path
-            )
-            print(cmd)
-            run_command(cmd)
+            if profile_type == "heap":
+                for heap_type in heap_val_selection_types:
+                    out_filename = "{}-{}.{}".format(profile_type, heap_type, format_type)
+                    dest_path = os.path.join(results_directory, out_filename)
+                    cmd = "go tool pprof -{} -{} {} {}/{} > {}".format(
+                        format_type,
+                        heap_type,
+                        sg_binary_path,
+                        sg_pprof_url,
+                        profile_type,
+                        dest_path
+                    )
+                    print(cmd)
+                    run_command(cmd)
+            else:
+                out_filename = "{}.{}".format(profile_type, format_type)
+                dest_path = os.path.join(results_directory, out_filename)
+                cmd = "go tool pprof -{} {} {}/{} > {}".format(
+                    format_type,
+                    sg_binary_path,
+                    sg_pprof_url,
+                    profile_type,
+                    dest_path
+                )
+                print(cmd)
+                run_command(cmd)
 
 
 def compress_and_copy(results_directory, final_results_directory):
@@ -60,7 +75,7 @@ def compress_and_copy(results_directory, final_results_directory):
     shutil.copy("{0}/{1}".format(results_directory, filename), final_results_directory)
 
 
-def collect_profile_loop(final_results_directory, profile_types, format_types, delay_secs):
+def collect_profile_loop(final_results_directory, profile_types, format_types, heap_val_selection_types, delay_secs):
 
     os.makedirs(final_results_directory)
 
@@ -82,6 +97,7 @@ def collect_profile_loop(final_results_directory, profile_types, format_types, d
                 sg_binary_path,
                 profile_types,
                 format_types,
+                heap_val_selection_types
             )
 
             compress_and_copy(results_directory, final_results_directory)
@@ -96,9 +112,9 @@ def collect_profile_loop(final_results_directory, profile_types, format_types, d
 
             time.sleep(delay_secs)
 
-        except:
+        except Exception as e:
 
-            print("Exception trying to collect profile.  Will sleep {} seconds and try again".format(delay_secs))
+            print("Exception trying to collect profile. {}  Will sleep {} seconds and try again".format(e, delay_secs))
             time.sleep(delay_secs)
 
 
@@ -122,6 +138,10 @@ if __name__ == "__main__":
     parser.add_argument("--profile-type-heap", help="Collect heap profile", action='store_true', default=False)
     parser.add_argument("--profile-type-goroutine", help="Collect goroutine profile", action='store_true', default=False)
     parser.add_argument("--delay-secs", help="How long to delay in between collections in seconds", default=(9 * 60))  # mins
+    parser.add_argument("--heap-inuse-space", help="Collect the heap in-use space", action='store_true', default=False)
+    parser.add_argument("--heap-alloc-space", help="Collect the heap alloc space", action='store_true', default=False)
+    parser.add_argument("--heap-inuse-objects", help="Collect the heap in-use objects", action='store_true', default=False)
+    parser.add_argument("--heap-alloc-objects", help="Collect the heap alloc objects", action='store_true', default=False)
 
     args = parser.parse_args()
 
@@ -129,6 +149,7 @@ if __name__ == "__main__":
 
     format_types = []
     profile_types = []
+    heap_val_selection_types = []
 
     if args.format_type_pdf:
         format_types.append("pdf")
@@ -148,9 +169,19 @@ if __name__ == "__main__":
     if len(profile_types) == 0:
         raise Exception("You must select at least one profile type")
 
+    if args.heap_inuse_space:
+        heap_val_selection_types.append("inuse_space")
+    if args.heap_alloc_space:
+        heap_val_selection_types.append("alloc_space")
+    if args.heap_inuse_objects:
+        heap_val_selection_types.append("inuse_objects")
+    if args.heap_alloc_objects:
+        heap_val_selection_types.append("alloc_objects")
+
     collect_profile_loop(
         main_final_results_directory,
         profile_types,
         format_types,
+        heap_val_selection_types,
         int(args.delay_secs),
     )
