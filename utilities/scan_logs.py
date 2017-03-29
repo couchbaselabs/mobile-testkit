ac26d22a5bfa2611669c0bba9a04f33bb3736f82
Seth Rosetter <seth.rosetter@gmail.com>
2017-03-29 14:34:26 -0700

    Update log scanner to unzip and look for all log files in a directory

--- utilities/scan_logs.py
+++ utilities/scan_logs.py
@@ -1,8 +1,59 @@
+import argparse
+import os
+import zipfile
+
 from keywords.utils import log_info
 from keywords.utils import log_error
+from keywords.exceptions import LogScanningError
 
 
-def scan_for_errors(error_strings, log_file_path):
+def gather_logs(directory):
+    """ 
+    1. Look for .zip files and unzip
+    2. Scan for sync gateway / accel logs
+    """
+
+    log_info('Looking in {} for sync_gateway / accel logs ...'.format(directory))
+
+    # Walk directory to get list of files
+    file_names = []
+    for root, dirs, files in os.walk(directory, topdown=True):
+        for f in files:
+            file_names.append(os.path.join(root, f))
+
+    # Extract / remove any .zip files
+    zip_files = [file_name for file_name in file_names if file_name.endswith('.zip')]
+    for zip_file in zip_files:
+        zip_file_extract_dir = zip_file.replace('.zip', '')
+        log_info('Unzipping: {}'.format(zip_file))
+        with zipfile.ZipFile(zip_file) as zf:
+            zf.extractall(zip_file_extract_dir)
+            os.remove(zip_file)
+
+    # Walk directory again to get list of files now that everything has been extracted
+    file_names = []
+    for root, dirs, files in os.walk(directory, topdown=True):
+        for f in files:
+            file_names.append(os.path.join(root, f))
+
+    # Get log files we would like to scan
+    log_files = [file_name for file_name in file_names if file_name.endswith('.log')]
+
+    # Scan the logs for panics and data races
+    log_info('Scanning logs {} logs ...'.format(len(log_files)))
+    issues_found = False
+    for log_file in log_files:
+        try:
+            scan_for_errors(log_file, ['panic', 'data race'])
+        except LogScanningError as lse:
+            log_info(lse.message)
+            issues_found = True
+
+    if issues_found:
+        raise LogScanningError('ERROR!!! Found panics or data races!!')
+
+
+def scan_for_errors(log_file_path, error_strings):
     """
     Scans a log file line by line for a provided array of words.
     We use this to look for errors, so we expect that no words will be found
@@ -12,9 +63,7 @@ def scan_for_errors(error_strings, log_file_path):
     """
 
     if type(error_strings) != list:
-        raise ValueError("'error_strings must be a list'")
-
-    log_info("Looking for {} in {} ...".format(error_strings, log_file_path))
+        raise ValueError('error_strings must be a list')
 
     # Scan each line in the log file for the words to search for
     with open(log_file_path) as f:
@@ -24,7 +73,13 @@ def scan_for_errors(error_strings, log_file_path):
                 # which handles the case where 'warning' will catch 'WARNING' and 'Warning', etc
                 if word.lower() in line.lower():
                     log_error(line)
-                    raise AssertionError("{} found!! Please review: {} ".format(word, log_file_path))
+                    raise LogScanningError('{} found!! Please review: {} '.format(word, log_file_path))
 
-    # No errors found
-    log_info("Scan complete. Did not find any error strings.")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path-to-log-dir', help='Directory containing the log files', required=True)
+    args = parser.parse_args()
+
+    gather_logs(args.path_to_log_dir)
