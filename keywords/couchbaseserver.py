031a5928cc12349f2c8ad79d909373b8c88aa098
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-04-01 22:05:27 -0700

    debug logs

--- keywords/couchbaseserver.py
+++ keywords/couchbaseserver.py
@@ -274,25 +274,15 @@ class CouchbaseServer:
             "flushEnabled": "1"
         }
 
-        # Create client an retry until KeyNotFound error is thrown
-        start = time.time()
-        while True:
-            if time.time() - start > keywords.constants.CLIENT_REQUEST_TIMEOUT:
-                raise Exception("TIMEOUT while trying to create server buckets.")
-            try:
-                resp = self._session.post("{}/pools/default/buckets".format(self.url), data=data)
-                log_info("resp: {}".format(resp))
-                log_info("resp.text: {}".format(resp.text))
-                log_r(resp)
-                resp.raise_for_status()
-                break
-            except HTTPError as h:
-                log_info("Error from server: Retrying ...", h)
-                time.sleep(1)
-                continue
-            except:
-                log_info("Error creating bucket")
-                raise
+        resp = ""
+        try:
+            resp = self._session.post("{}/pools/default/buckets".format(self.url), data=data)
+            log_r(resp)
+            resp.raise_for_status()
+        except:
+            log_info("resp code: {}".format(resp))
+            log_info("resp text: {}".format(resp.text))
+            raise
 
         # Create client an retry until KeyNotFound error is thrown
         start = time.time()
