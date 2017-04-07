a1b259272fb7b4292163e9f8ae250e0ebaf2d3e4
Seth Rosetter <seth.rosetter@gmail.com>
2017-04-07 13:22:23 -0700

    Add WIP travel Sample.NET

---
+++ testsuites/simulations/Sample.NET/Travel.NET/Travel.NET/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Couchbase.Lite;
+
+namespace Travel.NET
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Manager manager = Manager.SharedInstance;
+			Database airlineDb = manager.GetDatabase("airline");
+			Database routeDb = manager.GetDatabase("route");
+			Database airportDb = manager.GetDatabase("airport");
+			Database landmarkDb = manager.GetDatabase("landmark");
+			Database hotelDb = manager.GetDatabase("hotel");
+
+			var properties = new Dictionary<string, object> {
+				{ "title", "Couchbase Mobile"},
+				{ "sdk", "C#" }
+			};
+
+			Document document = database.CreateDocument();
+
+			document.PutProperties(properties);
+
+			Console.WriteLine($"Document ID :: {document.Id}");
+			Console.WriteLine($"Learning {document.GetProperty("title")} with {document.GetProperty("sdk")}");
+
+			string line = Console.ReadLine();
+		}
+	}
+}
