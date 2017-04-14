0b63aad0be192597835b781715bdf2a02312c4aa
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-04-13 17:13:17 -0700

    Removing CBSE reference

--- testsuites/listener/shared/client_sg/test_replication.py
+++ testsuites/listener/shared/client_sg/test_replication.py
@@ -1066,7 +1066,7 @@ def test_verify_open_revs_with_revs_limit_push_conflict(setup_client_syncgateway
 @pytest.mark.replication
 @pytest.mark.session
 def test_replication_with_session_cookie_short_ttl(setup_client_syncgateway_test):
-    """Regression test for https://issues.couchbase.com/browse/CBSE-3606
+    """Regression test for https://github.com/couchbaselabs/mobile-testkit/issues/1110
     1. SyncGateway Config with guest disabled = true and One user added (e.g. user1 / 1234)
     2. Create a new session on SGW for the user1 by using POST /_session.
        Capture the SyncGatewaySession cookie from the set-cookie in the response header.
