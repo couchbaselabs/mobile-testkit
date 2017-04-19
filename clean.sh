11c23040bd348564c91e324b16a649d37d53c858
Seth Rosetter <seth.rosetter@gmail.com>
2017-04-19 16:34:35 -0700

    Remove pytest sugar plugin

--- clean.sh
+++ clean.sh
@@ -4,5 +4,9 @@ echo "Cleaning repo"
 echo "Removing __pycache__/ ..."
 rm -rf __pycache__/
 
+echo "Removing mobile_testkit_tests/__pycache__/ and .cache ..."
+rm -rf mobile_testkit_tests/__pycache__/
+rm -rf mobile_testkit_tests/.cache/
+
 echo "Removing all .pyc files ..."
 find . -name "*.pyc" -exec rm -f {} \;
