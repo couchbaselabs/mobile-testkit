91de2a4490bdfc1d2e88f3606b505301fc41866f
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-04-26 10:50:23 -0700

    fetch logs upon provisioning error

--- testsuites/syncgateway/functional/tests/conftest.py
+++ testsuites/syncgateway/functional/tests/conftest.py
@@ -8,6 +8,7 @@ from keywords.SyncGateway import validate_sync_gateway_mode
 from keywords.SyncGateway import sync_gateway_config_path_for_mode
 from libraries.testkit import cluster
 from libraries.NetworkUtils import NetworkUtils
+from keywords.exceptions import ProvisioningError
 
 from utilities.enable_disable_ssl_cluster import enable_cbs_ssl_in_cluster_config
 from utilities.enable_disable_ssl_cluster import disable_cbs_ssl_in_cluster_config
@@ -99,13 +100,18 @@ def params_from_base_suite_setup(request):
     # Skip provisioning if user specifies '--skip-provisoning'
     if not skip_provisioning:
         cluster_helper = ClusterKeywords()
-        cluster_helper.provision_cluster(
-            cluster_config=cluster_config,
-            server_version=server_version,
-            sync_gateway_version=sync_gateway_version,
-            sync_gateway_config=sg_config,
-            race_enabled=race_enabled
-        )
+        try:
+            cluster_helper.provision_cluster(
+                cluster_config=cluster_config,
+                server_version=server_version,
+                sync_gateway_version=sync_gateway_version,
+                sync_gateway_config=sg_config,
+                race_enabled=race_enabled
+            )
+        except ProvisioningError:
+            logging_helper = Logging()
+            logging_helper.fetch_and_analyze_logs(cluster_config=cluster_config, test_name=request.node.name)
+            raise
 
     # Load topology as a dictionary
     cluster_utils = ClusterKeywords()
