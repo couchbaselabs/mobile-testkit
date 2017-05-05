fb6a548d69411d87360b02a64663a9833e1c56f3
Traun Leyden <tleyden@couchbase.com>
2017-05-04 20:04:20 -0700

    Update run_test_batch.sh

--- run_test_batch.sh
+++ run_test_batch.sh
@@ -5,8 +5,39 @@ import os
 
 DEFAULT_SUITE="testsuites/syncgateway/functional/tests/"
 TOPOLOGY_SPECIFIC_SUITE="testsuites/syncgateway/functional/topology_specific_tests/"
+SERVER_VERSION="4.5.1"
+SYNC_GATEWAY_COMMIT="4909d849677fd5b7cce0b0954f7265ee1a33ca48"
+
+
+
+# Passing
+
+# Base CC section: https://gist.github.com/tleyden/83b67617f6c6fd891cda641d9d21b0b6
+
+
+# Known to be failing
+#    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit_repeated_deletes"},
+#    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit"},
+#    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_multiple_sync_gateways"},
 
 tests = [
+
+    # Topology specific CC
+    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit_repeated_deletes"},
+    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit"},
+    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_multiple_sync_gateways"},
+    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_sg_replicate_basic_test"},
+
+    # Base DI
+    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_backfill_channel_grant_to_role_longpoll[custom_sync/access-CHANNEL-REST-channels_to_grant0]"},
+    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_offline_tap_loss_sanity[bucket_online_offline/bucket_online_offline_default_dcp-100]"},
+    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_offline_tap_loss_sanity[bucket_online_offline/bucket_online_offline_default-100]"},
+    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_multiple_dbs_unique_buckets_lose_tap[bucket_online_offline/bucket_online_offline_multiple_dbs_unique_buckets-100]"},
+    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_online_offline_webhooks_offline_two[sync_gateway_webhook-5-1-1-2]"},
+
+    # Topology specific DI
+    {"mode": "di", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_server_goes_down_rebuild_channels"},
+    {"mode": "di", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_dcp_reshard_sync_gateway_comes_up[resources/sync_gateway_configs/sync_gateway_default_functional_tests_di.json]"},
 
     # Base CC
     {"mode": "cc", "suite":DEFAULT_SUITE, "testname":"test_roles_sanity[sync_gateway_default_functional_tests]"},
@@ -21,40 +52,46 @@ tests = [
     {"mode": "cc", "suite":DEFAULT_SUITE, "testname":"test_overloaded_channel_cache[sync_gateway_channel_cache-1000-ABC-False-50]"},
     {"mode": "cc", "suite":DEFAULT_SUITE, "testname":"test_overloaded_channel_cache[sync_gateway_channel_cache-1000-ABC-True-50]"},
 
-    # Base DI
-    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_backfill_channel_grant_to_role_longpoll[custom_sync/access-CHANNEL-REST-channels_to_grant0]"},
-    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_offline_tap_loss_sanity[bucket_online_offline/bucket_online_offline_default_dcp-100]"},
-    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_offline_tap_loss_sanity[bucket_online_offline/bucket_online_offline_default-100]"},
-    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_multiple_dbs_unique_buckets_lose_tap[bucket_online_offline/bucket_online_offline_multiple_dbs_unique_buckets-100]"},
-    {"mode": "di", "suite":DEFAULT_SUITE, "testname":"test_db_online_offline_webhooks_offline_two[sync_gateway_webhook-5-1-1-2]"},
-
-    # Topology specific CC
-    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit_repeated_deletes"},
-    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_low_revs_limit"},
-    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_bucket_shadow_multiple_sync_gateways"},
-    {"mode": "cc", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_sg_replicate_basic_test"},
-
-    # Topology specific DI
-    {"mode": "di", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_server_goes_down_rebuild_channels"},
-    {"mode": "di", "suite":TOPOLOGY_SPECIFIC_SUITE, "testname":"test_dcp_reshard_sync_gateway_comes_up[resources/sync_gateway_configs/sync_gateway_default_functional_tests_di.json]"},
 
 ]
 
+provisioned_test_suite = ""
+
 for test in tests:
-    suite = test["suite"] 
+    suite = test["suite"]
     testname = test["testname"]
     mode = test["mode"]
 
     print "------------------------------------------------- Running test: suite: {}, testname: {}".format(suite, testname)
-    
+
     cmd_args = [
-        "pytest", 
+        "pytest",
         "-s",
         "--mode={}".format(mode),
-        "--skip-provisioning",
+    ]
+
+    if suite == provisioned_test_suite:
+        print "Skipping provisioning"
+        cmd_args += [ "--skip-provisioning" ]
+    else:
+        print "------------------------------------------------- Provisioning test: suite: {}, testname: {}".format(suite, testname)
+        # force provisioning and record this as the provisioned_test_suite
+        cmd_args += [
+            "--server-version={}".format(SERVER_VERSION),
+            "--sync-gateway-version={}".format(SYNC_GATEWAY_COMMIT),
+         ]
+
+        provisioned_test_suite = suite
+
+    cmd_args += [
         "-k",
         testname,
-        suite]
-    
+        suite
+    ]
+
     cmd = " ".join(cmd_args)
-    os.system(cmd)
+    print("{}".format(cmd))
+    raw_exit_val = os.system(cmd)
+    exit_code = os.WEXITSTATUS(raw_exit_val)
+    if exit_code != 0:
+        raise Exception("Test failed, aborting")
