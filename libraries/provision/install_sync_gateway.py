c0041ec2d73e34ef85c00c84fb45ef2daef9f2ae
Seth Rosetter <seth.rosetter@gmail.com>
2017-05-16 09:51:16 -0700

    Add 1.5.0 to valid versions

--- libraries/provision/install_sync_gateway.py
+++ libraries/provision/install_sync_gateway.py
@@ -40,7 +40,8 @@ class SyncGatewayConfig:
             "1.4.0.1",
             "1.4.0.2",
             "1.4.1",
-            "1.4.2"
+            "1.4.2",
+            "1.5.0"
         ]
 
         self.commit = commit
