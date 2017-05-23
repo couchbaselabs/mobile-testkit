5ce26a832e045d282da2a05d0db33b2d9b8ea6ac
Seth Rosetter <seth.rosetter@gmail.com>
2017-05-23 14:55:58 -0700

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-echo "Cleaning repo"
-echo "Removing __pycache__/ ..."
-rm -rf __pycache__/
-
-echo "Removing mobile_testkit_tests/__pycache__/ and .cache ..."
-rm -rf mobile_testkit_tests/__pycache__/
-rm -rf mobile_testkit_tests/.cache/
-
-echo "Removing testsuites/syncgateway/functional/tests/__pycache__/ and .cache ..."
-rm -rf testsuites/syncgateway/functional/tests/__pycache__/
-
-echo "Removing all .pyc files ..."
-find . -name "*.pyc" -exec rm -f {} \;
+echo "Removing all untracked files ..."
+git clean -ffdx
