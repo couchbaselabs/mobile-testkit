f81ad481462dcd9a6ba352c4168823074206e3fa
Seth Rosetter <seth.rosetter@gmail.com>
2017-05-25 11:26:04 -0700

    Add streaming output flag

--- docker/mobile-testkit/entrypoint.sh
+++ docker/mobile-testkit/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 # $1 = mobile-testkit branch to check out
-# $2 = mode 'cc' or 'di'
-# $3 = xattrs flag (--xattrs)
-# $4 = Couchbase Server version
-# $5 = Sync Gateway version
-# $6 = test filter -k "test_pattern"
+# $2 = stream test output flag (-s)
+# $3 = mode 'cc' or 'di'
+# $4 = xattrs flag (--xattrs)
+# $5 = Couchbase Server version
+# $6 = Sync Gateway version
+# $7 = test filter -k "test_pattern"
 
 if [ $# -ne 6 ]
   then
@@ -31,4 +32,4 @@ pip install -r requirements.txt
 python libraries/utilities/generate_clusters_from_pool.py
 
 # Run single test
-pytest -s --mode=$2 $3 --server-version=$4 --sync-gateway-version=$5 $6 testsuites/syncgateway/functional/tests
+pytest $2 --mode=$3 $4 --server-version=$5 --sync-gateway-version=$6 $7 testsuites/syncgateway/functional/tests
