030dc9ac3c4c1a16f9a23edcb29928623bd4a7e5
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-06-06 12:43:30 -0700

    Changes for iOS simulator runs

--- keywords/LiteServiOS.py
+++ keywords/LiteServiOS.py
@@ -1,20 +1,25 @@
 import json
 import subprocess
+import os
+import re
+import time
+from zipfile import ZipFile
+import requests
+from requests.exceptions import ConnectionError
 
 from keywords.LiteServBase import LiteServBase
 from keywords.constants import BINARY_DIR
+from keywords.constants import LATEST_BUILDS
+from keywords.constants import MAX_RETRIES
 from keywords.exceptions import LiteServError
 from keywords.utils import version_and_build
 from keywords.utils import log_info
 from keywords.utils import log_r
+from keywords.constants import REGISTERED_CLIENT_DBS
+from keywords.constants import CLIENT_REQUEST_TIMEOUT
 
 
 class LiteServiOS(LiteServBase):
-
-    def __init__(self, version_build, host, port, storage_engine):
-
-        # Initialize baseclass properies
-        super(LiteServiOS, self).__init__(version_build, host, port, storage_engine)
 
     def download(self):
         """
@@ -22,9 +27,40 @@ class LiteServiOS(LiteServBase):
         2. Download the LiteServ package from latest builds to 'deps/binaries'
         3. Unzip the packages and make the binary executable
         """
-        raise NotImplementedError("iOS not a part of build yet")
+        version, build = version_and_build(self.version_build)
 
-    def install(self):
+        package_name = "LiteServ-iOS.zip"
+        app_name = "LiteServ-iOS.app"
+        app_dir = "LiteServ-iOS"
+
+        if self.storage_engine == "SQLCipher":
+            package_name = "LiteServ-iOS-SQLCipher.zip"
+            app_name = "LiteServ-iOS-SQLCipher.app"
+            app_dir = "LiteServ-iOS-SQLCipher"
+
+        expected_binary_path = "{}/{}/{}".format(BINARY_DIR, app_dir, app_name)
+        if os.path.isfile(expected_binary_path):
+            log_info("Package is already downloaded. Skipping.")
+            return
+
+        # Package not downloaded, proceed to download from latest builds
+        downloaded_package_zip_name = "{}/{}".format(BINARY_DIR, package_name)
+        url = "{}/couchbase-lite-ios/{}/ios/{}/{}".format(LATEST_BUILDS, version, self.version_build, package_name)
+
+        log_info("Downloading {} -> {}/{}".format(url, BINARY_DIR, package_name))
+        resp = requests.get(url)
+        resp.raise_for_status()
+        with open("{}/{}".format(BINARY_DIR, package_name), "wb") as f:
+            f.write(resp.content)
+
+        extracted_directory_name = downloaded_package_zip_name.replace(".zip", "")
+        with ZipFile("{}".format(downloaded_package_zip_name)) as zip_f:
+            zip_f.extractall("{}".format(extracted_directory_name))
+
+        # Remove .zip
+        os.remove("{}".format(downloaded_package_zip_name))
+
+    def install_device(self):
         """Installs / launches LiteServ on iOS device
         Warning: Only works with a single device at the moment
         """
@@ -32,8 +68,8 @@ class LiteServiOS(LiteServBase):
         if self.storage_engine != "SQLite":
             raise LiteServError("https://github.com/couchbaselabs/liteserv-ios/issues/1")
 
-        package_name = "couchbase-lite-ios-liteserv-{}.app".format(self.version_build)
-        app_path = "{}/{}".format(BINARY_DIR, package_name)
+        package_name = "LiteServ-iOS.app"
+        app_path = "{}/{}/{}".format(BINARY_DIR, "LiteServ-iOS", package_name)
         log_info("Installing: {}".format(app_path))
 
         # install app / launch app to connected device
@@ -51,13 +87,61 @@ class LiteServiOS(LiteServBase):
 
         self.stop()
 
-    def remove(self):
+    def install(self):
+        """Installs / launches LiteServ on iOS simulator
+        Default is iPhone 7 Plus
+        """
+        device = "iPhone-7-Plus"
+        package_name = "LiteServ-iOS.app"
+        app_dir = "LiteServ-iOS"
+
+        if self.storage_engine == "SQLCipher":
+            package_name = "LiteServ-iOS-SQLCipher.app"
+            app_dir = "LiteServ-iOS-SQLCipher"
+
+        app_path = "{}/{}/{}".format(BINARY_DIR, app_dir, package_name)
+        output = subprocess.check_output([
+            "ios-sim", "--devicetypeid", device, "start"
+        ])
+
+        log_info("Installing: {}".format(app_path))
+        # Launch the simulator and install the app
+        output = subprocess.check_output([
+            "ios-sim", "--devicetypeid", device, "install", app_path, "--exit"
+        ])
+
+        log_info(output)
+
+        # Wait for the device to boot up
+        start = time.time()
+        while True:
+            if time.time() - start > CLIENT_REQUEST_TIMEOUT:
+                raise LiteServError("iPhone Simulator failed to start")
+
+            output = subprocess.Popen([
+                "xcrun", "simctl", "spawn", "booted", "launchctl", "print", "system"
+            ], stdout=subprocess.PIPE)
+            output = subprocess.check_output(('grep', 'com.apple.springboard.services'), stdin=output.stdout)
+            output = re.sub(' +', ' ', output).strip()
+            status = output.split(" ")[2]
+            if status == "A":
+                log_info("iPhone Simulator seems to have booted up")
+                break
+            else:
+                log_info("Waiting for the iPhone Simulator to boot up")
+                time.sleep(1)
+                continue
+
+    def remove_device(self):
         """
         Remove the iOS app from the connected device
         """
-        log_info("Removing LiteServ-iOS")
-
         bundle_id = "com.couchbase.LiteServ-iOS"
+        if self.storage_engine == "SQLCipher":
+            bundle_id = "com.couchbase.LiteServ-iOS-SQLCipher"
+
+        log_info("Removing {}".format(bundle_id))
+
         output = subprocess.check_output([
             "ios-deploy", "--uninstall_only", "--bundle_id", bundle_id
         ])
@@ -70,6 +154,44 @@ class LiteServiOS(LiteServBase):
         if bundle_id in output:
             raise LiteServError("LiteServ-iOS is still present after uninstall")
 
+    def remove(self):
+        """
+        Remove the iOS app from the simulator
+        """
+        bundle_id = "com.couchbase.LiteServ-iOS"
+        if self.storage_engine == "SQLCipher":
+            bundle_id = "com.couchbase.LiteServ-iOS-SQLCipher"
+
+        log_info("Removing LiteServ")
+
+        self.stop()
+
+        # Get the device ID
+        device_id = None
+        list_output = subprocess.Popen(["xcrun", "simctl", "list"], stdout=subprocess.PIPE)
+        output = subprocess.check_output(('grep', 'Booted'), stdin=list_output.stdout)
+
+        for line in output.splitlines():
+            if "Phone" in line:
+                device_id = re.sub(' +', ' ', line).strip()
+                device_id = device_id.split(" ")[4]
+                device_id = device_id.strip('(')
+                device_id = device_id.strip(')')
+
+        # Stop the simulator
+        log_info("device_id: {}".format(device_id))
+        output = subprocess.check_output([
+            "killall", "Simulator"
+        ])
+
+        # Erase the simulator
+        output = subprocess.check_output([
+            "xcrun", "simctl", "erase", device_id
+        ])
+
+        if bundle_id in output:
+            raise LiteServError("{} is still present after uninstall".format(bundle_id))
+
     def start(self, logfile_name):
         """
         1. Starts a LiteServ with logging to provided logfile file object.
@@ -79,25 +201,56 @@ class LiteServiOS(LiteServBase):
         4. Return the url of the running LiteServ
         """
 
-        if self.storage_engine != "SQLite":
-            raise NotImplementedError("Need to make sure to support other storage types")
+        data = {}
+        encryption_enabled = False
+        device = "iPhone-7-Plus"
+
+        package_name = "LiteServ-iOS.app"
+        app_dir = "LiteServ-iOS"
+
+        if self.storage_engine == "SQLCipher":
+            package_name = "LiteServ-iOS-SQLCipher.app"
+            app_dir = "LiteServ-iOS-SQLCipher"
+
+        app_path = "{}/{}/{}".format(BINARY_DIR, app_dir, package_name)
+
+        output = subprocess.check_output([
+            "ios-sim", "--devicetypeid", device, "--log", logfile_name, "launch", app_path, "--exit"
+        ])
+        log_info(output)
+
+        if self.storage_engine == "SQLite" or self.storage_engine == "SQLCipher":
+            data["storage"] = "SQLite"
+        elif self.storage_engine == "ForestDB" or self.storage_engine == "ForestDB+Encryption":
+            data["storage"] = "ForestDB"
+
+        if self.storage_engine == "ForestDB+Encryption" or self.storage_engine == "SQLCipher":
+            encryption_enabled = True
 
         self._verify_not_running()
 
         if self.port == 59850:
             raise LiteServError("On iOS, port 59850 is reserved for the admin port")
 
+        data["port"] = int(self.port)
+
+        if encryption_enabled:
+            log_info("Encryption enabled ...")
+
+            db_flags = []
+            for db_name in REGISTERED_CLIENT_DBS:
+                db_flags.append("{}:pass".format(db_name))
+            db_flags = ",".join(db_flags)
+
+            log_info("Running with db_flags: {}".format(db_flags))
+            data["dbpasswords"] = db_flags
+
+        self._wait_until_reachable(port=59850)
         liteserv_admin_url = "http://{}:59850".format(self.host)
         log_info("Starting LiteServ: {}".format(liteserv_admin_url))
-
-        data = {
-            "port": int(self.port)
-        }
-
         resp = self.session.put("{}/start".format(liteserv_admin_url), data=json.dumps(data))
         log_r(resp)
         resp.raise_for_status()
-
         self._verify_launched()
 
         return "http://{}:{}".format(self.host, self.port)
@@ -131,8 +284,20 @@ class LiteServiOS(LiteServBase):
 
         liteserv_admin_url = "http://{}:59850".format(self.host)
         log_info("Stopping LiteServ: {}".format(liteserv_admin_url))
-        resp = self.session.put("{}/stop".format(liteserv_admin_url))
-        log_r(resp)
-        resp.raise_for_status()
+
+        count = 0
+        while count < MAX_RETRIES:
+            if count == MAX_RETRIES:
+                raise LiteServError("Could not connect to LiteServ")
+
+            try:
+                resp = self.session.put("{}/stop".format(liteserv_admin_url))
+                log_r(resp)
+                resp.raise_for_status()
+                break
+            except ConnectionError:
+                log_info("LiteServ may not be launched (Retrying) ...")
+                time.sleep(1)
+                count += 1
 
         self._verify_not_running()
