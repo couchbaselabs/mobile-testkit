66e76b6ee27e1932d81104cc0dd292f37e35739e
Seth Rosetter <seth.rosetter@gmail.com>
2017-08-02 10:59:26 -0700

    Reenable rebalance / failover scenarios

--- testsuites/syncgateway/functional/topology_specific_tests/multiple_servers/test_multiple_servers.py
+++ testsuites/syncgateway/functional/topology_specific_tests/multiple_servers/test_multiple_servers.py
@@ -22,7 +22,6 @@ from keywords import couchbaseserver
 @pytest.mark.session
 @pytest.mark.channel
 @pytest.mark.bulkops
-@pytest.mark.skip(reason="Failing due to - https://github.com/couchbase/sync_gateway/issues/2173")
 def test_rebalance_sanity(params_from_base_test_setup):
 
     cluster_config = params_from_base_test_setup["cluster_config"]
@@ -101,7 +100,6 @@ def test_rebalance_sanity(params_from_base_test_setup):
 @pytest.mark.session
 @pytest.mark.channel
 @pytest.mark.bulkops
-@pytest.mark.skip(reason="Failing due to - https://github.com/couchbase/sync_gateway/issues/2197")
 def test_server_goes_down_sanity(params_from_base_test_setup):
     """
     1. Start with a two node couchbase server cluster
