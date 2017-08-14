a3dd1d06dd72e22baba2426d70ab6e018989d4ca
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-08-14 12:06:03 -0700

    Saving work

--- testsuites/syncgateway/upgrade/test_upgrade.py
+++ testsuites/syncgateway/upgrade/test_upgrade.py
@@ -5,3 +5,11 @@ def test_system_test(params_from_base_test_setup):
     cluster_config = params_from_base_test_setup['cluster_config']
     mode = params_from_base_test_setup['mode']
     xattrs_enabled = params_from_base_test_setup['xattrs_enabled']
+
+    # Add data to SG
+
+    # Upgrade CBS
+
+    # Upgrade SG
+
+    # Verify data
