32a58a0a6d727abf4fca96b8ad11ec599a5c7312
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-08-18 15:58:26 -0700

    Saving work

--- testsuites/syncgateway/upgrade/test_upgrade.py
+++ testsuites/syncgateway/upgrade/test_upgrade.py
@@ -26,7 +26,7 @@ def test_upgrade(params_from_base_test_setup):
     # TODO Add data to liteserv
 
     # Upgrade CBS
-    cluster = Cluster(config=cluster_config, lb_enable=False)
+    cluster = Cluster(config=cluster_config)
     if len(cluster.servers) < 3:
         raise Exception("Please provide at least 3 servers")
 
@@ -43,6 +43,7 @@ def test_upgrade(params_from_base_test_setup):
     log_info('------------------------------------------')
     # Upgrade all servers except the primary server
     for server in servers:
+        log_info("Checking for the server version: {}".format(server_version))
         verify_server_version(server.host, server_version)
         log_info("Rebalance out server: {}".format(server.host))
         primary_server.rebalance_out(server_urls, server)
@@ -52,10 +53,12 @@ def test_upgrade(params_from_base_test_setup):
         primary_server.add_node(server)
         log_info("Rebalance in server: {}".format(server.host))
         primary_server.rebalance_in(server_urls, server)
+        log_info("Checking for the server version: {}".format(server_upgraded_version))
         verify_server_version(server.host, server_upgraded_version)
 
     # Upgrade the primary server
     primary_server_services = "kv,index,n1ql"
+    log_info("Checking for the server version: {}".format(server_version))
     verify_server_version(primary_server.host, server_version)
     log_info("Rebalance out server: {}".format(primary_server.host))
     secondary_server.rebalance_out(server_urls, primary_server)
@@ -65,6 +68,7 @@ def test_upgrade(params_from_base_test_setup):
     secondary_server.add_node(primary_server, services=primary_server_services)
     log_info("Rebalance in server: {}".format(primary_server.host))
     secondary_server.rebalance_in(server_urls, primary_server)
+    log_info("Checking for the server version: {}".format(server_upgraded_version))
     verify_server_version(primary_server.host, server_upgraded_version)
 
     log_info("Upgraded all the server nodes in the cluster")
@@ -82,12 +86,15 @@ def test_upgrade(params_from_base_test_setup):
         sg_ip = host_for_url(sg["admin"])
         sg_obj = SyncGateway()
         verify_sync_gateway_product_info(sg_ip)
+        log_info("Checking for sunc gateway version: {}".format(sync_gateway_version))
         verify_sync_gateway_version(sg_ip, sync_gateway_version)
         log_info("Upgrading sync gateway: {}".format(sg_ip))
         sg_obj.upgrade_sync_gateways(cluster_config, sg_conf, sync_gateway_upgraded_version, sg_ip)
         verify_sync_gateway_product_info(sg_ip)
+        log_info("Checking for sunc gateway version: {}".format(sync_gateway_upgraded_version))
         verify_sync_gateway_version(sg_ip, sync_gateway_upgraded_version)
 
+    log_info("Upgraded all the sync gateway nodes in the cluster")
     log_info('------------------------------------------')
     log_info('END Sync Gateway cluster upgrade')
     log_info('------------------------------------------')
