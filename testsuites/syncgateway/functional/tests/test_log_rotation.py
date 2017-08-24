acf55d9b51eebb06dfa5bfbee8e8fc6073e5123e
Sridevi Saragadam <s_sridevi15@yahoo.com>
2017-08-23 23:39:13 -0700

    fixed couchbases schema and port when ssl enabled to one of the missed file

--- testsuites/syncgateway/functional/tests/test_log_rotation.py
+++ testsuites/syncgateway/functional/tests/test_log_rotation.py
@@ -10,7 +10,7 @@ from keywords.remoteexecutor import RemoteExecutor
 from keywords.SyncGateway import SyncGateway, sync_gateway_config_path_for_mode, get_sync_gateway_version
 from keywords.utils import log_info, add_cbs_to_sg_config_server_field, host_for_url
 from libraries.testkit.cluster import Cluster
-from utilities.cluster_config_utils import get_sg_replicas, get_sg_use_views, get_sg_version
+from utilities.cluster_config_utils import is_cbs_ssl_enabled, get_sg_replicas, get_sg_use_views, get_sg_version
 
 
 def load_sync_gateway_config(sync_gateway_config, mode, server_url, xattrs_enabled, cluster_config):
@@ -43,6 +43,10 @@ def load_sync_gateway_config(sync_gateway_config, mode, server_url, xattrs_enabl
             logging_prop = '"logging": {"debug": {"enabled": true}},'
 
         couchbase_server_primary_node = add_cbs_to_sg_config_server_field(cluster_config)
+        if is_cbs_ssl_enabled(cluster_conf) and get_sg_version(cluster_conf) >= "1.5.0":
+            server_scheme = "couchbases"
+            server_port = 11207
+
         temp = template.render(
             couchbase_server_primary_node=couchbase_server_primary_node,
             is_index_writer="false",
@@ -99,7 +103,6 @@ def test_log_rotation_default_values(params_from_base_test_setup, sg_conf_name):
 
     # read sample sg_conf
     data = load_sync_gateway_config(sg_conf, mode, cluster_hosts["couchbase_servers"][0], xattrs_enabled, cluster_conf)
-
     # delete rotation from sample config
     del data['logging']["default"]["rotation"]
     # create temp config file in the same folder as sg_conf
