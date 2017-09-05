f9cf7bee67f9f55f959d7d07da9da2d90b37fa2d
Sridevi Saragadam <s_sridevi15@yahoo.com>
2017-09-05 14:16:06 -0700

    used data set of mobile test kit for unit test

--- mobile_testkit_tests/test_utils.py
+++ mobile_testkit_tests/test_utils.py
@@ -1,10 +1,11 @@
 from keywords import utils
-import subprocess
+import pytest
 
 
-def test_add_cbs_to_sg_config_server_field():
-    subprocess.call("echo '{\"couchbase_servers\": [\n{\n\"ip\": \"192.168.33.10\",\n \"name\": \"cb1\"\n},\n {\n\"ip\": \"192.168.33.11\",\n\"name\": \"cb2\"\n},\n{\n\"ip\": \"192.168.33.12\",\n\"name\": \"cb3\"\n}\n]}' > resources/cluster_configs/test.json", shell=True)
-    cluster_config = "resources/cluster_configs/test"
-    expected_string = "192.168.33.10,192.168.33.11,192.168.33.12"
-    assert expected_string == utils.add_cbs_to_sg_config_server_field(cluster_config)
-    subprocess.call("rm resources/cluster_configs/test.json", shell=True)
+@pytest.mark.parametrize("test_data_cluster_config, expected_cbs_string", [
+    ("mobile_testkit_tests/test_data/cluster_configs/1sg_2ac_3cbs", "111.111.11.111,111.111.11.112,111.111.11.113"),
+    ("mobile_testkit_tests/test_data/cluster_configs/1sg_1ac_1cbs_1lgs", "111.111.11.111")
+])
+def test_add_cbs_to_sg_config_server_field(test_data_cluster_config, expected_cbs_string):
+    cluster_config = test_data_cluster_config
+    assert expected_cbs_string == utils.add_cbs_to_sg_config_server_field(cluster_config)
