890ff1c28f8167de66b111d330fbb67e24f81111
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-09-10 22:10:22 -0700

    version processing for mono on linux

--- keywords/LiteServNetMono.py
+++ keywords/LiteServNetMono.py
@@ -136,12 +136,18 @@ class LiteServNetMono(LiteServBase):
             raise LiteServError("Invalid platform running: {}!".format(running_version))
 
         log_info("Running_version: {}".format(running_version))
-        # [u'.NET', u'OS', u'X', u'10.12', u'x86_64', u'1.3.1', u'build0013', u'5d1553d']
+        # OSX - [u'.NET', u'OS', u'X', u'10.12', u'x86_64', u'1.3.1', u'build0013', u'5d1553d']
+        # Linux - .NET Linux / 1.4.0-b004/1276adf
         running_version_parts = re.split("[ /-]", running_version)
 
-        running_version = running_version_parts[5]
-        running_build = int(running_version_parts[6].strip("build"))
-        running_version_composed = "{}-{}".format(running_version, running_build)
+        if running_version.startswith(".NET OS X"):
+            running_version = running_version_parts[5]
+            running_build = int(running_version_parts[6].strip("build"))
+            running_version_composed = "{}-{}".format(running_version, running_build)
+        elif running_version.startswith(".NET Linux"):
+            running_version = running_version_parts[2]
+            running_build = int((running_version_parts[3].strip("b")).lstrip("0"))
+            running_version_composed = "{}-{}".format(running_version, running_build)
 
         if self.version_build != running_version_composed:
             raise LiteServError("Expected version does not match actual version: Expected={}  Actual={}".format(
