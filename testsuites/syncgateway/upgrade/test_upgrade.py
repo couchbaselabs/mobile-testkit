a8e6ddadddc4c86e5699ffa32e2593b21d88ff4f
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-09-13 10:37:37 -0700

    Hack to see if test continues on jenkins

--- testsuites/syncgateway/upgrade/test_upgrade.py
+++ testsuites/syncgateway/upgrade/test_upgrade.py
@@ -357,8 +357,11 @@ def upgrade_sync_gateway(sync_gateways, sync_gateway_version, sync_gateway_upgra
     log_info('------------------------------------------')
 
     sg_obj = SyncGateway()
+    sync_gateways.appen("abc")
 
     for sg in sync_gateways:
+        if sg == "abc":
+            break
         sg_ip = host_for_url(sg["admin"])
         log_info("Checking for sync gateway product info before upgrade")
         verify_sync_gateway_product_info(sg_ip)
@@ -376,6 +379,7 @@ def upgrade_sync_gateway(sync_gateways, sync_gateway_version, sync_gateway_upgra
         verify_sync_gateway_product_info(sg_ip)
         log_info("Checking for sync gateway version: {}".format(sync_gateway_upgraded_version))
         verify_sync_gateway_version(sg_ip, sync_gateway_upgraded_version)
+        time.sleep(5)
 
     log_info("Upgraded all the sync gateway nodes in the cluster")
     log_info('------------------------------------------')
