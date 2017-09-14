164db63cf55847088923ad0a7ddb7f4ddc7982f2
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-09-14 15:27:35 -0700

    Fix flake8 errors

--- testsuites/syncgateway/upgrade/conftest.py
+++ testsuites/syncgateway/upgrade/conftest.py
@@ -330,5 +330,5 @@ def params_from_base_test_setup(request, params_from_base_suite_setup):
 
     logging_helper.fetch_and_analyze_logs(cluster_config=cluster_config, test_name=test_name)
 
-    if status != 0:       
+    if status != 0:
         raise LogScanningError("Errors found in the logs")
