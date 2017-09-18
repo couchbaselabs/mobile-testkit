dc0cccaa1f2dcdb2f5f7628da98e8b98404bd7b3
raghusarangapani <raghu.sarangapani@couchbase.com>
2017-09-18 12:06:25 -0700

    Fix/disable multipleurls (#1422)
    * Add vulcan support
    
    * Disable multiple URLs for SG < 1.5
    
    * Bug fix
    
    * Skip multiple servers test for SG < 1.5

--- mobile_testkit_tests/test_utils.py
+++ mobile_testkit_tests/test_utils.py
@@ -1,11 +1,12 @@
-from keywords import utils
+from keywords.utils import add_cbs_to_sg_config_server_field
 import pytest
 
 
 @pytest.mark.parametrize("test_data_cluster_config, expected_cbs_string", [
-    ("mobile_testkit_tests/test_data/cluster_configs/1sg_2ac_3cbs", "111.111.11.111,111.111.11.112,111.111.11.113"),
-    ("mobile_testkit_tests/test_data/cluster_configs/1sg_1ac_1cbs_1lgs", "111.111.11.111")
+    ("mobile_testkit_tests/test_data/mock_base_di", "192.168.33.20"),
+    ("mobile_testkit_tests/test_data/mock_ci_di", "192.168.33.20,192.168.33.23,192.168.33.24"),
+    ("mobile_testkit_tests/test_data/mock_ci_di_sg14", "192.168.33.20")
 ])
 def test_add_cbs_to_sg_config_server_field(test_data_cluster_config, expected_cbs_string):
     cluster_config = test_data_cluster_config
-    assert expected_cbs_string == utils.add_cbs_to_sg_config_server_field(cluster_config)
+    assert expected_cbs_string == add_cbs_to_sg_config_server_field(cluster_config)
