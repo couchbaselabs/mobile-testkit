2abb9b49f942ce8eedcc232f4c2c61bda2c88996
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-09-18 09:02:27 -0700

    More logging and bug fix

--- testsuites/syncgateway/upgrade/test_upgrade.py
+++ testsuites/syncgateway/upgrade/test_upgrade.py
@@ -229,11 +229,13 @@ def test_upgrade(params_from_base_test_setup):
             # Verify through SDK that there is no _sync property in the doc body
             bucket_name = 'data-bucket'
             sdk_client = Bucket('couchbase://{}/{}'.format(primary_server.host, bucket_name), password='password', timeout=SDK_TIMEOUT)
+            log_info("Fetching docs from SDK")
             docs_from_sdk = sdk_client.get_multi(doc_ids)
 
+            log_info("Verifying that there is no _sync property in the docs")
             for i in docs_from_sdk:
                 if "_sync" in docs_from_sdk[i].value:
-                    raise Exception("sync section found in docs after upgrade")
+                    raise Exception("_sync section found in docs after upgrade")
 
 
 def add_client_docs(client, url, db, channels, generator, ndocs, id_prefix, attachments_generator):
