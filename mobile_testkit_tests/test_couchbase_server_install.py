15d57650f300b927d2d6b436e248d31583810494
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-10-05 11:51:50 -0700

    Fix unit tests

--- mobile_testkit_tests/test_couchbase_server_install.py
+++ mobile_testkit_tests/test_couchbase_server_install.py
@@ -1,4 +1,7 @@
+import pytest
+
 from libraries.provision.install_couchbase_server import CouchbaseServerConfig
+from keywords.couchbaseserver import CouchbaseServer
 
 
 def test_version_only():
@@ -21,15 +24,39 @@ def test_enterprise():
     server_config = CouchbaseServerConfig(
         version="4.1.1-5914"
     )
-    base_url, package_name = server_config.get_baseurl_package()
+    cb_server = CouchbaseServer("http://192.168.33.20:8091")
+    base_url, package_name = server_config.get_baseurl_package(cb_server)
     assert base_url == "http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-server/sherlock/5914"
     assert package_name == "couchbase-server-enterprise-4.1.1-5914-centos7.x86_64.rpm"
+
+
+def test_enterprise_centos6():
+    server_config = CouchbaseServerConfig(
+        version="4.1.1-5914"
+    )
+    cb_server = CouchbaseServer("http://192.168.33.20:8091")
+    cbs_platform = "centos6"
+    base_url, package_name = server_config.get_baseurl_package(cb_server, cbs_platform)
+    assert base_url == "http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-server/sherlock/5914"
+    assert package_name == "couchbase-server-enterprise-4.1.1-5914-centos6.x86_64.rpm"
 
 
 def test_mobile_url():
     server_config = CouchbaseServerConfig(
         version="4.1.1"
     )
-    base_url, package_name = server_config.get_baseurl_package()
+    cb_server = CouchbaseServer("http://192.168.33.20:8091")
+    base_url, package_name = server_config.get_baseurl_package(cb_server)
     assert base_url == "http://cbmobile-packages.s3.amazonaws.com"
     assert package_name == "couchbase-server-enterprise-4.1.1-5914-centos7.x86_64.rpm"
+
+
+def test_mobile_url_centos6():
+    server_config = CouchbaseServerConfig(
+        version="4.1.1"
+    )
+    cb_server = CouchbaseServer("http://192.168.33.20:8091")
+    cbs_platform = "centos6"
+    base_url, package_name = server_config.get_baseurl_package(cb_server, cbs_platform)
+    assert base_url == "http://cbmobile-packages.s3.amazonaws.com"
+    assert package_name == "couchbase-server-enterprise-4.1.1-5914-centos6.x86_64.rpm"
