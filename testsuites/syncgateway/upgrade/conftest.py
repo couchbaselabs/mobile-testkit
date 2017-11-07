128ef07712b2cd9038bc954d4f38b43689c2a8b5
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2017-11-07 11:53:14 -0800

    Revert the conftest changes

--- testsuites/syncgateway/upgrade/conftest.py
+++ testsuites/syncgateway/upgrade/conftest.py
@@ -23,6 +23,74 @@ from keywords.constants import RESULTS_DIR
 
 # Add custom arguments for executing tests in this directory
 def pytest_addoption(parser):
+    parser.addoption("--mode",
+                     action="store",
+                     help="Sync Gateway mode to run the test in, 'cc' for channel cache or 'di' for distributed index")
+
+    parser.addoption("--skip-provisioning",
+                     action="store_true",
+                     help="Skip cluster provisioning at setup",
+                     default=False)
+
+    parser.addoption("--server-version",
+                     action="store",
+                     help="server-version: Couchbase Server version to install (ex. 4.5.0 or 4.5.0-2601)")
+
+    parser.addoption("--sync-gateway-version",
+                     action="store",
+                     help="sync-gateway-version: Sync Gateway version to install (ex. 1.3.1-16 or 590c1c31c7e83503eff304d8c0789bdd268d6291)")
+
+    parser.addoption("--race",
+                     action="store_true",
+                     help="Enable -races for Sync Gateway build. IMPORTANT - This will only work with source builds at the moment")
+
+    parser.addoption("--collect-logs",
+                     action="store_true",
+                     help="Collect logs for every test. If this flag is not set, collection will only happen for test failures.")
+
+    parser.addoption("--server-ssl",
+                     action="store_true",
+                     help="If set, will enable SSL communication between server and Sync Gateway")
+
+    parser.addoption("--xattrs",
+                     action="store_true",
+                     help="Use xattrs for sync meta storage. Only works with Sync Gateway 2.0+ and Couchbase Server 5.0+")
+
+    parser.addoption("--sg-lb",
+                     action="store_true",
+                     help="If set, will enable load balancer for Sync Gateway")
+
+    parser.addoption("--sg-ce",
+                     action="store_true",
+                     help="If set, will install CE version of Sync Gateway")
+
+    parser.addoption("--sequoia",
+                     action="store_true",
+                     help="If set, the tests will use a cluster provisioned by sequoia")
+
+    parser.addoption("--liteserv-host",
+                     action="store",
+                     help="liteserv-host: the host to start liteserv on")
+
+    parser.addoption("--liteserv-port",
+                     action="store",
+                     help="liteserv-port: the port to assign to liteserv")
+
+    parser.addoption("--liteserv-version",
+                     action="store",
+                     help="liteserv-version: the version of liteserv to use")
+
+    parser.addoption("--liteserv-platform",
+                     action="store",
+                     help="liteserv-platform: the platform on which to run liteserv")
+
+    parser.addoption("--liteserv-storage-engine",
+                     action="store",
+                     help="liteserv-storage-engine: the storage-engine to use with liteserv")
+
+    parser.addoption("--ci",
+                     action="store_true",
+                     help="If set, will target larger cluster (3 backing servers instead of 1, 2 accels if in di mode)")
 
     parser.addoption("--cluster-config",
                      action="store",
