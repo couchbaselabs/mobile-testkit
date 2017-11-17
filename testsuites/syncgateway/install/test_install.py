783cbc9e560596504b3eabb30117aea013e1ed22
Andrew Reslan <andrew@couchbase.com>
2017-11-17 18:33:12 +0000

    Initial install test to validate correct version of SG/A has been installed and is running

---
+++ testsuites/syncgateway/install/test_install.py
@@ -0,0 +1,97 @@
+import os
+import random
+import time
+
+from keywords.couchbaseserver import verify_server_version
+from libraries.testkit.cluster import Cluster
+from keywords.utils import log_info, host_for_url
+from keywords.SyncGateway import (verify_sg_accel_version,
+                                  verify_sync_gateway_version,
+                                  verify_sg_accel_product_info,
+                                  verify_sync_gateway_product_info,
+                                  SyncGateway)
+from keywords.ClusterKeywords import ClusterKeywords
+from keywords.MobileRestClient import MobileRestClient
+from keywords import attachment
+from couchbase.bucket import Bucket
+from keywords.constants import SDK_TIMEOUT
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from requests.exceptions import HTTPError
+
+
+def test_install(params_from_base_test_setup):
+    """
+    @summary
+        The initial versions of SG and CBS has already been provisioned at this point
+        We have to query SG/A "/" endpoint on ADMIN Rest API
+    """
+    cluster_config = params_from_base_test_setup['cluster_config']
+    mode = params_from_base_test_setup['mode']
+    xattrs_enabled = params_from_base_test_setup['xattrs_enabled']
+    ls_url = params_from_base_test_setup["ls_url"]
+    server_version = params_from_base_test_setup['server_version']
+    sync_gateway_version = params_from_base_test_setup['sync_gateway_version']
+    sg_url = params_from_base_test_setup['sg_url']
+    sg_admin_url = params_from_base_test_setup['sg_admin_url']
+    cbs_platform = params_from_base_test_setup['cbs_platform']
+    cbs_toy_build = params_from_base_test_setup['cbs_toy_build']
+    sg_conf = "{}/resources/sync_gateway_configs/sync_gateway_default_functional_tests_{}.json".format(os.getcwd(), mode)
+
+        # 
+        cluster_util = ClusterKeywords()
+        topology = cluster_util.get_cluster_topology(cluster_config, lb_enable=False)
+        sync_gateways = topology["sync_gateways"]
+        sg_accels = topology["sg_accels"]
+
+        validate_sync_gateway(
+            sync_gateways,
+            sync_gateway_version,
+            sg_conf,
+            cluster_config
+        )
+
+        if mode == "di":
+            validate_sg_accel(
+                sg_accels,
+                sync_gateway_version,
+                sg_conf,
+                cluster_config
+            )
+
+
+def validate_sync_gateway(sync_gateways, sync_gateway_version, sg_conf, cluster_config):
+    log_info('---------------------------------------')
+    log_info('Validating installation of Sync Gateway')
+    log_info('---------------------------------------')
+
+    sg_obj = SyncGateway()
+
+    for sg in sync_gateways:
+        sg_ip = host_for_url(sg["admin"])
+        log_info("Checking for sync gateway product info")
+        verify_sync_gateway_product_info(sg_ip)
+        log_info("Checking for sync gateway version: {}".format(sync_gateway_version))
+        verify_sync_gateway_version(sg_ip, sync_gateway_version)
+
+    log_info("Checked all the sync gateway nodes in the cluster")
+    log_info('------------------------------------------')
+    log_info('END Sync Gateway cluster check')
+    log_info('------------------------------------------')
+
+
+def validate_sg_accel(sg_accels, sync_gateway_version, sg_conf, cluster_config):
+    log_info('----------------------------------')
+    log_info('Vaidating installation of SG Accel')
+    log_info('----------------------------------')
+
+    ac_obj = SyncGateway()
+
+    for ac in sg_accels:
+        ac_ip = host_for_url(ac)
+        log_info("Checking for sg_accel version: {}".format(sync_gateway_version))
+        verify_sg_accel_version(ac_ip, sync_gateway_version)
+
+    log_info("Checked  all the sg accel nodes in the cluster")
+    log_info('------------------------------------------')
+    log_info('END SG Accel cluster check')
+    log_info('------------------------------------------')
