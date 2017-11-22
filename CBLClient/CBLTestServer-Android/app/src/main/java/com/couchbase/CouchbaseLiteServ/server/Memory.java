cad5fc3f518cf2ea6db1983c5845c0c5a8dea0b5
Hemant <Hemant.rajput@couchbase.com>
2017-11-22 14:35:21 +0530

    Adding CBL Android app to CBL20 branch

---
+++ CBLClient/CBLTestServer-Android/app/src/main/java/com/couchbase/CouchbaseLiteServ/server/Memory.java
@@ -0,0 +1,29 @@
+package com.couchbase.CouchbaseLiteServ.server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Memory {
+    private Map<String, Object> _memory = new HashMap<>();
+    private int _address = 0;
+
+    public <T> T get(String address) {
+        return (T)_memory.get(address);
+    }
+
+    public String add(Object value) {
+        String address = "@" + Integer.toString(++_address);
+
+        _memory.put(address, value);
+
+        return address;
+    }
+
+    public void remove(String address) {
+        _memory.remove(address);
+    }
+
+    public Map<String, Object> get_memory_map(){
+        return _memory;
+    }
+}
---
+++ CBLClient/CBLTestServer-Android/app/src/main/java/com/couchbase/CouchbaseLiteServ/server/ValueSerializer.java
@@ -0,0 +1,43 @@
+package com.couchbase.CouchbaseLiteServ.server;
+
+public class ValueSerializer {
+    public static String serialize(Object value, Memory memory) {
+        if (value == null) {
+            return "null";
+        } else if (value instanceof String) {
+            String string = (String) value;
+
+            return "\"" + string + "\"";
+        } else if (value instanceof Number) {
+            Number number = (Number) value;
+
+            return number.toString();
+        } else if (value instanceof Boolean) {
+            Boolean bool = (Boolean) value;
+
+            return (bool ? "true" : "false");
+        } else {
+            return memory.add(value);
+        }
+    }
+
+    public static <T> T deserialize(String value, Memory memory) {
+        if (value == null) {
+            return null;
+        } else if (value.startsWith("@")) {
+            return memory.get(value);
+        } else if (value.equals("true")) {
+            return (T)Boolean.TRUE;
+        } else if (value.equals("false")) {
+            return (T)Boolean.FALSE;
+        } else if (value.startsWith("\"") && value.endsWith("\"")) {
+            return (T)value.substring(1, value.length() - 1);
+        } else {
+            if (value.contains(".")) {
+                return (T)new Double(value);
+            } else {
+                return (T)new Integer(value);
+            }
+        }
+    }
+}
