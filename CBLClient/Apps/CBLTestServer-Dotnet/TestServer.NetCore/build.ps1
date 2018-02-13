2feb850f39fefcca63c1918071cb7ac8b616e474
Jim Borden <jim.borden@couchbase.com>
2018-02-14 08:25:45 +0900

    Update the build script for .NET to modify the version number of Couchbase.Lite before building Call like this: build.ps1 -Version 2.0.0 -BuildNum 555 -Community (leave community out for enterprise)

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/build.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.NetCore/build.ps1
@@ -1,9 +1,63 @@
-﻿$MyPath = Split-Path $MyInvocation.MyCommand.Source
+﻿param (
+    [Parameter(Mandatory=$true)][string]$Version,
+    [Parameter(Mandatory=$true)][int]$BuildNum,
+    [switch]$Community
+)
+
+function Modify-Packages {
+    $filename = $args[0]
+    $ver = $args[1]
+    $community = $args[2]
+
+    $content = [System.IO.File]::ReadAllLines($filename)
+    $regex = New-Object -TypeName "System.Text.RegularExpressions.Regex" ".*?<PackageReference Include=`"Couchbase\.Lite(.*?)`" Version=`"(.*?)`""
+    for($i = 0; $i -lt $content.Length; $i++) {
+        $line = $content[$i]
+        $matches = $regex.Matches($line)
+        if($matches.Count -gt 0) {
+            $oldPackageName = $matches[0].Groups[1].Value;
+            $packageName = $oldPackageName.Replace(".Enterprise", "")
+            if(-Not $community) {
+                $packageName = ".Enterprise" + $packageName;
+            }
+
+            $oldVersion = $matches[0].Groups[2]
+            $line = $line.Replace("Couchbase.Lite$oldPackageName", "Couchbase.Lite$packageName").Replace($oldVersion, $ver)
+            $content[$i] = $line
+        }
+    }
+
+    [System.IO.File]::WriteAllLines($filename, $content)
+}
+
+$MyPath = Split-Path $MyInvocation.MyCommand.Source
 Push-Location $MyPath
 
 try {
+    $fullVersion = $version + "-b" + $buildNum.ToString("D4")
+    Modify-Packages "TestServer.NetCore.csproj" $fullVersion $Community
+    Modify-Packages "..\TestServer\TestServer.csproj" $fullVersion $Community
+
+    Push-Location ..\TestServer
     dotnet restore
+    if($LASTEXITCODE -ne 0) {
+        Write-Error "Restore failed for TestServer"
+        exit 1
+    }
+
+    Pop-Location
+
+    dotnet restore
+    if($LASTEXITCODE -ne 0) {
+        Write-Error "Restore failed for TestServer.NetCore"
+        exit 1
+    }
+
     dotnet publish -c Release
+    if($LASTEXITCODE -ne 0) {
+        Write-Error "Publish failed for TestServer.NetCore"
+        exit 1
+    }
 
     if(-Not (Test-Path "zips")) {
         New-Item -ItemType Directory "zips"
