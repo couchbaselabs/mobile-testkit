b5c91d7c4dbc0c73dcf46308bce3175c6463be1c
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-03-17 16:28:38 -0700

    CBLTestServer-iOS app

---
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/DatabaseRequestHandler.swift
@@ -0,0 +1,303 @@
+//
+//  DatabaseRequestHandler.swift
+//  CBLTestServer-iOS
+//
+//  Created by Raghu Sarangapani on 12/20/17.
+//  Copyright © 2017 Raghu Sarangapani. All rights reserved.
+//
+
+import Foundation
+import CouchbaseLiteSwift
+
+
+public class DatabaseRequestHandler {
+    public static let VOID: String? = nil
+    fileprivate var _pushPullReplListener:NSObjectProtocol?
+    
+    public func handleRequest(method: String, args: Args) throws -> Any? {
+        switch method {
+
+        //////////////
+        // Database //
+        //////////////
+    
+        case "database_create":
+            let name: String! = args.get(name: "name")
+            let dbConfig: DatabaseConfiguration? = args.get(name: "config")
+            do {
+                if dbConfig != nil {
+                    return try Database(name: name!, config: dbConfig!)
+                } else {
+                    return try Database(name: name!)
+                }
+            } catch {
+                print("Got error while creating DB \(error)")
+                return error.localizedDescription
+            }
+            
+        case "database_close":
+            let database: Database = args.get(name:"database")!
+            try database.close()
+
+        case "database_getPath":
+            let database: Database = (args.get(name:"database"))!
+
+            return database.path
+
+        case "database_delete":
+            let database: Database = (args.get(name:"database"))!
+            let document: Document = (args.get(name:"document"))!
+
+            do {
+                try database.deleteDocument(document)
+            } catch {
+                print("Got error while deleting DB \(error)")
+                return error
+            }
+            
+        case "database_deleteBulkDocs":
+            let database: Database = args.get(name:"database")!
+            let doc_ids: Array<String> = args.get(name: "doc_ids")!
+            print("Doc Id in database to delete documents are:\(doc_ids)")
+            try database.inBatch {
+                for id in doc_ids {
+                    let document: Document = database.document(withID: id)!
+                    do {
+                        try! database.deleteDocument(document)
+                    }
+                }
+            }
+
+        case "database_deleteDB":
+            let database: Database = args.get(name:"database")!
+            
+            try database.delete()
+
+        case "database_exists":
+            let name: String = args.get(name:"name")!
+            let directory: String? = args.get(name:"directory")!
+           
+            if let directory = directory {
+                print("is database exists in path\(Database.exists(withName: name, inDirectory: directory))")
+                return Database.exists(withName: name, inDirectory: directory) as Bool
+            } else {
+                return Database.exists(withName: name)
+            }
+
+        case "database_deleteIndex":
+            let database: Database = (args.get(name:"database"))!
+            let name: String = (args.get(name: "name"))!
+            
+            try database.deleteIndex(forName: name)
+
+        case "database_getName":
+            let database: Database = args.get(name:"database")!
+
+            return database.name
+
+        case "database_getDocument":
+            let database: Database = (args.get(name:"database"))!
+            let id: String? = args.get(name: "id")
+            return database.document(withID: id!)
+            
+        case "database_save":
+            let database: Database = (args.get(name:"database"))!
+            let document: MutableDocument = args.get(name:"document")!
+            try! database.saveDocument(document)
+            
+        case "database_saveWithConcurrency":
+            let database: Database = (args.get(name:"database"))!
+            let document: MutableDocument = args.get(name:"document")!
+            let concurrencyControlType : String? = args.get(name:"concurrencyControlType")!
+            var concurrencyType = ConcurrencyControl.lastWriteWins
+            
+            if let type = concurrencyControlType {
+                if type == "failOnConflict" {
+                    concurrencyType = .failOnConflict
+                } else {
+                    concurrencyType = .lastWriteWins
+                }
+            }
+            try! database.saveDocument(document, concurrencyControl: concurrencyType)
+
+        case "database_deleteWithConcurrency":
+            let database: Database = (args.get(name:"database"))!
+            let document: Document = (args.get(name:"document"))!
+            let concurrencyControlType : String? = args.get(name:"concurrencyControlType")!
+            var concurrencyType = ConcurrencyControl.lastWriteWins
+            
+            if let type = concurrencyControlType {
+                if type == "failOnConflict" {
+                    concurrencyType = .failOnConflict
+                } else {
+                    concurrencyType = .lastWriteWins
+                }
+            }
+            do {
+                try database.deleteDocument(document, concurrencyControl: concurrencyType)
+            } catch {
+                print("Got error while deleting DB \(error)")
+                return error
+            }
+
+            
+        case "database_purge":
+            let database: Database = (args.get(name:"database"))!
+            let document: MutableDocument = args.get(name:"document")!
+            try! database.purgeDocument(document)
+            
+        case "database_contains":
+            let database: Database = (args.get(name:"database"))!
+            let id: String = (args.get(name: "id"))!
+
+            return database.document(withID: id) != nil
+
+        case "database_getCount":
+            let database: Database = (args.get(name:"database"))!
+            return database.count
+
+        case "database_compact":
+            let database: Database = (args.get(name:"database"))!
+            return try! database.compact()
+            
+        case "database_addChangeListener":
+            let database: Database = (args.get(name:"database"))!
+            let changeListener = MyDatabaseChangeListener()
+            database.addChangeListener(changeListener.listener)
+            return changeListener
+
+        case "database_removeChangeListener":
+            let database: Database = (args.get(name:"database"))!
+            let changeListener: ListenerToken = (args.get(name: "changeListener"))!
+
+            database.removeChangeListener(withToken: changeListener)
+
+        case "database_databaseChangeListenerChangesCount":
+            let changeListener: MyDatabaseChangeListener = (args.get(name: "changeListener"))!
+
+            return changeListener.getChanges().count
+
+        case "database_databaseChangeListenerGetChange":
+            let changeListener: MyDatabaseChangeListener = (args.get(name: "changeListener"))!
+            let index: Int = (args.get(name: "index"))!
+
+            return changeListener.getChanges()[index]
+
+        case "database_databaseChangeListenerGetChanges":
+            let changeListener: MyDatabaseChangeListener = (args.get(name: "changeListener"))!
+            return changeListener.getChanges()
+            
+        case "database_databaseChangeGetDocumentId":
+            let change: DatabaseChange = (args.get(name: "change"))!
+
+            return change.documentIDs
+            
+        case "database_deleteDBbyName":
+            let name: String = args.get(name:"name")!
+            let directory: String? = args.get(name:"directory")!
+            if let directory = directory {
+                return try Database.delete(withName: name, inDirectory: directory)
+            } else {
+                return try Database.delete(withName: name)
+            }
+
+        case "database_saveDocuments":
+            let database: Database = args.get(name:"database")!
+            let documents: Dictionary<String, Dictionary<String, Any>> = args.get(name: "documents")!
+            try database.inBatch {
+                for doc in documents {
+                    let id = doc.key
+                    let data: Dictionary<String, Any> = doc.value
+                    let document = MutableDocument(id: id, data: data)
+                    try! database.saveDocument(document)
+                    
+                }
+            }
+ 
+        case "database_updateDocuments":
+            let database: Database = args.get(name:"database")!
+            let documents: Dictionary<String, Dictionary<String, Any>> = args.get(name: "documents")!
+            try database.inBatch {
+                for doc in documents {
+                    let id = doc.key
+                    let data: Dictionary<String, Any> = doc.value
+                    let updated_doc = database.document(withID: id)!.toMutable()
+                    updated_doc.setData(data)
+                    try database.saveDocument(updated_doc)
+                }
+            }
+            
+        case "database_updateDocument":
+            
+            let database: Database = (args.get(name:"database"))!
+            let data: Dictionary<String, Any> = args.get(name: "data")!
+            let docId: String = args.get(name: "id")!
+            let updated_doc = database.document(withID: docId)!.toMutable()
+            updated_doc.setData(data)
+            try! database.saveDocument(updated_doc)
+            
+            
+        case "database_getDocIds":
+            let database: Database = args.get(name:"database")!
+            let query = QueryBuilder
+                .select(SelectResult.expression(Meta.id))
+                .from(DataSource.database(database))
+
+            var result: [String] = []
+            do {
+                for row in try query.execute() {
+                    result.append(row.string(forKey: "id")!)
+                }
+            }
+
+            return result
+
+        case "database_getDocuments":
+            let database: Database = args.get(name:"database")!
+            let ids: [String] = args.get(name:"ids")!
+            var documents = [String: [String: Any]]()
+
+            for id in ids {
+                let document: Document = database.document(withID: id)!
+                documents[id] = document.toDictionary()
+            }
+
+            return documents
+          
+            // TODO : Uncomment once encrption feature is added
+        /*case "database_setEncryptionKey":
+            let database: Database = args.get(name:"database")!
+            let password: String? = args.get(name:"password")!
+            let encryptionKey: EncryptionKey? = EncryptionKey.password(password!)
+            do {
+                return try database.setEncryptionKey(encryptionKey)
+            } catch {
+                print("Got error setting encryption key \(error)")
+                return error.localizedDescription
+            }
+        */
+        case "database_queryAllDocuments":
+            let database: Database = args.get(name:"database")!
+            let searchQuery = QueryBuilder
+                .select(SelectResult.all())
+                .from(DataSource.database(database))
+
+            return searchQuery
+        default:
+            throw RequestHandlerError.MethodNotFound(method)
+        }
+        return DatabaseRequestHandler.VOID
+    }
+}
+
+class MyDatabaseChangeListener  {
+    var changes: [DatabaseChange] = []
+    
+    lazy var listener: (DatabaseChange) -> Void = { (change: DatabaseChange) in
+        self.changes.append(change)
+    }
+    
+    public func getChanges() -> [DatabaseChange] {
+        return changes
+    }
+}
---
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/Server.swift
@@ -0,0 +1,176 @@
+//
+//  Server.swift
+//  CBLTestServer-iOS
+//
+//  Created by Raghu Sarangapani on 10/24/17.
+//  Copyright © 2017 Raghu Sarangapani. All rights reserved.
+//
+
+import Foundation
+import CouchbaseLiteSwift
+
+enum ServerError: Error {
+    case MethodNotImplemented(String)
+}
+
+enum RequestHandlerError: Error {
+    case MethodNotFound(String)
+    case InvalidArgument(String)
+    case IOException(String)
+}
+
+public class Server {
+    let kPort:UInt = 8080
+    let server: GCDWebServer!
+    let dictionaryRequestHandler: DictionaryRequestHandler!
+    let queryRequestHandler: QueryRequestHandler!
+    let databaseRequestHandler: DatabaseRequestHandler!
+    let documentRequestHandler: DocumentRequestHandler!
+    let replicatorRequestHandler: ReplicatorRequestHandler!
+    let arrayRequestHandler: ArrayRequestHandler!
+    let sessionauthenticatorRequestHandler: SessionAuthenticatorRequestHandler!
+    let encryptionkeyRequestHandler: EncryptionKeyRequestHandler!
+    let blobRequestHandler: BlobRequestHandler!
+    let datatypeRequestHandler: DataTypesInitiatorRequestHandler!
+    let replicatorConfigurationRequestHandler: ReplicatorConfigurationRequestHandler!
+    let expressionRequestHandler: ExpressionRequestHandler!
+    let collationRequestHandler: CollationRequestHandler!
+    let dataSourceRequestHandler: DataSourceRequestHandler!
+    let functionRequestHandler: FunctionRequestHandler!
+    let selectResultRequestHandler: SelectResultRequestHandler!
+    let resultRequestHandler: ResultRequestHandler!
+    let basicAuthenticatorRequestHandler: BasicAuthenticatorRequestHandler!
+    let databaseConfigurationRequestHandler: DatabaseConfigurationRequestHandler!
+    let memory = Memory()
+    
+    public init() {
+        Database.setLogLevel(LogLevel.debug, domain: LogDomain.all)
+        dictionaryRequestHandler = DictionaryRequestHandler()
+        queryRequestHandler = QueryRequestHandler()
+        databaseRequestHandler = DatabaseRequestHandler()
+        documentRequestHandler = DocumentRequestHandler()
+        replicatorRequestHandler = ReplicatorRequestHandler()
+        arrayRequestHandler = ArrayRequestHandler()
+        sessionauthenticatorRequestHandler = SessionAuthenticatorRequestHandler()
+        encryptionkeyRequestHandler = EncryptionKeyRequestHandler()
+        blobRequestHandler = BlobRequestHandler()
+        datatypeRequestHandler = DataTypesInitiatorRequestHandler()
+        replicatorConfigurationRequestHandler = ReplicatorConfigurationRequestHandler()
+        expressionRequestHandler = ExpressionRequestHandler()
+        collationRequestHandler = CollationRequestHandler()
+        dataSourceRequestHandler = DataSourceRequestHandler()
+        functionRequestHandler = FunctionRequestHandler()
+        selectResultRequestHandler = SelectResultRequestHandler()
+        resultRequestHandler = ResultRequestHandler()
+        basicAuthenticatorRequestHandler = BasicAuthenticatorRequestHandler()
+        databaseConfigurationRequestHandler = DatabaseConfigurationRequestHandler()
+        server = GCDWebServer()
+        server.addDefaultHandler(forMethod: "POST", request: GCDWebServerDataRequest.self) {
+            (request) -> GCDWebServerResponse? in
+            
+            var rawArgs = [String: Any]()
+            
+            var method = ""
+            
+            if request.path.hasPrefix("/") {
+                let start = request.path.index(request.path.startIndex, offsetBy: 1)
+                method = request.path.substring(from: start)
+            } else {
+                method = request.path
+            }
+            
+            do {
+                let args = Args()
+                var queryParams = request.query
+                let r = request as! GCDWebServerDataRequest
+
+                if queryParams?.count == 0 {
+                    queryParams = r.jsonObject as? Dictionary<String, AnyObject>
+                }
+
+                if let queryParams = queryParams {
+                    // Get args from query params
+                    for param in queryParams {
+                        rawArgs[param.key as! String] = param.value
+
+                        if let value = ValueSerializer.deserialize(value:(param.value as? String), memory: self.memory) as Any? {
+                            // Handle nil value
+                            args.set(value: value, forName: param.key as! String)
+                        } else {
+                            args.set(value: "", forName: param.key as! String)
+                        }
+                    }
+                }
+
+                // Find and invoke the method on the RequestHandler.
+                var body: Any? = nil
+                if "release" == method {
+                    self.memory.remove(address: rawArgs["object"] as! String)
+                } else if "flushMemory" == method {
+                    self.memory.flushMemory()
+                } else{
+                    var result: Any? = nil
+                    if method.hasPrefix("query") {
+                        result = try self.queryRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("databaseConfiguration") {
+                        result = try self.databaseConfigurationRequestHandler.handleRequest(method: method, args: args)
+                    }else if method.hasPrefix("database") {
+                        result = try self.databaseRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("document") {
+                        result = try self.documentRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("dictionary") {
+                        result = try self.dictionaryRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("array") {
+                        result = try self.arrayRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("sessionAuthenticator") {
+                        result = try self.sessionauthenticatorRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("encryptionkey") {
+                        result = try self.encryptionkeyRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("blob") {
+                        result = try self.blobRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("datatype") {
+                        result = try self.datatypeRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("replicatorConfiguration") {
+                        result = try self.replicatorConfigurationRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("replicator") {
+                        result = try self.replicatorRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("expression") {
+                        result = try self.expressionRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("collation") {
+                        result = try self.collationRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("dataSource") {
+                        result = try self.dataSourceRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("function") {
+                        result = try self.functionRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("selectResult") {
+                        result = try self.selectResultRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("result") {
+                        result = try self.resultRequestHandler.handleRequest(method: method, args: args)
+                    } else if method.hasPrefix("basicAuthenticator") {
+                        result = try self.basicAuthenticatorRequestHandler.handleRequest(method: method, args: args)
+                    } else {
+                        throw ServerError.MethodNotImplemented(method)
+                    }
+                    if result != nil {
+                        body = ValueSerializer.serialize(value: result, memory: self.memory);
+                    }
+                }
+
+                if body != nil {
+                    return GCDWebServerDataResponse(text: body as! String)
+                } else {
+                    // Send 200 code and close
+                    return GCDWebServerDataResponse(text: "I-1")
+                }
+            } catch let error as NSError {
+                // Send 400 error code
+                let response = GCDWebServerDataResponse(text: error.localizedDescription)!
+                print("Error is : \(error.localizedDescription)")
+                response.statusCode = error.code as Int
+                response.contentType = error.localizedDescription
+                return response
+            }
+        }
+        server.start(withPort: kPort, bonjourName: nil)
+    }
+}
