ba9b380263b02dac255b9f977df96beb33af9c26
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-04-02 17:00:45 -0700

    Revert "Fix MemoryMap.Clear to handle already disposed objects"
    This reverts commit ffc42aa54b25ce7f8269dd9fcaf4af296464ec38.

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/MemoryMap.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/MemoryMap.cs
@@ -83,20 +83,7 @@ namespace Couchbase.Lite.Testing
                 {
                     try {
                         d.Dispose();
-                    } 
-                    catch (InvalidOperationException e)
-                    {
-                        // Ignore errors involving objects that are already disposed
-                        // TODO: tests need to be modified to not expect a new memory pointer 
-                        // everytime a set method is called on the document
-                        if (e.Message.Contains("Dispose called too many times")) {
-                            Console.WriteLine("Ignoring already disposed object: " + d.ToString());
-                        } else
-                        {
-                            throw;
-                        }
-                    }
-                    catch {
+                    } catch {
                         Console.WriteLine("Failed to dispose " + d.ToString());
                         throw;
                     }
