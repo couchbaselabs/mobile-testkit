5ce7838db205cab784c176d0af2300c4d1f15c97
Jim Borden <jim.borden@couchbase.com>
2018-04-18 07:27:47 +0900

    Fix the run script for UWP to choose the correct directory by default for running

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
@@ -5,10 +5,10 @@ param (
 $PackageFullName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_1.0.0.0_x64__1v3rwxh47wwxj"
 $LaunchName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_1v3rwxh47wwxj!App"
 if(-Not $AppDirectory) {
-    $AppDirectory = $PSScriptRoot
+    $AppDirectory = "$PSScriptRoot\AppPackages\TestServer.UWP_1.0.0.0_x64_Test"
 }
 
-Remove-AppxPackage $PackageFullName
+Remove-AppxPackage $PackageFullName -ErrorAction Ignore
 Push-Location $AppDirectory
 try {
     .\Add-AppDevPackage.ps1 -Force
