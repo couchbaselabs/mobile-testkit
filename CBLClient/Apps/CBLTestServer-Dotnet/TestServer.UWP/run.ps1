823142f7c2659987f19e4c756e1840c6ce770df5
Jim Borden <jim.borden@couchbase.com>
2018-04-18 07:49:49 +0900

    False alarm on last commit, reverting part of it

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer.UWP/run.ps1
@@ -5,7 +5,7 @@ param (
 $PackageFullName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_1.0.0.0_x64__1v3rwxh47wwxj"
 $LaunchName="b0bd6c4c-e257-44c1-aeeb-7188f65af9d0_1v3rwxh47wwxj!App"
 if(-Not $AppDirectory) {
-    $AppDirectory = "$PSScriptRoot\AppPackages\TestServer.UWP_1.0.0.0_x64_Test"
+    $AppDirectory = $PSScriptRoot
 }
 
 Remove-AppxPackage $PackageFullName -ErrorAction Ignore
