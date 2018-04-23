8ffbf2aa127b945e2db1f3f75f630c7b24d882d7
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-04-23 10:31:17 -0700

    Remove CBL 2.0 stuff from this branch

--- CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/CBLTestServer-iOS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import "GCDWebServer.h"
-#import "GCDWebServerErrorResponse.h"
-#import "GCDWebServerDataRequest.h"
-#import "GCDWebServerDataResponse.h"
-
