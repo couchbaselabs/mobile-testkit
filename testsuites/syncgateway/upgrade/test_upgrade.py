ffb2d04a05e05aad4afdf70959c3e1221dd590df
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-05-24 21:37:12 -0700

    fix flake8 issues

--- testsuites/syncgateway/upgrade/test_upgrade.py
+++ testsuites/syncgateway/upgrade/test_upgrade.py
@@ -250,6 +250,7 @@ def test_upgrade(params_from_base_test_setup):
                     raise Exception("_sync section found in docs after upgrade")
 
         # Clean up views/indexes
+        sg_obj = SyncGateway()
         sg_obj.post_upgrade_cleanup(cluster_config, sg_admin_url)
 
 
