48be6bf94c7cfe669a266b9a84dfed368bfc9cad
raghusarangapani <raghu.sarangapani@couchbase.com>
2018-06-08 13:49:31 -0700

    ssl fix for di mode (#1526)

--- libraries/testkit/sgaccel.py
+++ libraries/testkit/sgaccel.py
@@ -6,8 +6,9 @@ import requests
 import libraries.testkit.settings
 from libraries.provision.ansible_runner import AnsibleRunner
 from utilities.cluster_config_utils import is_cbs_ssl_enabled, is_xattrs_enabled, no_conflicts_enabled, get_revs_limit
-from utilities.cluster_config_utils import get_sg_replicas, get_sg_use_views, get_sg_version
+from utilities.cluster_config_utils import get_sg_replicas, get_sg_use_views, get_sg_version, sg_ssl_enabled
 from keywords.utils import add_cbs_to_sg_config_server_field
+from keywords.constants import SYNC_GATEWAY_CERT
 
 log = logging.getLogger(libraries.testkit.settings.LOGGER)
 
@@ -22,6 +23,7 @@ class SgAccel:
         self.cluster_config = cluster_config
         self.server_port = 8091
         self.server_scheme = "http"
+        self.sync_gateway_ssl = sg_ssl_enabled(self.cluster_config)
 
         if is_cbs_ssl_enabled(self.cluster_config):
             self.server_port = 18091
@@ -44,10 +46,13 @@ class SgAccel:
 
         log.info(">>> Starting sg_accel with configuration: {}".format(conf_path))
         couchbase_server_primary_node = add_cbs_to_sg_config_server_field(self.cluster_config)
+        sg_cert_path = os.path.abspath(SYNC_GATEWAY_CERT)
+
         playbook_vars = {
             "sync_gateway_config_filepath": conf_path,
             "server_port": self.server_port,
             "server_scheme": self.server_scheme,
+            "sg_cert_path": sg_cert_path,
             "autoimport": "",
             "xattrs": "",
             "no_conflicts": "",
@@ -55,6 +60,8 @@ class SgAccel:
             "num_index_replicas": "",
             "sg_use_views": "",
             "logging": "",
+            "sslcert": "",
+            "sslkey": "",
             "couchbase_server_primary_node": couchbase_server_primary_node
         }
 
@@ -71,6 +78,10 @@ class SgAccel:
         if is_xattrs_enabled(self.cluster_config):
             playbook_vars["autoimport"] = '"import_docs": "continuous",'
             playbook_vars["xattrs"] = '"enable_shared_bucket_access": true,'
+
+        if self.sync_gateway_ssl:
+            playbook_vars["sslcert"] = '"SSLCert": "sg_cert.pem",'
+            playbook_vars["sslkey"] = '"SSLKey": "sg_privkey.pem",'
 
         if no_conflicts_enabled(self.cluster_config):
             playbook_vars["no_conflicts"] = '"allow_conflicts": false,'
