9e38c0c8065bd1f9fd38f3dcb5bc1a02344b4b53
Hemant <Hemant.rajput@couchbase.com>
2018-06-15 11:31:08 +0530

    Uncommenting EncryptionCreate method

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/EncryptionKeyMethods.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/EncryptionKeyMethods.cs
@@ -11,22 +11,22 @@ namespace Couchbase.Lite.Testing
 {
     internal static class EncryptionKeyMethods
     {
-        //public static void EncryptionCreate([NotNull] NameValueCollection args,
-        //                                    [NotNull] IReadOnlyDictionary<string, object> postBody,
-        //                                    [NotNull] HttpListenerResponse response)
-        //{
-        //    var key = (byte[])postBody["key"];
-        //    var password = postBody["password"].ToString();
+        public static void EncryptionCreate([NotNull] NameValueCollection args,
+                                            [NotNull] IReadOnlyDictionary<string, object> postBody,
+                                            [NotNull] HttpListenerResponse response)
+        {
+            var key = (byte[])postBody["key"];
+            var password = postBody["password"].ToString();
 
-        //    if (password != null){
-        //        response.WriteBody(MemoryMap.New<EncryptionKey>(password));
-        //    }
-        //    else if (key != null){
-        //        response.WriteBody(MemoryMap.New<EncryptionKey>(key));
-        //    }
-        //    else{
-        //        throw new ArgumentException("Wrong Argument");
-        //    }
-        //}
+            if (password != null){
+                response.WriteBody(MemoryMap.New<EncryptionKey>(password));
+            }
+            else if (key != null){
+                response.WriteBody(MemoryMap.New<EncryptionKey>(key));
+            }
+            else{
+                throw new ArgumentException("Wrong Argument");
+            }
+        }
     }
 }
