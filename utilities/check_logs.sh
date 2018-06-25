92898008ee1829ab3901d6ccd4f612349125ee8c
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-06-25 11:58:41 -0700

    Remove case insensitive log check

--- utilities/check_logs.sh
+++ utilities/check_logs.sh
@@ -20,10 +20,10 @@ if [ -f $SYS_LOG ]; then
 fi
 
 # Array of error keys we look for in SG/SGAccel logs
-SG_ERRORS=('panic' 'data race' 'SIGSEGV' 'nil pointer dereference')
+SG_ERRORS=('panic' 'DATA RACE' 'SIGSEGV' 'nil pointer dereference')
 if [ -f $SG_LOG_FILE ]; then
 	for ((i = 0; i < ${#SG_ERRORS[@]}; i++));do
-		OUT=$(grep -i "${SG_ERRORS[$i]}" $SG_LOG_FILE)
+		OUT=$(grep "${SG_ERRORS[$i]}" $SG_LOG_FILE)
 		if [ ! -z "$OUT" ]; then
 			echo "Found ${SG_ERRORS[$i]} in $SG_LOG_FILE"
 			exit 1
@@ -33,7 +33,7 @@ fi
 
 if [ -f $SGA_LOG_FILE ]; then
 	for ((i = 0; i < ${#SG_ERRORS[@]}; i++));do
-		OUT=$(grep -i "${SG_ERRORS[$i]}" $SGA_LOG_FILE)
+		OUT=$(grep "${SG_ERRORS[$i]}" $SGA_LOG_FILE)
 		if [ ! -z "$OUT" ]; then
 			echo "Found ${SG_ERRORS[$i]} in $SGA_LOG_FILE"
 			exit 1
