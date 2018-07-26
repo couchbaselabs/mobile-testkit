66970a88ae49a08241c7b7d9d789a77532ecd583
s_sridevi <s_sridevi15@yahoo.com>
2018-07-26 14:24:31 -0700

    removed code for socket hosts and ports

---
+++ CBLClient/Apps/CBLTestServer-iOS/CBLTestServer-iOS/Server/ReplicatorTcpClientConnection.swift
@@ -0,0 +1,260 @@
+//
+//  ReplicatorTcpClientConnection.swift
+//  CouchbaseLite
+//
+//  Copyright (c) 2018 Couchbase, Inc. All rights reserved.
+//
+//  Licensed under the Couchbase License Agreement (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  https://info.couchbase.com/rs/302-GJY-034/images/2017-10-30_License_Agreement.pdf
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import CouchbaseLiteSwift
+
+private typealias ClientCompletionHandler = (Bool, Error?) -> Void
+
+private class PendingWrite {
+    let data: Data
+    let completion: ClientCompletionHandler?
+    var bytesWritten = 0
+    
+    init(data: Data, completion: ClientCompletionHandler?) {
+        self.data = data
+        self.completion = completion
+    }
+}
+
+/// MessageEndpointConnection implemenation used by the ReplicatorTcpListener.
+class ReplicatorTcpClientConnection : NSObject {
+    fileprivate let kReadBufferSize = 1024
+    
+    fileprivate let queue = DispatchQueue(label: "ReplicatorTcpServerConnection")
+    
+    fileprivate var url: URL!
+    
+    fileprivate var expectedAcceptHeader: String?
+    
+    fileprivate var response = CFHTTPMessageCreateEmpty(kCFAllocatorDefault, true).takeRetainedValue()
+    
+    fileprivate var openCompletion: ((Bool, MessagingError?) -> Void)?
+    
+    fileprivate let inputStream: InputStream
+    
+    fileprivate let outputStream: OutputStream
+    
+    fileprivate var replConnection: ReplicatorConnection!
+    
+    fileprivate var pendingWrites: [PendingWrite] = []
+    
+    fileprivate var hasSpace = false
+    
+    fileprivate var opened = false
+    
+    init(inputStream: InputStream, outputStream: OutputStream) {
+        self.inputStream = inputStream
+        self.outputStream = outputStream
+    }
+    
+    convenience init(url: URL) {
+        var input: InputStream?
+        var output: OutputStream?
+        Stream.getStreamsToHost(withName: url.host!, port: url.port!, inputStream: &input, outputStream: &output)
+        self.init(inputStream: input!, outputStream: output!)
+        self.url = url
+    }
+    
+    func open() {
+        inputStream.delegate = self
+        outputStream.delegate = self
+        
+        CFReadStreamSetDispatchQueue(inputStream, queue)
+        CFWriteStreamSetDispatchQueue(outputStream, queue)
+        
+        inputStream.open()
+        outputStream.open()
+    }
+    
+    fileprivate func closeStreams() {
+        inputStream.close()
+        outputStream.close()
+    }
+}
+
+/// MessageEndpointConnection
+extension ReplicatorTcpClientConnection: MessageEndpointConnection {
+    public func open(connection: ReplicatorConnection, completion: @escaping (Bool, MessagingError?) -> Void) {
+        replConnection = connection
+        openCompletion = completion
+        open()
+        sendWebSocketRequest()
+    }
+    
+    func sendWebSocketRequest() {
+        var keyBytes: [UInt8] = [UInt8](repeating: 0, count: 16)
+        _ = SecRandomCopyBytes(kSecRandomDefault, 16, &keyBytes)
+        let key = Data(bytes: keyBytes).base64EncodedString()
+        expectedAcceptHeader = key.appending("258EAFA5-E914-47DA-95CA-C5AB0DC85B11").sha1Base64()
+        let request = CFHTTPMessageCreateRequest(kCFAllocatorDefault, "GET" as CFString, url as CFURL, kCFHTTPVersion1_1).takeRetainedValue()
+        CFHTTPMessageSetHeaderFieldValue(request, "Connection" as CFString, "Upgrade" as CFString)
+        CFHTTPMessageSetHeaderFieldValue(request, "Upgrade" as CFString, "websocket" as CFString)
+        CFHTTPMessageSetHeaderFieldValue(request, "Sec-WebSocket-Version" as CFString, "13" as CFString)
+        CFHTTPMessageSetHeaderFieldValue(request, "Sec-WebSocket-Key" as CFString, key as CFString)
+        CFHTTPMessageSetHeaderFieldValue(request, "Sec-WebSocket-Protocol" as CFString, "BLIP_3+CBMobile_2" as CFString)
+        let data = CFHTTPMessageCopySerializedMessage(request)!.takeRetainedValue() as Data
+        write(data: data, completion: nil)
+    }
+    
+    public func close(error: Error?, completion: @escaping () -> Void) {
+        closeStreams()
+        completion()
+    }
+    
+    public func send(message: Message, completion: @escaping (Bool, MessagingError?) -> Void) {
+        write(data: message.toData()) { (success, error) in
+            completion(success, error?.toMessagingError(isRecoverable: false))
+        }
+    }
+    
+    fileprivate func connected() {
+        opened = true
+        openCompletion!(true, nil)
+    }
+    
+    fileprivate func closeConnection(error: Error?) {
+        closeStreams()
+        replConnection.close(error: error?.toMessagingError(isRecoverable: false))
+    }
+}
+
+/// StreamDelegate and Read/Write stream
+extension ReplicatorTcpClientConnection: StreamDelegate {
+    public func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        switch eventCode {
+        case Stream.Event.hasBytesAvailable:
+            doRead()
+        case Stream.Event.endEncountered:
+            closeConnection(error: nil)
+        case Stream.Event.errorOccurred:
+            closeConnection(error: aStream.streamError)
+        case Stream.Event.hasSpaceAvailable:
+            hasSpace = true
+            doWrite()
+        default:
+            break
+        }
+    }
+    
+    fileprivate func write(data: Data, completion: ClientCompletionHandler?) {
+        queue.async {
+            self.pendingWrites.append(PendingWrite.init(data: data, completion: completion))
+            self.doWrite()
+        }
+    }
+    
+    private func doWrite() {
+        if !hasSpace {
+            return
+        }
+        
+        while !pendingWrites.isEmpty {
+            let w = pendingWrites[0]
+            let nBytes = w.data.withUnsafeBytes { outputStream.write(
+                $0 + w.bytesWritten, maxLength: w.data.count - w.bytesWritten) }
+            if nBytes <= 0 {
+                hasSpace = false
+                return
+            }
+            w.bytesWritten = w.bytesWritten + nBytes
+            if w.bytesWritten < w.data.count {
+                hasSpace = false
+                return;
+            }
+            if let completion = w.completion {
+                completion(true, nil)
+            }
+            pendingWrites.remove(at: 0)
+        }
+    }
+    
+    private func doRead() {
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: kReadBufferSize)
+        while inputStream.hasBytesAvailable {
+            let length = inputStream.read(buffer, maxLength: kReadBufferSize)
+            if length <= 0 {
+                break
+            }
+            if opened {
+                receiveBytes(buffer: buffer, length: length)
+            } else {
+                receivedHTTPResponseBytes(buffer: buffer, length: length)
+            }
+        }
+        buffer.deallocate()
+    }
+    
+    private func receiveBytes(buffer: UnsafeMutablePointer<UInt8>, length: Int) {
+        let data = Data(bytes: buffer, count: length)
+        replConnection.receive(messge: Message.fromData(data))
+    }
+    
+    private func receivedHTTPResponseBytes(buffer: UnsafeMutablePointer<UInt8>, length: Int) {
+        if !CFHTTPMessageAppendBytes(response, buffer, length) {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        
+        if CFHTTPMessageIsHeaderComplete(response) {
+            verifyWebSocketRequestResponse()
+        }
+    }
+    
+    private func verifyWebSocketRequestResponse() {
+        let code = CFHTTPMessageGetResponseStatusCode(response) as Int
+        if code != 101 {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        guard let header: NSDictionary = CFHTTPMessageCopyAllHeaderFields(response)?.takeRetainedValue() else {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        guard let v1 = header["Connection"] as? String, v1.caseInsensitiveCompare("Upgrade") == .orderedSame else {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        guard let v2 = header["Upgrade"] as? String, v2.caseInsensitiveCompare("websocket") == .orderedSame else {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        guard let v3 = header["Sec-WebSocket-Accept"] as? String, v3 == expectedAcceptHeader! else {
+            closeConnection(error: NSError(domain: NSURLErrorDomain, code: NSURLErrorBadServerResponse, userInfo: nil))
+            return
+        }
+        // Success:
+        connected()
+    }
+}
+
+private extension String {
+    func sha1Base64() -> String {
+        let data = self.data(using: String.Encoding.ascii)!
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes { _ = CC_SHA1($0, CC_LONG(data.count), &digest) }
+        return Data(bytes: digest).base64EncodedString()
+    }
+}
+
+private extension Error {
+    func toMessagingError(isRecoverable: Bool) -> MessagingError {
+        return MessagingError.init(error: self, isRecoverable: isRecoverable)
+    }
+}
+
--- testsuites/CBLTester/System_test_multiple_device/test_peer_to_peer.py
+++ testsuites/CBLTester/System_test_multiple_device/test_peer_to_peer.py
@@ -35,18 +35,11 @@ def test_peer_to_peer_iosAndroid(params_from_base_suite_setup, num_of_docs, cont
         7. Check the revision list for the doc
     """
     sg_db = "db"
-    sg_url = params_from_base_suite_setup["sg_url"]
     sg_admin_url = params_from_base_suite_setup["sg_admin_url"]
-    sg_mode = params_from_base_suite_setup["mode"]
     cluster_config = params_from_base_suite_setup["cluster_config"]
-    sg_blip_url = params_from_base_suite_setup["target_url"]
-    no_conflicts_enabled = params_from_base_suite_setup["no_conflicts_enabled"]
-    sync_gateway_version = params_from_base_suite_setup["sync_gateway_version"]
     num_of_docs = 10
-    channels = ["ABC"]
     base_url_list = params_from_base_suite_setup["base_url_list"]
-    socket_host_list = params_from_base_suite_setup["socket_host_list"]
-    socket_port_list = params_from_base_suite_setup["socket_port_list"]
+    host_list = params_from_base_suite_setup["host_list"]
     cbl_db_list = params_from_base_suite_setup["cbl_db_list"]
     db_obj_list = params_from_base_suite_setup["db_obj_list"]
     db_name_list = params_from_base_suite_setup["db_name_list"]
@@ -54,8 +47,6 @@ def test_peer_to_peer_iosAndroid(params_from_base_suite_setup, num_of_docs, cont
     username = "autotest"
     password = "password"
     channel = ["peerToPeer"]
-    # continuous = True
-    # replication_type = "push_pull"
 
     # Reset cluster to ensure no data in system
     cluster = Cluster(config=cluster_config)
@@ -63,7 +54,7 @@ def test_peer_to_peer_iosAndroid(params_from_base_suite_setup, num_of_docs, cont
     sg_client = MobileRestClient()
     sg_client.create_user(sg_admin_url, sg_db, username, password, channels=channel)
     cookie, session = sg_client.create_session(sg_admin_url, sg_db, username)
-    
+
     base_url_client = base_url_list[1]
     base_url_server = base_url_list[0]
     replicator = Replication(base_url_client)
@@ -72,32 +63,23 @@ def test_peer_to_peer_iosAndroid(params_from_base_suite_setup, num_of_docs, cont
 
     peerToPeer_client = PeerToPeer(base_url_client)
     peerToPeer_server = PeerToPeer(base_url_server)
-    # for base_url in zip(base_url_list):
     cbl_db_server = cbl_db_list[0]
     db_obj_server = db_obj_list[0]
     cbl_db_client = cbl_db_list[1]
     db_obj_client = db_obj_list[1]
     db_name_server = db_name_list[0]
-    
-    server_host = socket_host_list[0]
-    server_port = socket_port_list[0]
-    client_host = socket_host_list[1]
-    client_port = socket_port_list[1]
 
+    server_host = host_list[0]
     db_obj_client.create_bulk_docs(num_of_docs, "cbl-peerToPeer", db=cbl_db_client, channels=channel)
-    server_port_re = int(server_port)
-    client_port_re = int(client_port)
     peerToPeer_server.server_start(cbl_db_server)
     print "server starting ....."
 
-    ## Now set up client
-
-    repl = peerToPeer_client.client_start(host=server_host, port=server_port_re, server_db_name=db_name_server, client_database=cbl_db_client, continuous=continuous, authenticator=replicator_authenticator, replication_type=replicator_type)
-    time.sleep(10)
+    # Now set up client
+    repl = peerToPeer_client.client_start(host=server_host, server_db_name=db_name_server, client_database=cbl_db_client, continuous=continuous, authenticator=replicator_authenticator, replication_type=replicator_type)
     replicator.wait_until_replicator_idle(repl)
     total = replicator.getTotal(repl)
     completed = replicator.getCompleted(repl)
-    assert total == completed, "replication from client to server did not completed "+ total + " not equal to "+ completed
+    assert total == completed, "replication from client to server did not completed " + total + " not equal to " + completed
     server_docs_count = db_obj_server.getCount(cbl_db_server)
     assert server_docs_count == num_of_docs, "Number of docs is not equivalent to number of docs in server "
-    
+    replicator.stop(repl)
