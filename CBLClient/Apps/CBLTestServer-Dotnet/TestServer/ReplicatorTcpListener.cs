6f89c460f0201f01b7c31c822272d9de553d64a3
Raghu Sarangapani <raghu.sarangapani@couchbase.com>
2018-07-30 11:47:52 -0700

    Just have 1 p2p server

--- CBLClient/Apps/CBLTestServer-Dotnet/TestServer/ReplicatorTcpListener.cs
+++ CBLClient/Apps/CBLTestServer-Dotnet/TestServer/ReplicatorTcpListener.cs
@@ -1,33 +1,14 @@
-// 
-//  ReplicatorTcpListener.cs
-// 
-//  Copyright (c) 2018 Couchbase, Inc All rights reserved.
-// 
-//  Licensed under the Couchbase License Agreement (the "License");
-//  you may not use this file except in compliance with the License.
-//  You may obtain a copy of the License at
-//  https://info.couchbase.com/rs/302-GJY-034/images/2017-10-30_License_Agreement.pdf
-// 
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS,
-//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//  See the License for the specific language governing permissions and
-//  limitations under the License.
-
+using Couchbase.Lite.P2P;
 using System;
-using System.IO;
-using System.Linq;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using Couchbase.Lite.P2P;
 
 namespace Couchbase.Lite.Testing
 {
-
-    public sealed class ReplicatorTcpListenerMsg
+    public sealed class ReplicatorTcpListener
     {
         private TcpClient _connectedTcpClient;
         private TcpListener _listener;
@@ -38,7 +19,7 @@ namespace Couchbase.Lite.Testing
         /// Constructs a new replicator Passive Peer Tcp listener
         /// </summary>
         /// <param name="endpointListener">used to accept any incoming peer tcp client connection</param>
-        public ReplicatorTcpListenerMsg(MessageEndpointListener endpointListener)
+        public ReplicatorTcpListener(MessageEndpointListener endpointListener)
         {
             _endpointListener = endpointListener;
         }
@@ -52,7 +33,7 @@ namespace Couchbase.Lite.Testing
             {
                 try
                 {
-                    _listener = new TcpListener(IPAddress.Any, ReplicatorTcpConnection.Port);
+                    _listener = new TcpListener(IPAddress.Any, TcpMessageEndpointConnection.Port);
                     _listener.Start();
                     _opened = true;
                     AcceptLoop();
