aad850d4dad9cfd382109dfb270bdf52a7f81e98
Sridevi <sridevi.saragadam@couchbase.com>
2018-08-06 11:19:50 -0700

    Update couchbaseserver.py
    added GA build for couchbase server

--- keywords/couchbaseserver.py
+++ keywords/couchbaseserver.py
@@ -793,6 +793,7 @@ class CouchbaseServer:
         Return the base_url of the package download URL (everything except the filename)
         """
         released_versions = {
+            "5.5.0": "2958",
             "5.1.0": "5552",
             "5.0.1": "5003",
             "5.0.0": "3519",
