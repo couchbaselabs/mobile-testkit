018059c27ce919d426e27683b198a79fda0a6af1
Hemant <Hemant.rajput@couchbase.com>
2019-05-07 12:28:30 +0530

    Saving fixes for password redaction

--- testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_log_redaction.py
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_log_redaction.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import pytest
 
@@ -7,15 +8,16 @@ from CBLClient.Authenticator import Authenticator
 from libraries.provision.ansible_runner import AnsibleRunner
 
 from libraries.testkit import cluster
+from keywords.utils import log_info
 
 
 @pytest.mark.sanity
 @pytest.mark.listener
 @pytest.mark.replication
 @pytest.mark.parametrize("password", [
-    ("auto-password"),
-    ("auto password"),
-    ("validpassword"),
+    "auto-password",
+    "auto password",
+    "validpassword",
 ])
 def test_mask_password_in_logs(params_from_base_test_setup, password):
     """
@@ -40,15 +42,14 @@ def test_mask_password_in_logs(params_from_base_test_setup, password):
 
     num_cbl_docs = 5
     # Cannot run on iOS as there is no support in xcode to grab cbl logs
-    if sync_gateway_version < "2.0.0" or liteserv_platform.lower() == "ios":
-        pytest.skip('This test cannnot run with sg version below 2.0 and if platform is iOS')
+    if sync_gateway_version < "2.0.0" and log_file is not None:
+        pytest.skip('This test cannot run with sg version below 2.0 and File logging not enabled.')
 
     channels = ["ABC"]
     c = cluster.Cluster(config=cluster_config)
     c.reset(sg_config_path=sg_config)
     # Clean up tmp logs before test runs
-    if liteserv_platform == "net-msft":
-        delete_tmp_logs()
+    delete_tmp_logs()
     replicator = Replication(base_url)
     authenticator = Authenticator(base_url)
 
@@ -60,7 +61,8 @@ def test_mask_password_in_logs(params_from_base_test_setup, password):
     sg_client.create_user(sg_admin_url, sg_db, "autotest", password=password, channels=channels)
     sg_client.create_session(sg_admin_url, sg_db, "autotest")
 
-    replicator_authenticator = authenticator.authentication(username="autotest", password=password, authentication_type="basic")
+    replicator_authenticator = authenticator.authentication(username="autotest", password=password,
+                                                            authentication_type="basic")
     repl_config = replicator.configure(cbl_db, target_url=sg_blip_url, continuous=False,
                                        channels=channels, replicator_authenticator=replicator_authenticator)
     repl = replicator.create(repl_config)
@@ -74,11 +76,11 @@ def test_mask_password_in_logs(params_from_base_test_setup, password):
 @pytest.mark.listener
 @pytest.mark.replication
 @pytest.mark.parametrize("invalid_password", [
-    ("auto-password"),
-    ("auto password"),
-    ("invalidpassword"),
+    "auto-password",
+    "auto password",
+    "invalidpassword",
 ])
-def test_verify_invalid_maskPassword_in_logs(params_from_base_test_setup, invalid_password):
+def test_verify_invalid_mask_password_in_logs(params_from_base_test_setup, invalid_password):
     """
         @summary:
         1. Create CBL DB and create bulk doc in CBL
@@ -101,29 +103,29 @@ def test_verify_invalid_maskPassword_in_logs(params_from_base_test_setup, invali
 
     num_cbl_docs = 5
     # Cannot run on iOS as there is no support in xcode to grab cbl logs
-    if sync_gateway_version < "2.0.0" or liteserv_platform.lower() == "ios":
-        pytest.skip('This test cannnot run with sg version below 2.0 and if platform is iOS')
-
+    if sync_gateway_version < "2.0.0" and log_file is not None:
+        pytest.skip('This test cannot run with sg version below 2.0 and File logging not enabled.')
+ 
     channels = ["ABC"]
     c = cluster.Cluster(config=cluster_config)
     c.reset(sg_config_path=sg_config)
-
-    if liteserv_platform == "net-msft":
-        delete_tmp_logs()  # Clean up tmp logs before test runs
+  
+    delete_tmp_logs()  # Clean up tmp logs before test runs
     replicator = Replication(base_url)
     authenticator = Authenticator(base_url)
-
+  
     sg_client = MobileRestClient()
     db.create_bulk_docs(number=num_cbl_docs, id_prefix="cblid", db=cbl_db, channels=channels)
-
+  
     # Add docs in SG
     sg_client.create_user(sg_admin_url, sg_db, "autotest", password="password", channels=channels)
     sg_client.create_session(sg_admin_url, sg_db, "autotest")
-
-    replicator_authenticator = authenticator.authentication(username="autotest", password=invalid_password, authentication_type="basic")
+  
+    replicator_authenticator = authenticator.authentication(username="autotest", password=invalid_password,
+                                                            authentication_type="basic")
     repl_config = replicator.configure(cbl_db, target_url=sg_blip_url, continuous=False,
                                        channels=channels, replicator_authenticator=replicator_authenticator)
-
+  
     repl = replicator.create(repl_config)
     replicator.start(repl)
     replicator.wait_until_replicator_idle(repl, err_check=False)
@@ -132,28 +134,40 @@ def test_verify_invalid_maskPassword_in_logs(params_from_base_test_setup, invali
 
 
 def verify_password_masked(liteserv_platform, log_file, password):
-    if liteserv_platform.lower() == "android":
-        log_file = log_file.replace(" ", "\ ")
-
-    if liteserv_platform == "net-msft":
-        log_full_path = "/tmp/cbl-logs/"
+    """
+    @note: Porting logs for Android, xamarin-android, net-core and net-uwp platform, as the logs reside
+           outside runner's file directory
+    """
+    log_full_path = "/tmp/cbl-logs/"
+    if liteserv_platform.lower() == "android" or liteserv_platform.lower() == "xamarin-android":
+        os.mkdir(log_full_path)
+        log_info("Running Command: 'adb pull {} {}".format(log_file, log_full_path))
+        cmd = ["adb", "pull", log_file, log_full_path]
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()
+        log_info(out, err)
+        log_file = os.path.join(log_full_path, os.path.basename(log_file))
+    elif liteserv_platform.lower() == "net-msft" or liteserv_platform.lower() == "net-uwp":
+        
         config_location = "resources/liteserv_configs/net-msft"
         ansible_runner = AnsibleRunner(config=config_location)
-
+        os.mkdir(log_full_path)
         status = ansible_runner.run_ansible_playbook(
             "fetch-windows-cbl-logs.yml",
             extra_vars={
-                "log_full_path": log_full_path
+                "log_full_path": log_full_path,
+                "log_file": log_file
             }
         )
         if status != 0:
             raise Exception("Could not fetch cbl logs from windows ")
 
-        log_file = subprocess.check_output("ls -t {}* | head -1".format(log_full_path), shell=True)
+        log_info("Checking {} for copied log files - {}".format(log_full_path, os.listdir(log_full_path)))
+        log_file = subprocess.check_output("ls -t {} | head -1".format(log_full_path), shell=True)
 
-    password = password.replace(" ", "\ ")
-    log_file = log_file.strip()
-    command = "grep {} {} | wc -l".format(password, log_file)
+    assert len(os.listdir(log_full_path)) != 0, "Log files are not copied to {}".format(log_full_path)
+    command = "grep '{}' {}/*.cbllog | wc -l".format(password, log_file)
+    log_info("Running command: {}".format(command))
     output = subprocess.check_output(command, shell=True)
     output = int(output.strip())
     assert output == 0, "password showed up in clear text in logs"
@@ -161,4 +175,4 @@ def verify_password_masked(liteserv_platform, log_file, password):
 
 def delete_tmp_logs():
     del_output = subprocess.check_output("rm -rf /tmp/cbl-logs", shell=True)
-    print "delete output is ", del_output
+    log_info("delete output is ", del_output)
