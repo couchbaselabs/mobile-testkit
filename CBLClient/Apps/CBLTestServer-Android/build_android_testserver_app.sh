a58e4c1fbcc331629472db7369025aaf5be104ea
G. Blake Meike <blake.meike@couchbase.com>
2019-06-24 15:20:06 -0700

    Improved build script

--- CBLClient/Apps/CBLTestServer-Android/build_android_testserver_app.sh
+++ CBLClient/Apps/CBLTestServer-Android/build_android_testserver_app.sh
@@ -2,30 +2,50 @@
 
 EDITION=$1
 VERSION=$2
-BLD_NUM=$3
+BUILD_NUM=$3
 
-export ANDROID_HOME=/home/couchbase/jenkins/tools/android-sdk
-export PATH=$PATH:$ANDROID_SDK_HOME/tools:$ANDROID_SDK_HOME/platform-tools
-export MAVEN_UPLOAD_VERSION=${VERSION}-${BLD_NUM}
+FAIL=0
+if [ -z "${ANDROID_HOME}" ] || [ ! -d "${ANDROID_HOME}" ]; then
+    echo "Not found: ANDROID_HOME = '$ANDROID_HOME'"
+    FAIL=1
+fi
 
-cd ${WORKSPACE}/mobile-testkit/CBLClient/Apps/CBLTestServer-Android
+if [ -z "${ARTIFACTS_DIR}" ] || [ ! -d "${ARTIFACTS_DIR}" ]; then
+    echo "Not found: ARTIFACTS_DIR = '$ARTIFACTS_DIR'"
+    FAIL=1
+fi
+
+if [ -z "${EDITION}" ]; then
+    echo 'Undefined: param #1 (edition)'
+    FAIL=1
+fi
+
+if [ -z "${VERSION}" ]; then
+    echo 'Undefined: param #2 (version)'
+    FAIL=1
+fi
+
+if [ -z "${BUILD_NUM}" ]; then
+    echo 'Undefined: param #3 (build-num)'
+    FAIL=1
+fi
+
+if [ $FAIL -ne 0 ]; then
+    echo "Exiting"
+    exit $FAIL
+fi
+
+export MAVEN_UPLOAD_VERSION=${VERSION}-${BUILD_NUM}
+echo "Building version ${MAVEN_UPLOAD_VERSION}"
+
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
 
 # Build TestServer
-echo ./gradlew clean && ./gradlew -Dversion=${MAVEN_UPLOAD_VERSION} assemble
-./gradlew clean && ./gradlew -Dversion=${MAVEN_UPLOAD_VERSION} assemble
+echo ./gradlew clean -Dversion=${MAVEN_UPLOAD_VERSION} assemble
+./gradlew clean -Dversion=${MAVEN_UPLOAD_VERSION} assemble
 
 TESTSERVER_DEBUG_APK=app/build/outputs/apk/debug/app-debug.apk
-TESTSERVER_RELEASE_APK=app/build/outputs/apk/release/app-release.apk
-if [ -e ${TESTSERVER_DEBUG_APK} ]
-then
-    cp -f ${TESTSERVER_DEBUG_APK} ${WORKSPACE}/artifacts_${EDITION}/CBLTestServer-Android-${MAVEN_UPLOAD_VERSION}-${EDITION}-debug.apk
-else
-    exit 1
-fi
+cp -f ${TESTSERVER_DEBUG_APK} ${ARTIFACTS_DIR}/CBLTestServer-Android-${MAVEN_UPLOAD_VERSION}-${EDITION}-debug.apk
 
-if [ -e ${TESTSERVER_RELEASE_APK} ]
-then
-    cp -f ${TESTSERVER_RELEASE_APK} ${WORKSPACE}/artifacts_${EDITION}/CBLTestServer-Android-${MAVEN_UPLOAD_VERSION}-${EDITION}-release.apk
-else
-    exit 1
-fi
+TESTSERVER_RELEASE_APK=app/build/outputs/apk/release/app-release.apk
+cp -f ${TESTSERVER_RELEASE_APK} ${ARTIFACTS_DIR}/CBLTestServer-Android-${MAVEN_UPLOAD_VERSION}-${EDITION}-release.apk
