2409b0992e7a017169e68f9c104db344c76e5a71
Hemant <Hemant.rajput@couchbase.com>
2019-06-25 18:07:07 +0530

    Saving work

--- testsuites/CBLTester/upgrade_tests/test_cbl_upgrade.py
+++ testsuites/CBLTester/upgrade_tests/test_cbl_upgrade.py
@@ -1,6 +1,7 @@
 import pytest
 import random
 import time
+import os
 
 from CBLClient.Database import Database
 from keywords.utils import log_info
@@ -33,70 +34,21 @@ def test_upgrade_cbl(params_from_base_suite_setup):
         b. Update docs for migrated db and replicate to cluster
         c. Delete docs from migrated db and replicate to cluster
     """
-    base_liteserv_version = params_from_base_suite_setup["base_liteserv_version"]
-    upgraded_liteserv_version = params_from_base_suite_setup["upgraded_liteserv_version"]
-    liteserv_platform = params_from_base_suite_setup["liteserv_platform"]
-    upgrade_cbl_db_name = "upgarded_db"
     base_url = params_from_base_suite_setup["base_url"]
-    encrypted_db = params_from_base_suite_setup["encrypted_db"]
-    db_password = params_from_base_suite_setup["db_password"]
     sg_db = "db"
     sg_admin_url = params_from_base_suite_setup["sg_admin_url"]
     sg_blip_url = params_from_base_suite_setup["target_url"]
     cluster_config = params_from_base_suite_setup["cluster_config"]
     sg_config = params_from_base_suite_setup["sg_config"]
     cbs_ip = params_from_base_suite_setup["cbs_ip"]
-    utils_obj = params_from_base_suite_setup["utils_obj"]
     server_url = params_from_base_suite_setup["server_url"]
-
-    if base_liteserv_version > upgraded_liteserv_version:
-        pytest.skip("Can't upgrade from higher version db to lower version db")
-
-    supported_base_liteserv = ["1.4", "2.0.0", "2.1.5", "2.5.0"]
     db = Database(base_url)
-    if encrypted_db:
-        if base_liteserv_version < "2.1.5":
-            pytest.skip("Encyption is supported from 2.1.0 onwards."
-                        "{} doesn't have encrypted db upgrade support".format(base_liteserv_version))
-        db_config = db.configure(password=db_password)
-        db_prefix = "travel-sample-encrypted"
-    else:
-        db_config = db.configure()
-        db_prefix = "travel-sample"
 
-    temp_db = db.create("temp_db", db_config)
-    time.sleep(1)
-    new_db_path = db.getPath(temp_db)
-    delimiter = "/"
-    if liteserv_platform == "net-msft" or liteserv_platform == "net-uwp":
-        delimiter = "\\"
-    new_db_path = "{}".format(delimiter).join(new_db_path.split(delimiter)[:-2]) +\
-                  "{}{}.cblite2".format(delimiter, upgrade_cbl_db_name)
-    db.deleteDB(temp_db)
-
-    old_liteserv_db_name = ""
-    if base_liteserv_version in supported_base_liteserv:
-        old_liteserv_db_name = db_prefix + "-" + base_liteserv_version
-    else:
-        pytest.skip("Run test with one of supported base liteserv version - ".format(supported_base_liteserv))
-
-    if liteserv_platform == "android":
-        prebuilt_db_path = "/assets/{}.cblite2.zip".format(old_liteserv_db_name)
-    elif liteserv_platform == "xamarin-android":
-        prebuilt_db_path = "{}.cblite2.zip".format(old_liteserv_db_name)
-    else:
-        prebuilt_db_path = "Databases/{}.cblite2".format(old_liteserv_db_name)
-
-    log_info("Copying db of CBL-{} to CBL-{}".format(base_liteserv_version, upgraded_liteserv_version))
-    prebuilt_db_path = db.get_pre_built_db(prebuilt_db_path)
-    assert "Copied" == utils_obj.copy_files(prebuilt_db_path, new_db_path)
-    cbl_db = db.create(upgrade_cbl_db_name, db_config)
-    assert isinstance(cbl_db, MemoryPointer), "Failed to migrate db from previous version of CBL"
+    cbl_db, upgrade_cbl_db_name = _upgrade_db(params_from_base_suite_setup)
     cbl_doc_ids = db.getDocIds(cbl_db, limit=40000)
+    assert len(cbl_doc_ids) == 31591
     get_doc_id_from_cbs_query = 'select meta().id from `{}` where meta().id not' \
                                 ' like "_sync%" ORDER BY id'.format("travel-sample")
-    assert len(cbl_doc_ids) == 31591
-
     # Replicating docs to CBS
     sg_client = MobileRestClient()
     replicator = Replication(base_url)
@@ -254,3 +206,118 @@ def test_upgrade_cbl(params_from_base_suite_setup):
     if db.exists(upgrade_cbl_db_name, db_path):
         log_info("Delete DB - {}".format(upgrade_cbl_db_name))
         db.deleteDB(cbl_db)
+
+
+@pytest.mark.listener
+@pytest.mark.upgrade_test
+def test_queries_on_upgrade_cbl(params_from_base_suite_setup):
+    """
+    @summary:
+    1. Upgrade the db from lower CBl version
+    2. Run all the query tests on upgraded db
+    """
+    upgraded_liteserv_version = params_from_base_suite_setup["upgraded_liteserv_version"]
+    liteserv_host = params_from_base_suite_setup["liteserv_host"]
+    sg_version = params_from_base_suite_setup["sg_version"]
+    server_version = params_from_base_suite_setup["server_version"]
+    liteserv_platform = params_from_base_suite_setup["liteserv_platform"]
+    base_url = params_from_base_suite_setup["base_url"]
+    sg_db = "db"
+    sg_admin_url = params_from_base_suite_setup["sg_admin_url"]
+    sg_blip_url = params_from_base_suite_setup["target_url"]
+    cluster_config = params_from_base_suite_setup["cluster_config"]
+    sg_config = params_from_base_suite_setup["sg_config"]
+
+    cbl_db, upgraded_cbl_db_name = _upgrade_db(params_from_base_suite_setup)
+
+    # Replicating docs to CBS
+    sg_client = MobileRestClient()
+    replicator = Replication(base_url)
+    username = "autotest"
+    password = "password"
+
+    # Reset cluster to ensure no data in system
+    c = Cluster(config=cluster_config)
+    c.reset(sg_config_path=sg_config)
+
+    sg_client.create_user(sg_admin_url, sg_db, username, password)
+    authenticator = Authenticator(base_url)
+    cookie, session_id = sg_client.create_session(sg_admin_url, sg_db, username)
+    replicator_authenticator = authenticator.authentication(session_id, cookie, authentication_type="session")
+    repl_config = replicator.configure(cbl_db, sg_blip_url, replication_type="push", continuous=True,
+                                       replicator_authenticator=replicator_authenticator)
+    repl = replicator.create(repl_config)
+    replicator.start(repl)
+    replicator.wait_until_replicator_idle(repl, sleep_time=10, max_times=500)
+    total = replicator.getTotal(repl)
+    completed = replicator.getCompleted(repl)
+    assert total == completed
+    replicator.stop(repl)
+
+    # Runing Query tests
+    log_info("Running Query tests")
+    directory = os.getcwd()
+    os.chdir(directory)
+    cmd = ["{}/testsuites/CBLTester/CBL_Functional_tests/SuiteSetup_FunctionalTests".format(os.getcwd()),
+           "--liteserv-version={}".format(upgraded_liteserv_version), "--skip-provisioning",
+           "--liteserv-host={}".format(liteserv_host), "--liteserv-port=8080",
+           "--sync-gateway-version={}".format(sg_version), "--mode=cc", "--server-version={}".format(server_version),
+           "--liteserv-platform={}".format(liteserv_platform), "--create-db-per-suite={}".format(upgraded_cbl_db_name)
+           ]
+    pytest.main(cmd)
+
+
+def _upgrade_db(args):
+    base_liteserv_version = args["base_liteserv_version"]
+    upgraded_liteserv_version = args["upgraded_liteserv_version"]
+    liteserv_platform = args["liteserv_platform"]
+    upgrade_cbl_db_name = "upgarded_db"
+    base_url = args["base_url"]
+    encrypted_db = args["encrypted_db"]
+    db_password = args["db_password"]
+    utils_obj = args["utils_obj"]
+
+    if base_liteserv_version > upgraded_liteserv_version:
+        pytest.skip("Can't upgrade from higher version db to lower version db")
+
+    supported_base_liteserv = ["1.4", "2.0.0", "2.1.5", "2.5.0"]
+    db = Database(base_url)
+    if encrypted_db:
+        if base_liteserv_version < "2.1.5":
+            pytest.skip("Encyption is supported from 2.1.0 onwards."
+                        "{} doesn't have encrypted db upgrade support".format(base_liteserv_version))
+        db_config = db.configure(password=db_password)
+        db_prefix = "travel-sample-encrypted"
+    else:
+        db_config = db.configure()
+        db_prefix = "travel-sample"
+
+    temp_db = db.create("temp_db", db_config)
+    time.sleep(1)
+    new_db_path = db.getPath(temp_db)
+    delimiter = "/"
+    if liteserv_platform == "net-msft" or liteserv_platform == "net-uwp":
+        delimiter = "\\"
+    new_db_path = "{}".format(delimiter).join(new_db_path.split(delimiter)[:-2]) +\
+                  "{}{}.cblite2".format(delimiter, upgrade_cbl_db_name)
+    db.deleteDB(temp_db)
+
+    old_liteserv_db_name = ""
+    if base_liteserv_version in supported_base_liteserv:
+        old_liteserv_db_name = db_prefix + "-" + base_liteserv_version
+    else:
+        pytest.skip("Run test with one of supported base liteserv version - ".format(supported_base_liteserv))
+
+    if liteserv_platform == "android":
+        prebuilt_db_path = "/assets/{}.cblite2.zip".format(old_liteserv_db_name)
+    elif liteserv_platform == "xamarin-android":
+        prebuilt_db_path = "{}.cblite2.zip".format(old_liteserv_db_name)
+    else:
+        prebuilt_db_path = "Databases/{}.cblite2".format(old_liteserv_db_name)
+
+    log_info("Copying db of CBL-{} to CBL-{}".format(base_liteserv_version, upgraded_liteserv_version))
+    prebuilt_db_path = db.get_pre_built_db(prebuilt_db_path)
+    assert "Copied" == utils_obj.copy_files(prebuilt_db_path, new_db_path)
+    cbl_db = db.create(upgrade_cbl_db_name, db_config)
+    assert isinstance(cbl_db, MemoryPointer), "Failed to migrate db from previous version of CBL"
+    return cbl_db, upgrade_cbl_db_name
