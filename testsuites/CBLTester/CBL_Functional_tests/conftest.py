070063258f1e9462341842e4432e7b5761122018
Hemant <Hemant.rajput@couchbase.com>
2019-11-07 15:59:48 +0530

    Uncommenting version check

--- testsuites/CBLTester/CBL_Functional_tests/conftest.py
+++ testsuites/CBLTester/CBL_Functional_tests/conftest.py
@@ -351,11 +351,11 @@ def params_from_base_suite_setup(request):
             raise
 
     # Hit this installed running services to verify the correct versions are installed
-#     cluster_utils.verify_cluster_versions(
-#         cluster_config,
-#         expected_server_version=server_version,
-#         expected_sync_gateway_version=sync_gateway_version
-#     )
+    cluster_utils.verify_cluster_versions(
+        cluster_config,
+        expected_server_version=server_version,
+        expected_sync_gateway_version=sync_gateway_version
+    )
 
     if enable_sample_bucket and not create_db_per_suite:
         # if enable_sample_bucket and not create_db_per_test:
