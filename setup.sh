6e2e949f786efed0e61caa1720412da0ef2d504e
mgcouchbase <manasa.ghanta@couchbase.com>
2019-11-15 16:44:01 -0800

    Initial version of changes for python2to3

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
-<<<<<<< HEAD
+#!/usr/bin/env bash
+# This scripts attempts to setup an environment for running mobile testkit tests.
+# 1. Check that you have Python 2.7 and virtualenv installed
+# 2. Installs venv/ in this directory containing a python 2.7 interpreter
+# 3. Installs all pip packages required by this repo
+# 4. Adds custom library paths to your PYTHONPATH
+
 py3version=$(python3 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 py2version=$(python -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 
 if [[ $py3version == 3.6.* ]]; then
     printf "Using Python3 version: %s\n" $py3version
     PYTHON=python3
-    PIP=pip3.6
+    PIP=pip3
 elif [[ $py2version == 2.7.* ]]; then
     printf "Using Python2 version: %s\n" $py2version
     PYTHON=python
