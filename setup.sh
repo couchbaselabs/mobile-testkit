6ac09904f63945eb3eb97aeb248edb6e166e315e
mgcouchbase <manasa.ghanta@couchbase.com>
2019-11-19 13:30:16 -0800

    Updating setup file with python .version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 py3version=$(python3 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 py2version=$(python -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 
-if [[ $py3version == 3.7.* ]]; then
+if [[ $py3version == 3.6.* ]]; then
     printf "Using Python3 version: %s\n" $py3version
     PYTHON=python3
     PIP=pip3
