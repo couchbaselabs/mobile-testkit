b737965467b6f4b0774e43bc5466f064609a7380
s_sridevi <s_sridevi15@yahoo.com>
2019-11-22 15:31:27 -0800

    fix for testfest scripts

--- libraries/provision/cloudformation_template.py
+++ libraries/provision/cloudformation_template.py
@@ -204,10 +204,7 @@ def gen_template(config):
 
         # Make syncgateway0 a cache writer, and the rest cache readers
         # See https://github.com/couchbase/sync_gateway/wiki/Distributed-channel-cache-design-notes
-        if i == 0:
-            instance.Tags = Tags(Name=name, Type="syncgateway", CacheType="writer")
-        else:
-            instance.Tags = Tags(Name=name, Type="syncgateway")
+        instance.Tags = Tags(Name=name, Type="syncgateway")
 
         t.add_resource(instance)
 
