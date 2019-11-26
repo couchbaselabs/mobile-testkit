c5bef02b3d222e0108e180e58370c8a43fd94fc3
Eunice Huang <eunice.huang@couchbase.com>
2019-11-25 17:51:11 -0800

    exclude couchbase-lite-java in war file

--- CBLClient/Apps/CBLTestServer-Java-WS/src/main/java/com/couchbase/javaws/TestServerContext.java
+++ CBLClient/Apps/CBLTestServer-Java-WS/src/main/java/com/couchbase/javaws/TestServerContext.java
@@ -5,6 +5,7 @@ import com.couchbase.mobiletestkit.javacommon.Context;
 import javax.servlet.ServletRequest;
 import java.io.File;
 import java.io.InputStream;
+import java.util.Base64;
 
 public class TestServerContext implements Context {
     private ServletRequest servletRequest;
@@ -22,7 +23,7 @@ public class TestServerContext implements Context {
 
     @Override
     public String getPlatform() {
-        return "android";
+        return "java-ws";
     }
 
     @Override
@@ -43,5 +44,11 @@ public class TestServerContext implements Context {
     @Override
     public String getLocalIpAddress() {
         return servletRequest.getServerName();
+    }
+
+    @Override
+    public String encodeBase64(byte[] hashBytes){
+        // load java.util.Base64 in java webservice app
+        return Base64.getEncoder().encodeToString(hashBytes);
     }
 }
