742fdb073b7cb1dd523a3b674b4ce2bbae35526c
mgcouchbase <manasa.ghanta@couchbase.com>
2020-01-17 17:16:22 -0800

    Test fix Sync config and android config fix

--- keywords/TestServerAndroid.py
+++ keywords/TestServerAndroid.py
@@ -90,7 +90,7 @@ class TestServerAndroid(TestServerBase):
                 output = subprocess.check_output(["adb", "-e", "install", "-r", apk_path])
                 break
             except Exception as e:
-                if "INSTALL_FAILED_ALREADY_EXISTS" in e.message or "INSTALL_FAILED_UPDATE_INCOMPATIBLE" in e.message:
+                if "INSTALL_FAILED_ALREADY_EXISTS" in e.message or "INSTALL_FAILED_UPDATE_INCOMPATIBLE" in str(e):
                     # Apk may be installed, remove and retry install
                     log_info("Trying to remove....")
                     self.remove()
