96085644cfe8eaa52954f6b89019985590b2ae76
Eunice Huang <eunice.huang@couchbase.com>
2020-01-31 12:10:43 -0800

    experiment for making a daemon service

--- CBLClient/Apps/CBLTestServer-Java-Desktop/src/main/java/com/couchbase/mobiletestkit/javatestserver/TestServerMain.java
+++ CBLClient/Apps/CBLTestServer-Java-Desktop/src/main/java/com/couchbase/mobiletestkit/javatestserver/TestServerMain.java
@@ -1,5 +1,9 @@
 package com.couchbase.mobiletestkit.javatestserver;
 
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonInitException;
+
 import com.couchbase.mobiletestkit.javacommon.Context;
 import com.couchbase.mobiletestkit.javalistener.Server;
 import com.couchbase.mobiletestkit.javacommon.util.Log;
@@ -9,27 +13,45 @@ import org.nanohttpd.util.ServerRunner;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
-public class TestServerMain {
+
+public class TestServerMain implements Daemon {
     private static final int PORT = 8080;
     private static final String TMP_DIR = System.getProperty("java.io.tmpdir");
     private static Context context;
 
     public static void main(String[] args) throws IOException {
+        String ip = context.getLocalIpAddress();  //here the context hands out an ip address of 0.0.0.0
+        Server.memory.setIpAddress(ip);
+        Log.i("JavaDesktop", "Launching a HTTP server at " + ip + ", port = " + PORT);
+
+        NanoHTTPD server = new Server(context, PORT);
+        ServerRunner.executeInstance(server);
+    }
+
+    @Override
+    public void init(DaemonContext dc) throws DaemonInitException, Exception {
         Log.init(new TestServerLogger());
         CouchbaseLite.init();
 
         File tempDir = new File(TMP_DIR, "TestServerTemp");
         context = new TestServerContext(tempDir);
         Server.setContext(context);
+    }
 
-        String ip = context.getLocalIpAddress();  //here the context hands out an ip address of 0.0.0.0
-        Server.memory.setIpAddress(ip);
-        Log.i("JavaDesktop", "Starting the server at " + ip + ", port = " + PORT);
+    @Override
+    public void start() throws Exception {
+        Log.i("JavaDesktop", "Starting TestServer application as a daemon service.");
+        main(null);
+    }
 
-        NanoHTTPD server = new Server(context, PORT);
+    @Override
+    public void stop() throws Exception {
+        Log.i("JavaDesktop", "Stopping TestServer daemon service.");
+    }
 
-        ServerRunner.executeInstance(server);
+    @Override
+    public void destroy() {
+        Log.i("JavaDesktop", "TestServer daemon service is destroyed.");
     }
 }
