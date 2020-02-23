6a5c1c8f6270f963a965354abefb40913400c961
Eunice Huang <eunicehuang@Eunice-Huang-MacBook-Pro.local>
2020-02-22 23:08:06 -0800

    fix windows account format

--- CBLClient/Apps/CBLTestServer-Java-Desktop/win_service_manager.bat
+++ CBLClient/Apps/CBLTestServer-Java-Desktop/win_service_manager.bat
@@ -8,7 +8,7 @@ set service_pwd=%6
 
 
 IF "%status%"=="start" (
-  %service_name%.exe //IS//%service_name% --Install=%location%\\%service_name%.exe --Description=%service_name% --Jvm=auto --Classpath=%location%\\%jar_name% --StartMode=jvm --StartClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StartMethod=windowsService --StartParams=start --StopMode=jvm --StopClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StopMethod=windowsService --StopParams=stop --LogPath=%location%\\logs --StdOutput=auto --StdError=auto --ServiceUser=%service_user% --ServicePassword=%service_pwd%
+  %service_name%.exe //IS//%service_name% --Install=%location%\\%service_name%.exe --Description=%service_name% --Jvm=auto --Classpath=%location%\\%jar_name% --StartMode=jvm --StartClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StartMethod=windowsService --StartParams=start --StopMode=jvm --StopClass=com.couchbase.mobiletestkit.javatestserver.TestServerMain --StopMethod=windowsService --StopParams=stop --LogPath=%location%\\logs --StdOutput=auto --StdError=auto --ServiceUser=.\%service_user% --ServicePassword=%service_pwd%
   
   %service_name%.exe //ES//%service_name%
 ) ELSE (
