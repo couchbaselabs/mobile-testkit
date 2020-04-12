e6853aadf3b3eed6034d642fe089176ec68ec976
Eunice Huang <eunice.huang@couchbase.com>
2020-04-11 19:33:45 -0700

    add mad hatter version to release version list (#1682)

--- keywords/couchbaseserver.py
+++ keywords/couchbaseserver.py
@@ -835,6 +835,7 @@ class CouchbaseServer:
         Return the base_url of the package download URL (everything except the filename)
         """
         released_versions = {
+            "6.5.0": "4960",
             "5.5.0": "2958",
             "5.1.0": "5552",
             "5.0.1": "5003",
