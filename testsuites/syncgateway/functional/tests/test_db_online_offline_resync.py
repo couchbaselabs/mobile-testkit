a8cd8a05ee5e4dfe7431b9c56e33e6473085fb42
mgcouchbase <manasa.ghanta@couchbase.com>
2020-06-02 19:35:56 -0700

    Test fix

--- testsuites/syncgateway/functional/tests/test_db_online_offline_resync.py
+++ testsuites/syncgateway/functional/tests/test_db_online_offline_resync.py
@@ -496,7 +496,7 @@ def test_bucket_online_offline_resync_with_offline(params_from_base_test_setup, 
 
     log_info("online request issued !!!!! response status: {}".format(status))
 
-    time.sleep(30)
+    time.sleep(50)
     db_info = admin.get_db_info("db")
     log_info("Status of db = {}".format(db_info["state"]))
 
