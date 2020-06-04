615f37e01360bd38cb60a0aa418444bd0925e6df
mgcouchbase <manasa.ghanta@couchbase.com>
2020-06-04 15:29:02 -0700

    DB offline test fix

--- testsuites/syncgateway/functional/tests/test_db_online_offline.py
+++ testsuites/syncgateway/functional/tests/test_db_online_offline.py
@@ -188,9 +188,17 @@ def test_online_to_offline_changes_feed_controlled_close_continuous(params_from_
         futures = dict()
         futures[executor.submit(seth.start_continuous_changes_tracking, termination_doc_id=None)] = "continuous"
         futures[executor.submit(doc_pusher.add_docs, num_docs)] = "docs_push"
-        time.sleep(5)
-        futures[executor.submit(sg_client.take_db_offline, cluster_conf, "db")] = "db_offline_task"
-
+        offline_retries = 0
+        while offline_retries < 8:
+            try:
+                assert sg_client.take_db_offline(cluster_conf, "db") == 0
+                futures[executor.submit(sg_client.take_db_offline, cluster_conf, "db")] = "db_offline_task"
+                break
+            except AssertionError as error:
+                offline_retries = offline_retries + 1
+                time.sleep(50)
+                if offline_retries == 8:
+                    raise error
         for future in concurrent.futures.as_completed(futures):
             task_name = futures[future]
 
