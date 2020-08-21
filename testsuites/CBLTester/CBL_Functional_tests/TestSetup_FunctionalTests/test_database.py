f7f1e880dc6e241cb336fdad399d4cf1ac57a45c
Eunice Huang <eunice.huang@couchbase.com>
2020-08-21 13:38:11 -0700

    add a yield for replicator connected method to not wait for idle

--- testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_database.py
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_database.py
@@ -403,7 +403,7 @@ def test_db_close_on_active_replicators(params_from_base_test_setup):
     db_config = db.configure()
     cbl_db = db.create(db_name, db_config)
 
-    num_of_docs = 100
+    num_of_docs = 10000
     channel1 = ["Replication-1"]
     channel2 = ["Replication-2"]
     channel3 = ["Replication-3"]
@@ -434,14 +434,14 @@ def test_db_close_on_active_replicators(params_from_base_test_setup):
 
     replicator_authenticator1 = authenticator.authentication(session_id1, cookie1, authentication_type="session")
     repl1 = replicator.configure_and_replicate(source_db=cbl_db, replicator_authenticator=replicator_authenticator1, target_url=sg_blip_url,
-                                               replication_type="push_pull", continuous=True, channels=channel1)
+                                               replication_type="push_pull", continuous=True, channels=channel1, wait_until_idle=False)
 
     replicator_authenticator2 = authenticator.authentication(session_id2, cookie2, authentication_type="session")
     repl2 = replicator.configure_and_replicate(source_db=cbl_db, replicator_authenticator=replicator_authenticator2, target_url=sg_blip_url,
-                                               replication_type="push", continuous=True, channels=channel2)
+                                               replication_type="push", continuous=True, channels=channel2, wait_until_idle=False)
     replicator_authenticator3 = authenticator.authentication(session_id3, cookie3, authentication_type="session")
     repl3 = replicator.configure_and_replicate(source_db=cbl_db, replicator_authenticator=replicator_authenticator3, target_url=sg_blip_url,
-                                               replication_type="pull", continuous=True, channels=channel3)
+                                               replication_type="pull", continuous=True, channels=channel3, wait_until_idle=False)
 
     log_info(replicator.getActivitylevel(repl1))
     log_info(replicator.getActivitylevel(repl2))
@@ -453,6 +453,7 @@ def test_db_close_on_active_replicators(params_from_base_test_setup):
         log_info("database closed successfully")
         assert True
     except KeyError:
+        log_info(KeyError)
         assert False, "closing database with active replicators are failed"
 
 
