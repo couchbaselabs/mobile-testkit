aed630a09c8c35f35e1584527bd1f571fbd8311f
mgcouchbase <manasa.ghanta@couchbase.com>
2020-08-30 08:28:14 -0700

    Adding new test to test 100 Listeners and Replicator

---
+++ testsuites/CBLTester/p2p_tests/test_peer_to_peer_multi_listeners.py
@@ -0,0 +1,80 @@
+import pytest
+
+from keywords.utils import log_info
+from keywords import attachment
+from CBLClient.Replication import Replication
+from CBLClient.PeerToPeer import PeerToPeer
+
+
+@pytest.mark.listener
+@pytest.mark.parametrize("continuous, replicator_type, endPointType, with_certs", [
+    (True, "push_pull", "URLEndPoint", True),
+    (False, "push", "URLEndPoint", True),
+    (True, "push", "URLEndPoint", False),
+    (False, "push_pull", "URLEndPoint", False),
+])
+def test_peer_to_peer_many_listeners_replicators(params_from_base_test_setup, server_setup, continuous, replicator_type,
+                                                endPointType, with_certs):
+    """
+        @summary:
+        1. Start the server with the self signed certs using the create identity api.
+        2. Start replication with AcceptOnlySelfSignedServerCertificate.
+        3. Verify replication is completed.
+        4. Verify all docs got replicated on server
+    """
+    host_list = params_from_base_test_setup["host_list"]
+    db_obj_list = params_from_base_test_setup["db_obj_list"]
+    db_name_list = params_from_base_test_setup["db_name_list"]
+    base_url_list = server_setup["base_url_list"]
+
+    cbl_db_server = server_setup["cbl_db_server"]
+    cbl_db_list = server_setup["cbl_db_list"]
+    channels = ["peerToPeer"]
+    base_url_client = base_url_list[1]
+    replicator = Replication(base_url_client)
+
+    peerToPeer_client = PeerToPeer(base_url_client)
+    db_obj_server = db_obj_list[0]
+    cbl_db_client = cbl_db_list[1]
+    db_obj_client = db_obj_list[1]
+    db_name_server = db_name_list[0]
+    peerToPeer_server = PeerToPeer(base_url_list[0])
+    peer_to_peer_server = PeerToPeer(base_url_list[0])
+    message_url_tcp_listener = server_setup["message_url_tcp_listener"]
+    print(message_url_tcp_listener)
+    peer_to_peer_server.server_stop(message_url_tcp_listener, "MessageEndPoint")
+
+    server_host = host_list[0]
+    num_of_docs = 50
+    port_array = []
+    repl = []
+    listeners = []
+
+    for i in range(100):
+
+        url_listener = peerToPeer_server.server_start(cbl_db_server, tls_disable=with_certs)
+        url_listener_port = peer_to_peer_server.get_url_listener_port(url_listener)
+        listeners.append(url_listener)
+        port_array.append(url_listener_port)
+
+    db_obj_client.create_bulk_docs(num_of_docs, "replication", db=cbl_db_client, channels=channels,
+                                   attachments_generator=attachment.generate_png_100_100)
+
+    # Now set up client
+    for i in range(100):
+        repl = peerToPeer_client.configure(host=server_host, server_db_name=db_name_server,
+                                       client_database=cbl_db_client, continuous=continuous,
+                                       replication_type=replicator_type, endPointType=endPointType,
+                                       port=port_array[i], tls_disable=with_certs, server_verification_mode=with_certs)
+
+        peerToPeer_client.client_start(repl)
+        replicator.wait_until_replicator_idle(repl)
+        total = replicator.getTotal(repl)
+        completed = replicator.getCompleted(repl)
+        assert total == completed, "replication from client to server did not completed " + str(
+        total) + " not equal to " + str(completed)
+        server_docs_count = db_obj_server.getCount(cbl_db_server)
+        assert server_docs_count == num_of_docs, "Number of docs is not equivalent to number of docs in server "
+        replicator.stop(repl)
+    for i in range(100):
+        peerToPeer_server.server_stop(listeners[i], replicator_type)
