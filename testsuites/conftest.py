6c083472365d4abf48c09114db570ec39b7d1d7a
s_sridevi <s_sridevi15@yahoo.com>
2020-09-21 17:08:47 -0700

    removed exclude and include flags and conditions

--- testsuites/conftest.py
+++ testsuites/conftest.py
@@ -2,15 +2,6 @@ from utilities.xml_parser import parse_junit_result_xml
 
 
 def pytest_addoption(parser):
-    parser.addoption("--exclude-tests", action="store",
-                     help="Value can be 'failed' (or) 'passed' (or) 'failed=<junit_xml_path (or) "
-                     "jenkins_build_url>' (or) 'passed=<junit_xml_path or "
-                     "jenkins_build_url>' (or) 'file=<filename>'")
-
-    parser.addoption("--include-tests", action="store",
-                     help="Value can be 'failed' (or) 'passed' (or) 'failed=<junit_xml_path (or) "
-                     "jenkins_build_url>' (or) 'passed=<junit_xml_path or "
-                     "jenkins_build_url>' (or) 'file=<filename>'")
 
     parser.addoption("--rerun-tests", action="store",
                      help="Value can be 'failed' (or) 'passed' (or) 'failed=<junit_xml_path (or) "
@@ -22,14 +13,8 @@ def pytest_collection_modifyitems(items, config):
     selected_items = []
     deselected_items = []
 
-    exclude_result_option = config.getoption("--exclude-tests")
-    include_result_option = config.getoption("--include-tests")
     rerun_result_option = config.getoption("--rerun-tests")
-    if exclude_result_option:
-        result_option = exclude_result_option
-    elif include_result_option:
-        result_option = include_result_option
-    elif rerun_result_option:
+    if rerun_result_option:
         result_option = rerun_result_option
     if result_option:
         result_option = result_option.split("=")
@@ -50,9 +35,5 @@ def pytest_collection_modifyitems(items, config):
             else:
                 deselected_items.append(item)
 
-        if exclude_result_option:
-            config.hook.pytest_deselected(items=selected_items)
-            items[:] = deselected_items
-        else:
-            config.hook.pytest_deselected(items=deselected_items)
-            items[:] = selected_items
+        config.hook.pytest_deselected(items=deselected_items)
+        items[:] = selected_items
