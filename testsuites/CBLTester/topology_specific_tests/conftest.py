fbe45baf7e4873bfda32b546e73f1039054df05f
Eunice Huang <eunice.huang@couchbase.com>
2021-03-04 16:08:17 -0800

    Feature/cbl heartbeat test (#1812)
    * add heartbeat support
    
    * clear flake8 check
    
    * update cluster config test case since new config files are added
    
    * make changes by review comments

---
+++ testsuites/CBLTester/topology_specific_tests/conftest.py
@@ -0,0 +1,127 @@
+def pytest_addoption(parser):
+    parser.addoption("--mode",
+                     action="store",
+                     help="Sync Gateway mode to run the test in, 'cc' for channel cache or 'di' for distributed index")
+
+    parser.addoption("--skip-provisioning",
+                     action="store_true",
+                     help="Skip cluster provisioning at setup",
+                     default=False)
+
+    parser.addoption("--use-local-testserver",
+                     action="store_true",
+                     help="Skip download and launch TestServer, use local debug build",
+                     default=False)
+
+    parser.addoption("--server-version",
+                     action="store",
+                     help="server-version: Couchbase Server version to install (ex. 4.5.0 or 4.5.0-2601)")
+
+    parser.addoption("--sync-gateway-version",
+                     action="store",
+                     help="sync-gateway-version: Sync Gateway version to install (ex. 1.3.1-16 or 590c1c31c7e83503eff304d8c0789bdd268d6291)")
+
+    parser.addoption("--liteserv-platform",
+                     action="store",
+                     help="liteserv-platform: the platform to assign to the liteserv")
+
+    parser.addoption("--liteserv-version",
+                     action="store",
+                     help="liteserv-version: the version to download / install for the liteserv")
+
+    parser.addoption("--liteserv-host",
+                     action="store",
+                     help="liteserv-host: the host to start liteserv on")
+
+    parser.addoption("--liteserv-port",
+                     action="store",
+                     help="liteserv-port: the port to assign to liteserv")
+
+    parser.addoption("--enable-sample-bucket",
+                     action="store",
+                     help="enable-sample-bucket: Enable a sample server bucket")
+
+    parser.addoption("--xattrs",
+                     action="store_true",
+                     help="xattrs: Enable xattrs for sync gateway")
+
+    parser.addoption("--create-db-per-test",
+                     action="store",
+                     help="create-db-per-test: Creates/deletes client DB for every test")
+
+    parser.addoption("--create-db-per-suite",
+                     action="store",
+                     help="create-db-per-suite: Creates/deletes client DB per suite")
+
+    parser.addoption("--no-conflicts",
+                     action="store_true",
+                     help="If set, allow_conflicts is set to false in sync-gateway config")
+
+    parser.addoption("--device", action="store_true",
+                     help="Enable device if you want to run it on device", default=False)
+
+    parser.addoption("--community", action="store_true",
+                     help="If set, community edition will get picked up , default is enterprise", default=False)
+
+    parser.addoption("--sg-ssl",
+                     action="store_true",
+                     help="If set, will enable SSL communication between Sync Gateway and CBL")
+
+    parser.addoption("--flush-memory-per-test",
+                     action="store_true",
+                     help="If set, will flush server memory per test")
+
+    parser.addoption("--debug-mode", action="store_true",
+                     help="Enable debug mode for the app ", default=False)
+
+    parser.addoption("--use-views",
+                     action="store_true",
+                     help="If set, uses views instead of GSI - SG 2.1 and above only")
+
+    parser.addoption("--enable-file-logging",
+                     action="store_true",
+                     help="If set, CBL file logging would enable. Supported only cbl2.5 onwards")
+
+    parser.addoption("--number-replicas",
+                     action="store",
+                     help="Number of replicas for the indexer node - SG 2.1 and above only",
+                     default=0)
+
+    parser.addoption("--enable-encryption",
+                     action="store_true",
+                     help="Encryption will be enabled for CBL db",
+                     default=True)
+
+    parser.addoption("--encryption-password",
+                     action="store",
+                     help="Encryption will be enabled for CBL db",
+                     default="password")
+
+    parser.addoption("--delta-sync",
+                     action="store_true",
+                     help="delta-sync: Enable delta-sync for sync gateway")
+
+    parser.addoption("--sg-ce", action="store_true",
+                     help="If set, SGW community edition will get picked up , default is enterprise", default=False)
+
+    parser.addoption("--cbs-ce", action="store_true",
+                     help="If set, community edition will get picked up , default is enterprise", default=False)
+
+    parser.addoption("--server-ssl",
+                     action="store_true",
+                     help="If set, will enable SSL communication between server and Sync Gateway")
+
+    parser.addoption("--cluster-config",
+                     action="store",
+                     help="Provide a custom cluster config",
+                     default="multiple_sync_gateways_")
+
+    parser.addoption("--prometheus-enable",
+                     action="store",
+                     help="Starts the prometheus metrics",
+                     default=False)
+
+    parser.addoption("--hide-product-version",
+                     action="store_true",
+                     help="Hides SGW product version when you hit SGW url",
+                     default=False)
