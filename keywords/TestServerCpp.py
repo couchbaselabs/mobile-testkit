7c632841afa03abd9fa8dbaff08fb7609841e34d
mgcouchbase <manasa.ghanta@couchbase.com>
2021-05-24 18:54:08 -0700

    c api changes

--- keywords/TestServerCpp.py
+++ keywords/TestServerCpp.py
@@ -6,8 +6,10 @@ from keywords.constants import LATEST_BUILDS, RELEASED_BUILDS
 from keywords.exceptions import LiteServError
 from keywords.utils import version_and_build
 from keywords.utils import log_info
+from keywords.constants import BINARY_DIR
+from zipfile import ZipFile
 from libraries.provision.ansible_runner import AnsibleRunner
-
+import requests
 
 class TestServerCpp(TestServerBase):
     def __init__(self, version_build, host, port, debug_mode=None, platform="javaws-centos", community_enabled=None):
@@ -25,13 +27,13 @@ class TestServerCpp(TestServerBase):
             self.build_type = "enterprise"
         if self.build is None:
             self.package_name = "CBLTestServer-C-{}-{}".format(self.build_type, self.version)
-            self.download_url = "{}/couchbase-lite-c/{}/{}.war".format(RELEASED_BUILDS, self.version, self.package_name)
+            self.download_url = "{}/couchbase-lite-c/{}/{}.zip".format(RELEASED_BUILDS, self.version, self.package_name)
+            self.binary_path = "{}/{}.exe".format(BINARY_DIR, self.package_name)
 
         self.build_name = "TestServer-C-{}-{}".format(self.build_type, self.version_build)
 
         log_info("package_name: {}".format(self.package_name))
         log_info("download_url: {}".format(self.download_url))
-        log_info("download_corelib_url: {}".format(self.download_corelib_url))
         log_info("build_name: {}".format(self.build_name))
         log_info("self.platform = {}".format(self.platform))
 
@@ -41,35 +43,17 @@ class TestServerCpp(TestServerBase):
          TODO: once we know the steps add it
         """
 
-        if self.platform == "c-msft":
-            # download war file to a remote Windows server machine
-            status = self.ansible_runner.run_ansible_playbook("download-testserver-c.yml", extra_vars={
-                "testserver_download_url": self.download_url,
-                "build_name": self.build_name
-            })
-        if status == 0:
-            return
-        else:
-            raise LiteServError("Failed to download Test server on remote machine")
+        if self.platform == "c-macosx" or "c-linux":
+            resp = requests.get(self.download_url, verify=False)
+            resp.raise_for_status()
+            with open("{}/{}".format(BINARY_DIR, self.package_name), "wb") as f:
+                f.write(resp.content)
+            extracted_directory_name = self.binary_path.replace(".zip", "")
+            with ZipFile("{}".format(self.binary_path)) as zip_f:
+                zip_f.extractall("{}".format(extracted_directory_name))
 
-    def install(self):
-        if self.platform == "c-macosx":
-            # deploy jar/war files to Tomcat on macOS
-            status = self.ansible_runner.run_ansible_playbook("install-testserver-c-macosx.yml", extra_vars={
-                "binary_path": self.binary_path,
-                "version_build": self.version_build
-            })
-        else:
-            # deploy jar/war files to Tomcat on non-Windows
-            status = self.ansible_runner.run_ansible_playbook("install-testserver-c-linux.yml", extra_vars={
-                "binary_path": self.binary_path,
-                "version_build": self.version_build
-            })
-
-        if status == 0:
-            return
-        else:
-            raise LiteServError("Failed to install Test server on remote machine")
+            # Remove .zip
+            os.remove("{}".format(self.binary_path))
 
     def remove(self):
         raise NotImplementedError()
@@ -77,12 +61,11 @@ class TestServerCpp(TestServerBase):
     def start(self, logfile_name):
         if self.platform == "c-macosx":
             status = self.ansible_runner.run_ansible_playbook("start-testserver-c-macosx.yml", extra_vars={
-                "service_status": "start",
-                "catalina_base": os.environ["CATALINA_BASE"]
+                "binary_path": self.binary_path
             })
-
+        else:
             status = self.ansible_runner.run_ansible_playbook("start-testserver-c-linux.yml", extra_vars={
-                "service_status": "start"
+                "binary_path": self.binary_path
             })
 
         time.sleep(15)
@@ -98,7 +81,7 @@ class TestServerCpp(TestServerBase):
     def stop(self):
         if self.platform == "c-macosx":
             # stop Tomcat Windows Service
-            status = self.ansible_runner.run_ansible_playbook("stop-testserver--c-macos.yml", extra_vars={
+            status = self.ansible_runner.run_ansible_playbook("stop-testserver-c-macos.yml", extra_vars={
                 "service_status": "stopped"
             })
         else:
