0a997f58f2c2024d7e279c600619900916ec2988
mgcouchbase <manasa.ghanta@couchbase.com>
2021-06-24 16:19:52 -0700

    fixing the start api for android device

--- testsuites/CBLTester/CBL_Functional_tests/conftest.py
+++ testsuites/CBLTester/CBL_Functional_tests/conftest.py
@@ -263,7 +263,12 @@ def params_from_base_suite_setup(request):
         else:
             testserver.install()
 
-        testserver.start()
+        if device_enabled and "android" in liteserv_platform:
+            test_name_cp = test_name.replace("/", "-")
+            testserver.start_device("{}/logs/{}-{}-{}.txt".format(RESULTS_DIR, type(testserver).__name__,
+                                                                  test_name_cp, datetime.datetime.now()))
+        else:
+            testserver.start()
 
     base_url = "http://{}:{}".format(liteserv_host, liteserv_port)
     sg_config = sync_gateway_config_path_for_mode("sync_gateway_travel_sample", mode)
