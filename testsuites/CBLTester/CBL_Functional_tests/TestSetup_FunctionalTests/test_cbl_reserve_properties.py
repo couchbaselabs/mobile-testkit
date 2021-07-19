c00e47958d6db7f9e49689f95121e9701678b697
mgcouchbase <manasa.ghanta@couchbase.com>
2021-07-19 13:13:54 -0700

    Reserver property test changes for C platform

--- testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_reserve_properties.py
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_reserve_properties.py
@@ -19,6 +19,7 @@ def test_reserve_property(params_from_base_test_setup):
     liteserv_version = params_from_base_test_setup["liteserv_version"]
     db = params_from_base_test_setup["db"]
     cbl_db = params_from_base_test_setup["source_db"]
+    liteserv_platform = params_from_base_test_setup["liteserv_platform"]
     if liteserv_version < "3.0.0":
         pytest.skip('This test cannot run with CBL version below 3.0.0')
     channel = ["Replication-1"]
@@ -38,5 +39,14 @@ def test_reserve_property(params_from_base_test_setup):
     # This is added to catch the
     doc_body = document.create_doc(doc_id="doc_2", content="doc2", channels=channel, cbl=True, expiry=2,
                                    attachments=attachment.generate_2_png_10_10())
+
     doc2 = documentObj.create("doc_2", doc_body)
-    db.saveDocument(cbl_db, doc2)
+    if "c-" in liteserv_platform:
+        try:
+            db.saveDocument(cbl_db, doc2)
+            assert False, "Did not throw the unsupported reserve property error"
+        except Exception as err:
+            assert "Illegal top-level key `_id` in document" in str(err), \
+                "Did not throw the unsupported reserve property error"
+    else:
+        db.saveDocument(cbl_db, doc2)
