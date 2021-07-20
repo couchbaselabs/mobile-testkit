b8f57453f185e39303393c6e2cfb026bbdb3b00d
mgcouchbase <manasa.ghanta@couchbase.com>
2021-07-19 20:43:56 -0700

    blob changes

--- CBLClient/Blob.py
+++ CBLClient/Blob.py
@@ -39,6 +39,8 @@ class Blob(object):
         args = Args()
         args.setString("image", image)
         args.setMemoryPointer("database", database)
+        print(args)
+        print("**********Image Stream")
         # this call will return an input stream
         return self._client.invokeMethod("blob_createImageStream", args)
 
