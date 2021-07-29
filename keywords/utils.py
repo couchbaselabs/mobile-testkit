f4d8b7495b35304582823ab36583b67067c728be
mgcouchbase <manasa.ghanta@couchbase.com>
2021-07-28 20:14:22 -0700

    blob test fix

--- keywords/utils.py
+++ keywords/utils.py
@@ -458,9 +458,9 @@ def meet_supported_version(version_list, target_version):
 def get_embedded_asset_file_path(cblite_platform, db, cbl_db, file_name):
     log_info("get a file path from embedded resources.")
     if cblite_platform in ["android", "xamarin-android", "java-macosx", "java-msft", "java-ubuntu", "java-centos",
-                           "javaws-macosx", "javaws-msft", "javaws-ubuntu", "javaws-centos", "c-android", "c-ios", "c-msft"]:
+                           "javaws-macosx", "javaws-msft", "javaws-ubuntu", "javaws-centos", "c-android"]:
         return file_name
-    elif cblite_platform == "ios":
+    elif "ios" or "c-ios" in cblite_platform:
         return "Files/{}".format(file_name)
     elif cblite_platform == "net-msft":
         db_path = db.getPath(cbl_db).rstrip("\\")
