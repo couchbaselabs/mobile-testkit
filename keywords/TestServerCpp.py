f3846a00f229f0fccc70b6e30af4fc113565a3fd
mgcouchbase <manasa.ghanta@couchbase.com>
2021-08-10 14:25:04 -0700

    ubuntu testapp cleanup change

--- keywords/TestServerCpp.py
+++ keywords/TestServerCpp.py
@@ -6,16 +6,16 @@ from keywords.constants import LATEST_BUILDS, RELEASED_BUILDS
 from keywords.exceptions import LiteServError
 from keywords.utils import version_and_build
 from keywords.constants import BINARY_DIR
-from zipfile import ZipFile
 from keywords.utils import log_info
 from libraries.provision.ansible_runner import AnsibleRunner
-import requests
+from keywords.remoteexecutor import RemoteExecutor
 import subprocess
 
 class TestServerCpp(TestServerBase):
     def __init__(self, version_build, host, port, debug_mode=None, platform="c-linux", community_enabled=None):
         super(TestServerCpp, self).__init__(version_build, host, port)
         self.platform = platform
+        self.host = host
         self.released_version = {
             "3.0.0": 94
         }
@@ -47,45 +47,22 @@ class TestServerCpp(TestServerBase):
 
         '''
            generate ansible config file base on platform format
-           '''
-        if self.platform == "c-msft":
-            # java ws on Windows platform
-            if "LITESERV_MSFT_HOST_USER" not in os.environ:
-                raise LiteServError(
-                    "Make sure you define 'LITESERV_MSFT_HOST_USER' as the windows user for the host you are targeting")
+        '''
+        if "TESTSERVER_HOST_USER" not in os.environ:
+            raise LiteServError(
+                "Make sure you define 'TESTSERVER_HOST_USER' as the user for the host you are targeting")
 
-            if "LITESERV_MSFT_HOST_PASSWORD" not in os.environ:
-                raise LiteServError(
-                    "Make sure you define 'LITESERV_MSFT_HOST_PASSWORD' as the windows user for the host you are targeting")
-
-            # Create config for TestServer Windows host
-            ansible_testserver_target_lines = [
-                "[windows]",
-                "win1 ansible_host={}".format(host),
-                "[windows:vars]",
-                "ansible_user={}".format(os.environ["LITESERV_MSFT_HOST_USER"]),
-                "ansible_password={}".format(os.environ["LITESERV_MSFT_HOST_PASSWORD"]),
-                "ansible_port=5985",
-                "ansible_connection=winrm",
-                "# The following is necessary for Python 2.7.9+ when using default WinRM self-signed certificates:",
-                "ansible_winrm_server_cert_validation=ignore"
-            ]
-        else:
-            if "TESTSERVER_HOST_USER" not in os.environ:
-                raise LiteServError(
-                    "Make sure you define 'TESTSERVER_HOST_USER' as the user for the host you are targeting")
-
-            if "TESTSERVER_HOST_PASSWORD" not in os.environ:
-                raise LiteServError(
-                    "Make sure you define 'TESTSERVER_HOST_PASSWORD' as the user for the host you are targeting")
+        if "TESTSERVER_HOST_PASSWORD" not in os.environ:
+            raise LiteServError(
+                "Make sure you define 'TESTSERVER_HOST_PASSWORD' as the user for the host you are targeting")
 
             # Create config for TestServer non-Windows host
-            ansible_testserver_target_lines = [
-                "[testserver]",
-                "testserver ansible_host={}".format(host),
-                "[testserver:vars]",
-                "ansible_user={}".format(os.environ["TESTSERVER_HOST_USER"]),
-                "ansible_password={}".format(os.environ["TESTSERVER_HOST_PASSWORD"])
+        ansible_testserver_target_lines = [
+            "[testserver]",
+            "testserver ansible_host={}".format(host),
+            "[testserver:vars]",
+            "ansible_user={}".format(os.environ["TESTSERVER_HOST_USER"]),
+            "ansible_password={}".format(os.environ["TESTSERVER_HOST_PASSWORD"])
             ]
 
         ansible_testserver_target_string = "\n".join(ansible_testserver_target_lines)
@@ -101,7 +78,7 @@ class TestServerCpp(TestServerBase):
         """
         Noop on Mac OSX. The LiteServ is a commandline binary
         """
-        log_info("No install needed for macosx")
+        log_info("No install needed for C")
 
 
     def download(self, version_build=None):
@@ -118,37 +95,6 @@ class TestServerCpp(TestServerBase):
             return
         else:
             raise LiteServError("Failed to download Test server on remote machine")
-
-
-        # if version_build is not None:
-        #     self.version_build = version_build
-        # app_name = self.package_name
-        # expected_binary_path = "{}/{}/{}".format(BINARY_DIR, self.app_dir, app_name)
-        # if os.path.exists(expected_binary_path):
-        #     log_info("Package is already downloaded. Skipping.")
-        #     return
-        # # Package not downloaded, proceed to download from latest builds
-        # downloaded_package_zip_name = "{}/{}".format(BINARY_DIR, self.package_name)
-        # self.version, self.build = version_and_build(self.version_build)
-        # if self.build is None:
-        #     url = "{}/couchbase-lite-c/{}/{}".format(RELEASED_BUILDS, self.version, self.package_name)
-        # else:
-        #     url = "{}/couchbase-lite-c/{}/{}/{}".format(LATEST_BUILDS, self.version, self.build, self.package_name)
-        # log_info("Downloading {} -> {}/{}".format(url, BINARY_DIR, self.package_name))
-        #
-        # resp = requests.get(url, verify=False)
-        # resp.raise_for_status()
-        # if self.platform == "c-macosx" or "c-linux":
-        #     resp = requests.get(self.download_url, verify=False)
-        #     resp.raise_for_status()
-        #     with open("{}/{}".format(BINARY_DIR, self.package_name), "wb") as f:
-        #         f.write(resp.content)
-        #     extracted_directory_name = self.binary_path.replace(".zip", "")
-        #     with ZipFile("{}".format(self.binary_path)) as zip_f:
-        #         zip_f.extractall("{}".format(extracted_directory_name))
-        #
-        #     # Remove .zip
-        #     os.remove("{}".format(self.binary_path))
 
     def remove(self):
         raise NotImplementedError()
@@ -182,10 +128,8 @@ class TestServerCpp(TestServerBase):
                 "service_status": "stopped"
             })
         else:
-            # stop Tomcat Server
-            status = self.ansible_runner.run_ansible_playbook("stop-testserver-c-linux.yml", extra_vars={
-                "service_status": "stop"
-            })
+            remote_executor = RemoteExecutor(self.host, self.platform, os.environ["TESTSERVER_HOST_USER"], os.environ["TESTSERVER_HOST_PASSWORD"])
+            remote_executor.execute("ps -ef | grep 'testserver' | awk '{print $1}' | xargs kill -9 $1")
 
         if status == 0:
             return
