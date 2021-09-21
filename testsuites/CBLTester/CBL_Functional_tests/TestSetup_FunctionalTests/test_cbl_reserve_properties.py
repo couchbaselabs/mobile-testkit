9d87dca85f4dc564eafa7dfe260c8723b5204b34
mgcouchbase <manasa.ghanta@couchbase.com>
2021-09-21 12:58:45 -0700

    reserver property test change

--- testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_reserve_properties.py
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_cbl_reserve_properties.py
@@ -19,7 +19,6 @@ def test_reserve_property(params_from_base_test_setup):
     liteserv_version = params_from_base_test_setup["liteserv_version"]
     db = params_from_base_test_setup["db"]
     cbl_db = params_from_base_test_setup["source_db"]
-    # liteserv_platform = params_from_base_test_setup["liteserv_platform"]
     if liteserv_version < "3.0.0":
         pytest.skip('This test cannot run with CBL version below 3.0.0')
     channel = ["Replication-1"]
@@ -42,12 +41,3 @@ def test_reserve_property(params_from_base_test_setup):
 
     doc2 = documentObj.create("doc_2", doc_body)
     db.saveDocument(cbl_db, doc2)
-    # if "c-" in liteserv_platform:
-    #     try:
-    #         db.saveDocument(cbl_db, doc2)
-    #         assert False, "Did not throw the unsupported reserve property error"
-    #     except Exception as err:
-    #         assert "Illegal top-level key `_id` in document" in str(err), \
-    #             "Did not throw the unsupported reserve property error"
-    # else:
-    #     db.saveDocument(cbl_db, doc2)
