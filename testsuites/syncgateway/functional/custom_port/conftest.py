0343330a30385f46c1551ded5d21bb65f1b824d8
Sridevi Saragadam <sridevi.saragadam@couchbase.com>
2022-01-18 18:21:14 -0800

    fixed developer previoew flag for custom port tests

--- testsuites/syncgateway/functional/custom_port/conftest.py
+++ testsuites/syncgateway/functional/custom_port/conftest.py
@@ -177,6 +177,10 @@ def pytest_addoption(parser):
     parser.addoption("--disable-admin-auth",
                      action="store_true",
                      help="Disable Admin auth")
+    parser.addoption("--enable-cbs-developer-preview",
+                     action="store_true",
+                     help="Enabling CBS developer preview",
+                     default=False)
 
 # This will be called once for the at the beggining of the execution in the 'tests/' directory
 # and will be torn down, (code after the yeild) when all the test session has completed.
@@ -221,6 +225,7 @@ def params_from_base_suite_setup(request):
     disable_tls_server = request.config.getoption("--disable-tls-server")
 
     disable_admin_auth = request.config.getoption("--disable-admin-auth")
+    enable_cbs_developer_preview = request.config.getoption("--enable-cbs-developer-preview")
 
     if xattrs_enabled and version_is_binary(sync_gateway_version):
         check_xattr_support(server_version, sync_gateway_version)
@@ -250,6 +255,7 @@ def params_from_base_suite_setup(request):
     log_info("delta_sync_enabled: {}".format(delta_sync_enabled))
     log_info("hide_product_version: {}".format(hide_product_version))
     log_info("disable_persistent_config: {}".format(disable_persistent_config))
+    log_info("enable_cbs_developer_preview: {}".format(enable_cbs_developer_preview))
 
     # sg-ce is invalid for di mode
     if mode == "di" and sg_ce:
@@ -418,6 +424,13 @@ def params_from_base_suite_setup(request):
     else:
         log_info("Enabled Admin Auth")
         persist_cluster_config_environment_prop(cluster_config, 'disable_admin_auth', False)
+
+    if enable_cbs_developer_preview:
+        log_info("Enable CBS developer preview")
+        persist_cluster_config_environment_prop(cluster_config, 'cbs_developer_preview', True)
+    else:
+        log_info("Running without CBS developer preview")
+        persist_cluster_config_environment_prop(cluster_config, 'cbs_developer_preview', False)
 
     sg_config = sync_gateway_config_path_for_mode("sync_gateway_default_functional_tests", mode)
 
