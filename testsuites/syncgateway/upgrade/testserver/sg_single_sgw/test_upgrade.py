df5035ffd0c3800c2b20222efca9a98c4f4d93f7
Sridevi Saragadam <sridevi.saragadam@couchbase.com>
2022-03-23 15:33:16 -0700

    removed unnecessary if condition

--- testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
+++ testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
@@ -359,8 +359,7 @@ def verify_sg_docs_revision_history(url, db, cbl_db2, num_docs, sg_db, added_doc
                 del added_docs[key]["_id"]
             except KeyError:
                 log_info("Ignoring id verification")
-            if rev_gen != expected_doc_map[key]:
-                assert rev_gen == expected_doc_map[key], "revision mismatch on the key {}".format(key)
+            assert rev_gen == expected_doc_map[key], "revision mismatch on the key {}".format(key)
 
             if "_attachments" in doc["doc"]:
                 assert (len(doc["doc"]) - 1) == len(added_docs[key]), "doc length mismatch {}".format(key)
