54bbcb0180f61f522d8f145462f6ca1ec6627285
Sridevi Saragadam <sridevi.saragadam@couchbase.com>
2022-05-16 22:00:41 -0700

    updated the upgrade test

--- testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
+++ testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
@@ -285,11 +285,12 @@ def test_upgrade(params_from_base_test_setup):
         log_info("Trying to create terminator id ....")
         db.create_bulk_docs(number=1, id_prefix=terminator_doc_id, db=cbl_db, channels=sg_user_channels)
         log_info("Waiting for doc updates to complete")
-        replicator.wait_until_replicator_idle(repl2, max_times=3000)
         updated_doc_revs = updates_future.result()
 
         # Wait for replication to completed from cbl db1 to sgw and sgw to cbl db2
-        replicator.wait_until_replicator_idle(repl)
+        replicator.wait_until_replicator_idle(repl2, max_times=3000)
+        time.sleep(20)  # to avoid doc updates of previous logic conflict with below wait for replication logic
+        replicator.wait_until_replicator_idle(repl, max_times=3000)
 
         # 7. Gather CBL docs new revs for verification
         log_info("Gathering the updated revs for verification")
