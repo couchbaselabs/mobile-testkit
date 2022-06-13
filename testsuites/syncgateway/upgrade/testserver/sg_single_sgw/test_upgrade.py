9bfb415ec7b422d03f144d0eadcd1f937909a644
Sridevi Saragadam <sridevi.saragadam@couchbase.com>
2022-06-13 11:19:52 -0700

    debugging msg

--- testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
+++ testsuites/syncgateway/upgrade/testserver/sg_single_sgw/test_upgrade.py
@@ -133,7 +133,7 @@ def test_upgrade(params_from_base_test_setup):
     cluster_util = ClusterKeywords(cluster_config)
     topology = cluster_util.get_cluster_topology(cluster_config)
     lb_url = topology["load_balancers"]
-    lb_ip = host_for_url(lb_url)
+    lb_ip = host_for_url(lb_url[0])
     target_lb_url = "ws://{}:4984/{}".format(lb_ip, sg_db)
     # 1. Create user, session and docs on SG
     sg_client = MobileRestClient()
