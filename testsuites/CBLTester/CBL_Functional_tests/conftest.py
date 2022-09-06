683bfd5b0a4475e9e1bb1a755e4d3dee44089c7b
Thuan Nguyen <soccon@gmail.com>
2022-09-05 22:14:28 -0700

    Update conftest.py
    update start_device

--- testsuites/CBLTester/CBL_Functional_tests/conftest.py
+++ testsuites/CBLTester/CBL_Functional_tests/conftest.py
@@ -499,13 +499,12 @@ def params_from_base_suite_setup(request):
         log_info("Starting TestServer...")
         testserver.stop()
         test_name_cp = test_name.replace("/", "-")
+        log_filename = "{}/logs/{}-{}-{}.txt".format(RESULTS_DIR, type(testserver).__name__, \
+                                                                  test_name_cp, datetime.datetime.now())
         if device_enabled:
-            testserver.start_device("{}/logs/{}-{}-{}.txt".format(RESULTS_DIR, type(testserver).__name__,
-                                                                  test_name_cp, datetime.datetime.now()))
+            testserver.start_device(log_filename)
         else:
-            testserver.start("{}/logs/{}-{}-{}.txt".format(RESULTS_DIR, type(testserver).__name__,
-                                                           test_name_cp,
-                                                           datetime.datetime.now()))
+            testserver.start(log_filename)
         time.sleep(2)
 
     suite_source_db = None
