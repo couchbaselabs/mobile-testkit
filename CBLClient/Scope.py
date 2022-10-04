6f40637e5526d173449c510aca1d257c267f622c
Abhay Aggrawal <108788393+abhayayay@users.noreply.github.com>
2022-10-04 13:30:21 +0530

    scopes and collection cbl (#1948)

---
+++ CBLClient/Scope.py
@@ -0,0 +1,26 @@
+from CBLClient.Client import Client
+from CBLClient.Args import Args
+
+
+class Scope(object):
+    _client = None
+
+    def __init__(self, base_url):
+        self.base_url = base_url
+
+        # If no base url was specified, raise an exception
+        if not self.base_url:
+            raise Exception("No base_url specified")
+
+        self._client = Client(base_url)
+
+    def scopeName(self, scope):
+        args = Args()
+        args.setMemoryPointer("scope", scope)
+        return self._client.invokeMethod("scope_scopeName", args)
+
+    def collection(self, scope, collectionName):
+        args = Args()
+        args.setMemoryPointer("scope", scope)
+        args.setString("collectionName", collectionName)
+        return self._client.invokeMethod("scope_collectionObject", args)
