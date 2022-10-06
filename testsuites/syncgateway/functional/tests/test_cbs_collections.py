10f0775c0905e53b3d73d753f29863d84cdf6a8b
Gilad Kalchheim <gilad.kalchheim@couchbase.com>
2022-10-06 16:31:08 +0100

    Initial code for a new test and tiny bit  of refactoring

--- testsuites/syncgateway/functional/tests/test_cbs_collections.py
+++ testsuites/syncgateway/functional/tests/test_cbs_collections.py
@@ -9,8 +9,8 @@ from libraries.testkit.admin import Admin
 
 # test file shared variables
 bucket = "data-bucket"
-admin_client = None
-cb_server = None
+sg_password = "password"
+admin_client = cb_server = sg_username = channels = None
 
 
 @pytest.fixture
@@ -27,9 +27,9 @@ def scopes_collections_tests_fixture(params_from_base_test_setup):
         # get/set the parameters
         global admin_client
         global cb_server
-        session_id = None
-        pre_test_db_exists = None
-        pre_test_user_exists = None
+        global sg_username
+        global channels
+        session_id = pre_test_db_exists = pre_test_user_exists = None
         random_suffix = str(uuid.uuid4())[:8]
         db_prefix = "db_"
         scope_prefix = "scope_"
@@ -38,10 +38,8 @@ def scopes_collections_tests_fixture(params_from_base_test_setup):
         scope = scope_prefix + random_suffix
         collection = collection_prefix + random_suffix
         sg_username = "scopes_collections_user" + random_suffix
-        sg_password = "password"
         channels = ["ABC"]
         data = {"bucket": bucket, "scopes": {scope: {"collections": {collection: {}}}}, "num_index_replicas": 0}
-
         auth_session = sg_client = sg_url = sg_admin_url = auth_session = None
         cluster_config = params_from_base_test_setup["cluster_config"]
         sg_admin_url = params_from_base_test_setup["sg_admin_url"]
@@ -53,6 +51,7 @@ def scopes_collections_tests_fixture(params_from_base_test_setup):
         sg_client = MobileRestClient()
         cb_server = couchbaseserver.CouchbaseServer(cbs_url)
         admin_client = Admin(cluster.sync_gateways[0])
+        sg_url = params_from_base_test_setup["sg_url"]
         auth = [RBAC_FULL_ADMIN['user'], RBAC_FULL_ADMIN['pwd']]
 
         # Scope creation on the Couchbase server
@@ -78,7 +77,7 @@ def scopes_collections_tests_fixture(params_from_base_test_setup):
     except Exception as e:
         raise e
     finally:
-        # Cleanup everything the was created
+        # Cleanup everything that was created
         if (session_id is not None) and (sg_client.does_session_exist(sg_admin_url, db=db, session_id=session_id) is True):
             sg_client.delete_session(sg_admin_url, db, session_id=session_id)
         if (pre_test_user_exists is not None) and (pre_test_user_exists is False):
@@ -136,8 +135,8 @@ def test_change_collection_name(scopes_collections_tests_fixture):
     new_collection_name = "new_collection_test"
 
     # 1. Upload a document to a collection
-    if sg_client.does_doc_exist(sg_url, db, doc_id, auth_session, scope=scope, collection=collection) is False:
-        sg_client.add_docs(sg_url, db, 1, doc_prefix, auth_session, scope=scope, collection=collection)
+    if sg_client.does_doc_exist(sg_url, db, doc_id, auth=auth_session, scope=scope, collection=collection) is False:
+        sg_client.add_docs(sg_url, db, 1, doc_prefix, auth=auth_session, scope=scope, collection=collection)
 
     # 2. Rename the collection by updating the config
     cb_server.create_collection(bucket, scope, new_collection_name)
@@ -156,6 +155,37 @@ def test_change_collection_name(scopes_collections_tests_fixture):
         sg_client.get_doc(sg_admin_url, db, doc_id, auth=auth_session, scope=scope, collection=collection)
     except Exception as e:
         pytest.fail("The document could not be read from the collection after it was renamed and renamed back. The error: " + str(e))
+
+
+@pytest.mark.syncgateway
+@pytest.mark.collections
+def test_collection_channels(scopes_collections_tests_fixture):
+    # setup
+    sg_client, sg_url, sg_admin_url, auth_session, db, scope, collection = scopes_collections_tests_fixture
+    random_str = str(uuid.uuid4())[:6]
+    doc_prefix = "scp_tests_doc_" + random_str
+    channels_tests_doc_prefix = "channels_doc_" + random_str
+    sg_channel_test_username = "channel_test_user" + random_str
+    channels_test_channel = ["CHANNEL_TEST"]
+    auth = [RBAC_FULL_ADMIN['user'], RBAC_FULL_ADMIN['pwd']]
+    sg_client.create_user(sg_admin_url, db, sg_channel_test_username, sg_password, channels=channels_test_channel, auth=auth)
+    auth_scopes_user = sg_username, sg_password
+    auth_channels_user = sg_channel_test_username, sg_password
+
+    sg_client.add_docs(sg_url, db, 3, doc_prefix, auth_scopes_user, channels=channels, scope=scope, collection=collection)
+    sg_client.add_docs(sg_url, db, 3, channels_tests_doc_prefix, auth_channels_user, channels=channels_test_channel, scope=scope, collection=collection)
+
+    scopes_tests_docs = sg_client.get_all_docs(url=sg_url, db=db, auth=auth_scopes_user, include_docs=True)
+    channels_test_docs = sg_client.get_all_docs(url=sg_url, db=db, auth=auth_channels_user, include_docs=True)
+
+    scopes_tests_docs_ids = [doc["id"] for doc in scopes_tests_docs["rows"]]
+    channels_test_docs_ids = [doc["id"] for doc in channels_test_docs["rows"]]
+    for doc in scopes_tests_docs_ids:
+        if channels_tests_doc_prefix in doc:
+            pytest.fail("A document is available in a channel that it was not assigned to. Document prefix: " + doc_prefix + ". The document: " + doc)
+    for doc in channels_test_docs_ids:
+        if doc_prefix in doc:
+            pytest.fail("A document is available in a channel that it was not assigned to. Document prefix: " + doc_prefix + ". The document: " + doc)
 
 
 def rename_a_single_collection(db, scope, new_name):
