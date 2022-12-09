fd39a1d88058ee40803f7f517fb8423502346f6b
Gilad Kalchheim <gilad.kalchheim@couchbase.com>
2022-12-09 15:52:11 +0000

    add

--- libraries/provision/ansible_python_runner.py
+++ libraries/provision/ansible_python_runner.py
@@ -124,7 +124,8 @@ class Runner(object):
         # Set inventory, using most of above objects
         self.inventory = InventoryManager(loader=self.loader, sources=[inventory_filename])
         self.inventory.subset(self.options.subset)
-        print("**********************************************************************self.inventory.get_hosts()" + self.inventory.get_hosts())
+        print("**********************************************************************self.inventory.get_hosts()")
+        print(self.inventory.get_hosts())
         self.variable_manager = VariableManager(loader=self.loader, inventory=self.inventory)
         self.variable_manager.extra_vars = extra_vars
 
