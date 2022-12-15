9f3a35bc4dbd402d45083b42e384fbb394c2dc6b
Abhay Aggrawal <108788393+abhayayay@users.noreply.github.com>
2022-12-15 16:10:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@
 
 # py37version=$(python3.7 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 py3version=$(python3 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
-py2version=$(python -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 
 
 if [[ $py3version == 3.8.* ]]; then
@@ -18,10 +17,6 @@ elif [[ $py3version == 3.7.* ]]; then
     printf "Using Python3 version: %s\n" $py3version
     PYTHON=python3
     PIP=pip3.7
-elif [[ $py2version == 2.7.* ]]; then
-    printf "Using Python2 version: %s\n" $py2version
-    PYTHON=python
-    PIP=pip
 else
     echo "Exiting. Make sure Python version is 3.8 or 2.7."
     return 1
