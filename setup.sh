582804bcf46378033de22b6cc9d2680695447baf
Gilad Kalchheim <gilad.kalchheim@couchbase.com>
2023-01-10 10:46:42 +0000

    add Python3.8

--- setup.sh
+++ setup.sh
@@ -8,12 +8,16 @@
 # py37version=$(python3.7 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 py3version=$(python3 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 
-if [[ $py3version == 3.7.* ]]; then
-    printf "Using Python3 version: %s\n" $py37version
+if [[ $py3version == 3.8.* ]]; then
+    printf "Using Python3 version: %s\n" $py3version
+    PYTHON=python3
+    PIP=pip3
+elif [[ $py3version == 3.7.* ]]; then
+    printf "Using Python3 version: %s\n" $py3version
     PYTHON=python3
     PIP=pip3
 else
-    echo "Exiting. Make sure Python version is 3.7 or 2.7."
+    echo "Exiting. Make sure Python version is 3.7 or 2.7 or 3.8."
     return 1
 fi
 
