613a40f453769ff42c78e027126428ae2be9479f
gilkal <gilkal1979@gmail.com>
2023-05-10 12:19:34 +0100

    Fix/isgr scp with magma (#2071)
    * Skip scopes and collecetions tests for magma - they are not needed
    
    * Skip scopes and collecetions tests for magma - they are not needed
    
    * Skip scopes and collecetions tests for magma - they are not needed
    
    * Skip scopes and collecetions tests for magma - they are not needed
    
    * Skip scopes and collecetions tests for magma - they are not needed
    
    * Skip the tests
    
    * Skip the tests
    
    * Fix a small problem with the logs collection
    
    * Put back a line that was accidently deleted
    
    * Debug a log problem
    
    * Debug a log problem
    
    * Debug a log problem
    
    * Debug a log problem

--- testsuites/syncgateway/functional/topology_specific_tests/multiple_sync_gateways/conftest.py
+++ testsuites/syncgateway/functional/topology_specific_tests/multiple_sync_gateways/conftest.py
@@ -369,7 +369,12 @@ def params_from_base_test_setup(request, params_from_base_suite_setup):
     c = cluster.Cluster(cluster_config)
     errors = c.verify_alive(mode)
 
+    try:
+        failures = request.node.rep_call.failed
+    except Exception:
+        failures = False
+
     # if the test failed pull logs
-    if collect_logs or request.node.rep_call.failed or len(errors) != 0:
+    if collect_logs or failures or len(errors) != 0:
         logging_helper = Logging()
         logging_helper.fetch_and_analyze_logs(cluster_config=cluster_config, test_name=test_name)
