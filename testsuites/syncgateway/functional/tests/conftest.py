d8a3c09ea9cd3d0b4abe9a9b91c100ee3d6c8e17
Abhay Aggrawal <abhay.aggrawal@couchbase.com>
2023-07-18 11:04:57 +0530

    code_ coverage feature for sgw

--- testsuites/syncgateway/functional/tests/conftest.py
+++ testsuites/syncgateway/functional/tests/conftest.py
@@ -727,5 +727,4 @@ def coverage_report(params_from_base_suite_setup):
         ansible_runner.run_ansible_playbook("fetch-code-coverage-files.yml")
 
 
-if params_from_base_suite_setup["code_coverage"]:
-    atexit.register(coverage_report)
+atexit.register(coverage_report)
