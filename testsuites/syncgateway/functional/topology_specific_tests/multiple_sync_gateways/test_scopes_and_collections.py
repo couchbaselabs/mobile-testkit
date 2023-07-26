d19d8b4627388e2426f599ae3ca24b8886ce5192
Gilad Kalchheim <gilkal1979@gmail.com>
2023-07-26 06:45:13 +0100

    Try to change to persistent config

--- testsuites/syncgateway/functional/topology_specific_tests/multiple_sync_gateways/test_scopes_and_collections.py
+++ testsuites/syncgateway/functional/topology_specific_tests/multiple_sync_gateways/test_scopes_and_collections.py
@@ -680,9 +680,10 @@ def reset_cluster_configuration(params_from_base_test_setup):
         sg_config_name = "sync_gateway_cpc_custom_group"
         sg_conf1 = sync_gateway_config_path_for_mode(sg_config_name, "cc", cpc=True)
         #sg_config = sync_gateway_config_path_for_mode(sg_config_name, "cc", cpc=True)
+        groupid_str = '"group_id": "group"' + str(i)
         cpc_temp_sg_config = "{}/temp_sg_config_{}".format(SYNC_GATEWAY_CONFIGS_CPC, "cc")
         shutil.copyfile(sg_conf1, cpc_temp_sg_config)
-        cpc_temp_sg_config = replace_string_on_sgw_config(cpc_temp_sg_config, '{{ groupid }}', "group" + str(i))
+        cpc_temp_sg_config = replace_string_on_sgw_config(cpc_temp_sg_config, '{{ groupid }}', groupid_str)
         c_cluster = Cluster(config=cpc_temp_sg_config)
         c_cluster.reset(sg_config_path=cpc_temp_sg_config)
         os.remove(cpc_temp_sg_config)
