c6515ec4658a9f54debfda8964517d126b41692a
Gilad Kalchheim <gilkal1979@gmail.com>
2023-10-04 08:33:37 +0100

    move the debug prints

--- keywords/TestServerJavaWS.py
+++ keywords/TestServerJavaWS.py
@@ -31,10 +31,8 @@ class TestServerJavaWS(TestServerBase):
 
             # The new distribution method for the support libs starts after release v3.1.1
             if re.compile('^([456789]|3\.[23456789]|3.1.[23456789])').match(self.version):
-                print("***************************************" + self.version)
                 self.cbl_core_lib_name = "couchbase-lite-java-linux-supportlibs-{}".format(self.version_build)
             else:
-                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++OUTSIDE IF")
                 if community_enabled:
                     self.cbl_core_lib_name = "couchbase-lite-java-{}".format(self.version)
                 else:
@@ -47,8 +45,10 @@ class TestServerJavaWS(TestServerBase):
 
             # The new distribution method for the support libs starts after release v3.1.1
             if re.compile('^([456789]|3\.[23456789]|3.1.[23456789])').match(self.version):
+                print("***************************************" + self.version)
                 self.cbl_core_lib_name = "couchbase-lite-java-linux-supportlibs-{}".format(self.version)
             else:
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++OUTSIDE IF")
                 if community_enabled:
                     self.cbl_core_lib_name = "couchbase-lite-java-{}-{}".format(self.version, self.build)
                 else:
