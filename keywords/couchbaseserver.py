1a44f6ecacc9fbad93415c9c88a3751769fcd430
Gilad Kalchheim <gilkal1979@gmail.com>
2023-11-30 08:45:34 +0000

    Add debug prints

--- keywords/couchbaseserver.py
+++ keywords/couchbaseserver.py
@@ -912,7 +912,8 @@ class CouchbaseServer:
             edition = "community"
         else:
             edition = "enterprise"
-
+        print("===============================================GILAD" + cbs_platform)
+        print("debian" in cbs_platform)
         if ("ubuntu" or "debian") in cbs_platform:
             if version.startswith("3.1.6"):
                 return "couchbase-server-{}-{}-{}.x86_64.deb".format(edition, version, cbs_platform)
