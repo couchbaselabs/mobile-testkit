9debf99b0a96ce0024cd0f1d8d6f97e58eb61815
Gilad Kalchheim <gilkal1979@gmail.com>
2023-12-26 17:36:29 +0000

    Install iptables on Debian

--- libraries/testkit/syncgateway.py
+++ libraries/testkit/syncgateway.py
@@ -202,6 +202,8 @@ class SyncGateway:
                 playbook_vars["server_scheme"] = "couchbases"
                 playbook_vars["server_port"] = 11207
                 block_http_vars = {}
+                if "debian" in sg_platform.lower():
+                    block_http_vars["ansible_distribution"] = sg_platform.capitalize()
                 port_list = ["8091:8096,11210:11211"]
                 for port in port_list:
                     block_http_vars["port"] = port
@@ -362,6 +364,8 @@ class SyncGateway:
                 playbook_vars["server_scheme"] = "couchbases"
                 playbook_vars["server_port"] = 11207
                 block_http_vars = {}
+                if "debian" in sg_platform.lower():
+                    block_http_vars["ansible_distribution"] = sg_platform.capitalize()
                 port_list = ["8091:8096,11210:11211"]
                 for port in port_list:
                     block_http_vars["port"] = port
@@ -833,6 +837,8 @@ def setup_sgwconfig_db_config(cluster_config, sg_config_path, use_config=False):
         server_scheme_var = "couchbases"
         server_port_var = "11207"
         block_http_vars = {}
+        if "debian" in sg_platform.lower():
+            block_http_vars["ansible_distribution"] = sg_platform.capitalize()
         port_list = ["8091:8096,11210:11211"]
         for port in port_list:
             block_http_vars["port"] = port
