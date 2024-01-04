4963e48831b34882004174a11dc997292fd0e0fe
Gilad Kalchheim <gilkal1979@gmail.com>
2024-01-04 12:09:25 +0000

    Print the exception

--- testsuites/syncgateway/functional/tests/test_automatic_migration.py
+++ testsuites/syncgateway/functional/tests/test_automatic_migration.py
@@ -276,6 +276,7 @@ def test_automatic_migration_with_server_connection_fails(params_from_base_test_
     server.stop(cbs_platform=cbs_platform)
     time.sleep(20)  # Wait for the server to actually stop
     # 3 . Upgrade SGW to lithium and have Automatic upgrade
+    print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&GILAD")
     try:
         sg_obj.upgrade_sync_gateways(cluster_config=cluster_conf, sg_conf=sg_conf, sgw_previous_version=sync_gateway_previous_version, sync_gateway_version=sync_gateway_version)
     except Exception as ex:
