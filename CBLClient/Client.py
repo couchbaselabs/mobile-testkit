85421264fa0f050475deeea3f3453470293f0158
Gilad Kalchheim <gilkal1979@gmail.com>
2024-01-24 10:01:34 +0000

    Remove the prints

--- CBLClient/Client.py
+++ CBLClient/Client.py
@@ -28,8 +28,6 @@ class Client(object):
             # Create connection to method endpoint.
             headers = {"Content-Type": "application/json"}
             self.session.headers = headers
-            print("***************GILAD BODY=" + str(body))
-            print("****************GILAD URL=" + str(url))
             resp = self.session.post(url, data=json.dumps(body))
             resp.raise_for_status()
             responseCode = resp.status_code
