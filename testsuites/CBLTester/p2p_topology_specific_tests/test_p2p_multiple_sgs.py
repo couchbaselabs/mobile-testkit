1a716bbf2d3d4bdafc5cee6a2eb2779710dbb696
Gilad Kalchheim <gilkal1979@gmail.com>
2024-02-02 14:52:11 +0000

    Add another test to be skipped

--- testsuites/CBLTester/p2p_topology_specific_tests/test_p2p_multiple_sgs.py
+++ testsuites/CBLTester/p2p_topology_specific_tests/test_p2p_multiple_sgs.py
@@ -13,6 +13,8 @@ from utilities.cluster_config_utils import persist_cluster_config_environment_pr
 from CBLClient.PeerToPeer import PeerToPeer
 
 
+@pytest.mark.skipif(pytest.config.getoption("--liteserv-platform").startswith("net-"),
+                    reason="Under investigation:https://issues.couchbase.com/browse/CM-1164")
 @pytest.mark.listener
 @pytest.mark.replication
 @pytest.mark.parametrize("sg_conf_name, num_of_docs", [
