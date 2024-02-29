5f6e519244c8fcb6294aa6fca53d8ed9b5013d40
gilkal <gilkal1979@gmail.com>
2024-02-29 08:33:19 +0000

    Add 3.1.3 in cluster settings as well (#2111)

--- keywords/ClusterKeywords.py
+++ keywords/ClusterKeywords.py
@@ -245,7 +245,8 @@ class ClusterKeywords:
             "3.0.5": "8",
             "3.0.7": "3",
             "3.1.0": "592",
-            "3.1.2": "34"
+            "3.1.2": "34",
+            "3.1.3": "6"
         }
         if sg_build is None:
             expected_sync_gateway_version = "{}-{}".format(expected_sync_gateway_version,
