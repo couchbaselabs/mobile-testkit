d9fa74f7adf39ba7258e237efbca923e0b367ad7
Gilad Kalchheim <gilkal1979@gmail.com>
2024-03-18 11:40:40 +0000

    change the replicated db to the one the test app creates

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -171,7 +171,7 @@ def test_vector_search_index_correctness(vector_search_test_fixture):
         auth = (RBAC_FULL_ADMIN['user'], RBAC_FULL_ADMIN['pwd']) or None
         sg_client.create_user(sg_admin_url, sg_db, username, password, channels=channels_sg, auth=auth)
         session, replicator_authenticator, repl = replicator.create_session_configure_replicate(
-        baseUrl=base_url, sg_admin_url=sg_admin_url, sg_db=sg_db, username=username, password=password, channels=channels_sg, sg_client=sg_client, cbl_db=cbl_db, sg_blip_url=sg_blip_url, continuous=False, replication_type="push_pull", auth=None)
+        baseUrl=base_url, sg_admin_url=sg_admin_url, sg_db=sg_db, username=username, password=password, channels=channels_sg, sg_client=sg_client, cbl_db=vsTestDatabase, sg_blip_url=sg_blip_url, continuous=False, replication_type="push_pull", auth=None)
         sg_docs = sg_client.get_all_docs(url=sg_url, db=sg_db, auth=session)
         sg_docs = sg_docs["rows"]
         cbl_doc_count = db.getCount(cbl_db)
