e77b4b51ab5cb9146cf1a3e0d7b23e56e067bc2f
Gilad Kalchheim <gilkal1979@gmail.com>
2024-03-19 11:06:18 +0000

    Load thedatabase from Python

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -64,10 +64,10 @@ def vector_search_test_fixture(params_from_base_test_setup):
     c = cluster.Cluster(config=cluster_config)
     auth = need_sgw_admin_auth and [RBAC_FULL_ADMIN['user'], RBAC_FULL_ADMIN['pwd']] or None
     admin_client = Admin(c.sync_gateways[0])
-    db = Database(base_url)
+    vsTestDatabase = Database(base_url)
     vsHandler = VectorSearch(base_url)
     sg_client = MobileRestClient()
-    db_config = db.configure()
+    # db_config = db.configure()
 
     sync_gateway_version = params_from_base_test_setup["sync_gateway_version"]
 
@@ -96,7 +96,7 @@ def vector_search_test_fixture(params_from_base_test_setup):
     # vsTestDatabase = vsHandler.loadDatabase()
     db_prefix = "vtestDatabase"
     prebuilt_db_path = "{}.cblite2.zip".format(db_prefix)
-    db.get_pre_built_db(prebuilt_db_path)
+    vsTestDatabase.get_pre_built_db(prebuilt_db_path)
 
     channels = ["ABC"]
     user_scopes_collections = {scope: {
