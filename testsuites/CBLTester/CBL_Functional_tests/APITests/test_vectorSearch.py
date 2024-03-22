0c263e6c5b629ae856d9b56dfbddbe72e5db4b16
Monty Ali <monty.ali@couchbase.com>
2024-03-22 12:14:18 +0000

    revert model name

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -170,7 +170,7 @@ def test_vector_search_index_correctness(vector_search_test_fixture):
         # Very rough draft of CBL side work
         # Register model
         vsHandler = VectorSearch(base_url)
-        vsHandler.register_model(key="word", name="thisIsTheModelName")
+        vsHandler.register_model(key="word", name="gteSmall")
         print("Registered model gteSmall on field 'word'")
         vsHandler.createIndex(
              database = vsTestDatabase,
