580f07d243a7eac3e0ace3d16f98ba80e4065cf3
Gilad Kalchheim <gilkal1979@gmail.com>
2024-04-13 17:07:31 +0100

    Remove unecessaary db configuration

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -76,7 +76,7 @@ def vector_search_test_fixture(params_from_base_test_setup):
     db = Database(base_url)
     vsHandler = VectorSearch(base_url)
     sg_client = MobileRestClient()
-    db.deleteDB(db)
+    db.delete()
 
     sync_gateway_version = params_from_base_test_setup["sync_gateway_version"]
 
