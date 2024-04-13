60122fb070122168d9c797d9668a1ed7062aa9ce
Gilad Kalchheim <gilkal1979@gmail.com>
2024-04-13 17:44:39 +0100

    Delete unsed default database

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -76,7 +76,7 @@ def vector_search_test_fixture(params_from_base_test_setup):
     db = Database(base_url)
     vsHandler = VectorSearch(base_url)
     sg_client = MobileRestClient()
-    db.deleteDBbyName("cbl-test")
+    db.close(cbl_db)
 
     sync_gateway_version = params_from_base_test_setup["sync_gateway_version"]
 
