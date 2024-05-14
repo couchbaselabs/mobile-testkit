fb77ba2bf0e68d76e8240d7a4d290e4e586f9b10
Gilad Kalchheim <gilkal1979@gmail.com>
2024-05-14 16:22:56 +0100

    Set the sleep value back to 15

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -309,7 +309,7 @@ def test_vector_search_index_correctness(vector_search_test_fixture):
         print("Waiting for indexes to update")
         # TODO find a better way than sleep
         # takes around 50-100ms per word so should cover all the words with this
-        time.sleep(30)
+        time.sleep(15)
              
         ivQueryAll = vsHandler.query(term="dinner",
                         sql=("SELECT word, vector_distance(indexVectorsIndex) AS distance "
