5acef9ae2f065bb73711f9268acddfe773ff6b86
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-03 14:23:30 +0100

    Enable register model

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -172,7 +172,7 @@ def test_vector_search_index_correctness(vector_search_test_fixture):
         # Very rough draft of CBL side work
         # Register model
         vsHandler = VectorSearch(base_url)
-        # vsHandler.register_model(key="word", name="gteSmall")
+        vsHandler.register_model(key="word", name="gteSmall")
         print("Registered model gteSmall on field 'word'")
 
         # create indexes
