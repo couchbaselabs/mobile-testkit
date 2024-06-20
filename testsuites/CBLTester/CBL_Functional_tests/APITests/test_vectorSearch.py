6feafe7821ae25cf81428a45faf5d92cd76d1b91
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-20 07:18:41 +0100

    Update the sgw if it is not created from scratch

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -94,6 +94,8 @@ def vector_search_test_fixture(params_from_base_test_setup, params_from_base_sui
     # sgw database creation
     if admin_client.does_db_exist(sg_db) is False:
         admin_client.create_db(sg_db, data)
+    else:
+        admin_client.post_db_config(sg_db, data)
 
     # load vsTestDatabase on cbl
     vsTestDatabase = vsHandler.loadDatabase()
