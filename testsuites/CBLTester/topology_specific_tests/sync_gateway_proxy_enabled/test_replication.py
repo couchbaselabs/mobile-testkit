3899a0bbe8a15204bae6c6cd9cc1d42baf145dfd
Gilad Kalchheim <gilkal1979@gmail.com>
2024-06-24 17:16:11 +0100

    More refactoring

--- testsuites/CBLTester/topology_specific_tests/sync_gateway_proxy_enabled/test_replication.py
+++ testsuites/CBLTester/topology_specific_tests/sync_gateway_proxy_enabled/test_replication.py
@@ -53,7 +53,7 @@ def test_replication_heartbeat(params_from_base_test_setup):
 
     # Reset nginx with shorter keep_alive frequency config
     from libraries.provision.install_nginx import install_nginx
-    install_nginx(cluster_config, True, sg_platform="debian")
+    install_nginx(cluster_config, True)
 
     # Reset cluster to ensure no data in system
     c = cluster.Cluster(config=cluster_config)
