16e5c11d769f58361e00a6d8f1203c84549ba0f5
gilkal <gilkal1979@gmail.com>
2024-06-25 13:00:53 +0100

    Vector search phase 1 (#2123)
    * Initial commit for the first phase vector-search branch
    
    * Adjust the retrieval of the default db
    
    * Attempt tokeep  the default db
    
    * Remove the cbl_db variable
    
    * Fix flake8 errors
    
    * Undo some flake8 fixes
    
    * revert flake8 changes
    
    * First chunck of chanegs to fix flake8 problems
    
    * Second chunck of chanegs to fix flake8 problems
    
    * Third chunck of chanegs to fix flake8 problems
    
    * Fourth chunck of chanegs to fix flake8 problems
    
    * Fifth chunck of chanegs to fix flake8 problems
    
    * Randomise SGW db
    
    * Randomise SGW db
    
    * Randomise SGW db
    
    * Update the sgw if it is not created from scratch
    
    * Remove the symlink creation - it's done in the test server, when it starts
    
    * Fix flake8 errors
    
    * Skip the vector search test for now
    
    * Skip the sanity test as well
    
    * Skip the sanity test as well
    
    * Refactor and fix typos. Also added comments for clarity

---
+++ CBLClient/VectorSearch.py
@@ -0,0 +1,88 @@
+from CBLClient.Client import Client
+from CBLClient.Args import Args
+
+
+class VectorSearch(object):
+    _client = None
+
+    def __init__(self, base_url):
+
+        self.base_url = base_url
+        # If no base url was specified, raise an exception
+        if not self.base_url:
+            raise Exception("No base_url specified")
+
+        self._client = Client(base_url)
+
+    # TEMPORARY TEST FUNCTIONS
+    def testTokenizer(self, input=None):
+        args = Args()
+        if input is not None:
+            args.setString("input", input)
+        return self._client.invokeMethod("vectorSearch_testTokeniser", args)
+
+    def testDecode(self, input=None):
+        args = Args()
+        if input is not None:
+            args.setString("input", input)
+        return self._client.invokeMethod("vectorSearch_testDecode", args)
+
+    # USEFUL FUNCTIONS
+    def createIndex(self, database, scopeName, collectionName, indexName, expression,
+                    dimensions, centroids, scalarEncoding=None, subquantizers=None, bits=None,
+                    metric=None, minTrainingSize=None, maxTrainingSize=None):
+        args = Args()
+        args.setMemoryPointer("database", database)
+        args.setString("collectionName", collectionName)
+        args.setString("scopeName", scopeName)
+        args.setString("indexName", indexName)
+        args.setString("expression", expression)
+        args.setInt("dimensions", dimensions)
+        args.setInt("centroids", centroids)
+        if scalarEncoding:
+            args.setMemoryPointer("scalarEncoding", scalarEncoding)
+        if subquantizers:
+            args.setInt("subquantizers", subquantizers)
+        if bits:
+            args.setInt("bits", bits)
+        if metric:
+            args.setString("metric", metric)
+        if minTrainingSize:
+            args.setInt("minTrainingSize", minTrainingSize)
+        if maxTrainingSize:
+            args.setInt("maxTrainingSize", maxTrainingSize)
+        return self._client.invokeMethod("vectorSearch_createIndex", args)
+
+    def getEmbedding(self, input):
+        args = Args()
+        args.setString("input", input)
+        return self._client.invokeMethod("vectorSearch_getEmbedding", args)
+
+    def registerModel(self, key, name):
+        args = Args()
+        args.setString("key", key)
+        args.setString("name", name)
+        return self._client.invokeMethod("vectorSearch_registerModel", args)
+
+    def query(self, term, sql, database):
+        args = Args()
+        args.setString("term", term)
+        args.setString("sql", sql)
+        args.setMemoryPointer("database", database)
+
+        return self._client.invokeMethod("vectorSearch_query", args)
+
+    def loadDatabase(self, dbPath=None):
+        args = Args()
+        if dbPath:
+            args.setString("dbPath", dbPath)
+        return self._client.invokeMethod("vectorSearch_loadDatabase", args)
+
+    def regenerateWordEmbeddings(self):
+        return self._client.invokeMethod("vectorSearch_regenerateWordEmbeddings")
+
+    def register_model(self, key, name):
+        args = Args()
+        args.setString("key", key)
+        args.setString("name", name)
+        return self._client.invokeMethod("vectorSearch_registerModel", args)
