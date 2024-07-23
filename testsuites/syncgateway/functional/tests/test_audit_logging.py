90f02e3d309cde9038890ef3ce6cb04561c3e630
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-23 16:01:11 +0100

    Move the log deletion to the actual test

--- testsuites/syncgateway/functional/tests/test_audit_logging.py
+++ testsuites/syncgateway/functional/tests/test_audit_logging.py
@@ -32,6 +32,7 @@ channels = ["audit_logging"]
 auth = None
 bucket = "data-bucket"
 bucket2 = "audit-logging-bucket2"
+remote_executor = None
 
 
 @pytest.fixture
@@ -44,6 +45,7 @@ def audit_logging_fixture(params_from_base_test_setup):
     global channels
     global auth
     global bucket2
+    global remote_executor
 
     cluster_config = params_from_base_test_setup["cluster_config"]
     cluster_config = params_from_base_test_setup["cluster_config"]
@@ -79,8 +81,6 @@ def audit_logging_fixture(params_from_base_test_setup):
             admin_client.create_db(sg2_db, {"bucket": bucket2, "num_index_replicas": 0})
         if admin_client.does_user_exist(sg_db, username) is False:
             sg_client.create_user(url=sg_admin_url, db=sg_db, name=username, password=password, channels=channels, auth=auth)
-    # Delete the previous audit logging log at the beginning of each test
-    remote_executor.execute("rm -rf /home/sync_gateway/logs/audit_log*")
     yield sg_client, admin_client, sg_url, sg_admin_url
 
     remote_executor.execute("rm -rf /home/sync_gateway/logs/audit_log*")
@@ -100,6 +100,8 @@ def test_default_audit_settings(params_from_base_test_setup, audit_logging_fixtu
     '''
     cluster_config = params_from_base_test_setup["cluster_config"]
     sg_client, admin_client, sg_url, sg_admin_url = audit_logging_fixture
+    # Delete the previous audit logging log at the beginning of each test
+    remote_executor.execute("rm -rf /home/sync_gateway/logs/audit_log*")
     event_user = "user" + random_suffix + use_settings
     event_role = "role" + random_suffix + use_settings
     tested_ids = {"53281": EXPECTED_IN_LOGS,  # public API User authetication failed
