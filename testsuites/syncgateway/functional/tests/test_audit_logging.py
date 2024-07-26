f8bc81459a01f45d1a8f7af6256a9a6952f50b13
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-26 10:25:06 +0100

    Refactor

--- testsuites/syncgateway/functional/tests/test_audit_logging.py
+++ testsuites/syncgateway/functional/tests/test_audit_logging.py
@@ -20,29 +20,50 @@ from keywords import couchbaseserver, document
 
 EXPECTED_IN_LOGS = True
 NOT_EXPECTED_IN_THE_LOGS = False
-DEFAULT_EVENTS_SETTINGS = {"53281": EXPECTED_IN_LOGS,  # public API User authetication failed
-                           "53280": EXPECTED_IN_LOGS,  # public API User authetication
-                           "54100": EXPECTED_IN_LOGS,  # Create user
-                           "54101": EXPECTED_IN_LOGS,  # Read user
-                           "54102": EXPECTED_IN_LOGS,  # Update user
-                           "54103": EXPECTED_IN_LOGS,  # Delete user
-                           "54110": EXPECTED_IN_LOGS,  # Create role
-                           "54111": EXPECTED_IN_LOGS,  # Read role
-                           "54112": EXPECTED_IN_LOGS,  # Update role
-                           "55000": NOT_EXPECTED_IN_THE_LOGS,  # Create document
-                           "55001": NOT_EXPECTED_IN_THE_LOGS,  # Read document
-                           "55002": NOT_EXPECTED_IN_THE_LOGS,  # Update document
-                           "55003": NOT_EXPECTED_IN_THE_LOGS,  # Delete document
+EVENTS = {"public_api_auth_failed": "53281",
+          "public_api_auth_success": "53280",
+          "create_user": "54100",
+          "read_user": "54101",
+          "update_user": "54102",
+          "delete_user": "54103",
+          "create_role": "54110",
+          "read_role": "54111",
+          "update_role": "54112",
+          "create_document": "55000",
+          "read_document": "55001",
+          "update_document": "55002",
+          "delete_document": "55003",
+          "audit_enabled": "53248",
+          "audit_configuration_changed": "53250",
+          "sgw_startup": "53260",
+          "public_api_request": "53270",
+          "read_all_databases": "54003",
+          "admin_http_api_request": "53271"
+          }
+
+DEFAULT_EVENTS_SETTINGS = {EVENTS["public_api_auth_failed"]: EXPECTED_IN_LOGS,
+                           EVENTS["public_api_auth_success"]: EXPECTED_IN_LOGS,
+                           EVENTS["create_user"]: EXPECTED_IN_LOGS,
+                           EVENTS["read_user"]: EXPECTED_IN_LOGS,
+                           EVENTS["update_user"]: EXPECTED_IN_LOGS,
+                           EVENTS["delete_user"]: EXPECTED_IN_LOGS,
+                           EVENTS["create_role"]: EXPECTED_IN_LOGS,
+                           EVENTS["read_role"]: EXPECTED_IN_LOGS,
+                           EVENTS["update_role"]: EXPECTED_IN_LOGS,
+                           EVENTS["create_document"]: NOT_EXPECTED_IN_THE_LOGS,
+                           EVENTS["read_document"]: NOT_EXPECTED_IN_THE_LOGS,
+                           EVENTS["update_document"]: NOT_EXPECTED_IN_THE_LOGS,
+                           EVENTS["delete_document"]: NOT_EXPECTED_IN_THE_LOGS,
                            }
 
 
 # The global events as defined in resources/sync_gateway_configs_cpc/audit_logging_cc.json or unfirtable settings
-GLOBAL_EVENTS_SETTINGS = {"53248": EXPECTED_IN_LOGS,  # Auditing enabled
-                          "53250": EXPECTED_IN_LOGS,  # Auditing configuration changed
-                          "53260": EXPECTED_IN_LOGS,  # Sync Gateway startup
-                          "53270": EXPECTED_IN_LOGS,  # Public HTTP API request
-                          "54003": NOT_EXPECTED_IN_THE_LOGS,  # Read all databases
-                          "53271": NOT_EXPECTED_IN_THE_LOGS   # Admin HTTP API request
+GLOBAL_EVENTS_SETTINGS = {EVENTS["audit_enabled"]: EXPECTED_IN_LOGS,
+                          EVENTS["audit_configuration_changed"]: EXPECTED_IN_LOGS,
+                          EVENTS["sgw_startup"]: EXPECTED_IN_LOGS,
+                          EVENTS["public_api_request"]: EXPECTED_IN_LOGS,
+                          EVENTS["read_all_databases"]: NOT_EXPECTED_IN_THE_LOGS,
+                          EVENTS["admin_http_api_request"]: NOT_EXPECTED_IN_THE_LOGS
                           }
 
 random_suffix = str(uuid.uuid4())[:8]
@@ -136,19 +157,19 @@ def test_audit_settings(params_from_base_test_setup, audit_logging_fixture, sett
 
     print("The audit events configuration: " + str(admin_client.get_audit_logging_conf(sg_db)))
     # 1. Trigger the tested events
-    trigger_event_53281(sg_client=sg_client, sg_url=sg_url)
-    trigger_event_53280(sg_client=sg_client, sg_url=sg_url, auth=(username, password))
-    trigger_event_54100(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
-    trigger_event_54101(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
-    trigger_event_54102(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
-    trigger_event_54103(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
-    trigger_event_54110(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
-    trigger_event_54111(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
-    trigger_event_54112(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
-    trigger_event_55000(sg_client, sg_url, doc_id_prefix, auth=(username, password))
-    trigger_event_55001(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
-    trigger_event_55002(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
-    trigger_event_55003(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
+    trigger_user_auth_failed(sg_client=sg_client, sg_url=sg_url)
+    trigger_user_auth_succeeded(sg_client=sg_client, sg_url=sg_url, auth=(username, password))
+    trigger_create_user(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
+    trigger_get_user(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
+    trigger_update_user(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
+    trigger_update_delete(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
+    trigger_create_role(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
+    trigger_read_role(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
+    trigger_update_role(sg_client=sg_client, sg_admin_url=sg_admin_url, role=event_role)
+    trigger_create_document(sg_client, sg_url, doc_id_prefix, auth=(username, password))
+    trigger_read_document(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
+    trigger_update_document(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
+    trigger_delete_document(sg_client, sg_url, doc_id_prefix + "_0", auth=(username, password))
 
     # 2. Check that the events are are recorded/not recorded in the audit_log file
     audit_log_folder = get_audit_log_folder(cluster_config)
@@ -176,7 +197,7 @@ def test_audit_log_rotation(params_from_base_test_setup, audit_logging_fixture):
 
     # 1. Triggering event 53280 multiple times to increaes the audit log size to more than 1MB
     for i in range(0, 3000):
-        trigger_event_53280(sg_client=sg_client, sg_url=sg_url, auth=(username, password))
+        trigger_user_auth_succeeded(sg_client=sg_client, sg_url=sg_url, auth=(username, password))
     # 2. Looking at the content of the logs directory and expecting it to contain an archive
     _, stdout, _ = remote_executor.execute("ls /home/sync_gateway/logs | grep sg_audit.*.gz")
     if len(stdout) > 0:
@@ -197,8 +218,8 @@ def test_events_logs_per_db(params_from_base_test_setup, audit_logging_fixture):
     db2_pattern = re.compile('\"db\":\"{}\".*\"id\":54100'.format(sg2_db))
 
     # 1. Triggering 2 events in 2 different dbs
-    trigger_event_54110(sg_client, sg_admin_url, role="db1_role", db=sg_db)
-    trigger_event_54100(sg_client=sg_client, sg_admin_url=sg_admin_url, user="db2_user", db=sg2_db)
+    trigger_create_role(sg_client, sg_admin_url, role="db1_role", db=sg_db)
+    trigger_create_user(sg_client=sg_client, sg_admin_url=sg_admin_url, user="db2_user", db=sg2_db)
 
     # 2. Checking that the right event was logged against the right db
     audit_log_folder = get_audit_log_folder(cluster_config)
@@ -222,10 +243,10 @@ def test_global_events(params_from_base_test_setup, audit_logging_fixture):
     tested_ids = GLOBAL_EVENTS_SETTINGS
 
     # 1. Trigging global events
-    trigger_event_53271(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
-    trigger_event_53270(sg_client, sg_url, auth=(username, password), db=sg_db)
-    trigger_event_53250(admin_client, db=sg_db)
-    trigger_event_54003(admin_client)
+    trigger_admin_http_api_request(sg_client=sg_client, sg_admin_url=sg_admin_url, user=event_user)
+    trigger_public_api_request(sg_client, sg_url, auth=(username, password), db=sg_db)
+    trigger_audit_configuration_change(admin_client, db=sg_db)
+    trigger_get_all_databases(admin_client)
 
     # 2. Check that the events are are recorded/not recorded in the audit_log file
     audit_log_folder = get_audit_log_folder(cluster_config)
@@ -257,98 +278,80 @@ def get_audit_log_folder(cluster_config):
         return "{}/sg1".format(temp_log_path)
 
 
-# public API User authetication failed
-def trigger_event_53281(sg_client, sg_url, db=sg_db):
+def trigger_user_auth_failed(sg_client, sg_url, db=sg_db):
     try:
         sg_client.get_all_docs(url=sg_url, db=db, auth=("fake_user", "fake_password"))
     except (Exception):
         pass
 
 
-# public API User authetication
-def trigger_event_53280(sg_client, sg_url, auth, db=sg_db):
+def trigger_user_auth_succeeded(sg_client, sg_url, auth, db=sg_db):
     sg_client.get_all_docs(url=sg_url, db=db, auth=auth)
 
 
-# Create user
-def trigger_event_54100(sg_client, sg_admin_url, user, db=sg_db):
+def trigger_create_user(sg_client, sg_admin_url, user, db=sg_db):
     sg_client.create_user(url=sg_admin_url, db=db, name=user, password=password, channels=channels, auth=auth)
 
 
-# Read user
-def trigger_event_54101(sg_client, sg_admin_url, user, db=sg_db):
+def trigger_get_user(sg_client, sg_admin_url, user, db=sg_db):
     sg_client.get_user(url=sg_admin_url, db=db, name=user, auth=auth)
 
 
-# Update user
-def trigger_event_54102(sg_client, sg_admin_url, user, db=sg_db):
+def trigger_update_user(sg_client, sg_admin_url, user, db=sg_db):
     sg_client.update_user(url=sg_admin_url, db=db, name=user, password="password1", auth=auth)
 
 
-# Delete user
-def trigger_event_54103(sg_client, sg_admin_url, user, db=sg_db):
+def trigger_update_delete(sg_client, sg_admin_url, user, db=sg_db):
     sg_client.delete_user(url=sg_admin_url, db=db, name=user, auth=auth)
 
 
-# Create role
-def trigger_event_54110(sg_client, sg_admin_url, role, db=sg_db):
+def trigger_create_role(sg_client, sg_admin_url, role, db=sg_db):
     sg_client.create_role(url=sg_admin_url, db=db, name=role)
 
 
-# Read role
-def trigger_event_54111(sg_client, sg_admin_url, role, db=sg_db):
+def trigger_read_role(sg_client, sg_admin_url, role, db=sg_db):
     sg_client.get_role(url=sg_admin_url, db=db, name=role)
 
 
-# Update role
-def trigger_event_54112(sg_client, sg_admin_url, role, db=sg_db):
+def trigger_update_role(sg_client, sg_admin_url, role, db=sg_db):
     sg_client.update_role(url=sg_admin_url, db=db, name=role)
 
 
-# Create session
-def trigger_event_53282(sg_client, sg_admin_url, db=sg_db):
+def trigger_create_session(sg_client, sg_admin_url, db=sg_db):
     sg_client.create_session(url=sg_admin_url, db=db, name=username, auth=auth)
 
 
-# Admin HTTP API request
-def trigger_event_53271(sg_client, sg_admin_url, user, db=sg_db):
+def trigger_admin_http_api_request(sg_client, sg_admin_url, user, db=sg_db):
     sg_client.create_user(url=sg_admin_url, db=db, name=user, password=password, channels=channels, auth=auth)
 
 
-# Public HTTP API request
-def trigger_event_53270(sg_client, sg_url, auth, db=sg_db):
+def trigger_public_api_request(sg_client, sg_url, auth, db=sg_db):
     sg_client.get_all_docs(url=sg_url, db=db, auth=auth)
 
 
-# Audit configuration changed
-def trigger_event_53250(admin_client, db=sg_db):
+def trigger_audit_configuration_change(admin_client, db=sg_db):
     eventsConfiguration = admin_client.get_audit_logging_conf(db)
     audit_config = {"enabled": True, "events": {"53280": not eventsConfiguration["events"]["53280"]}}
     admin_client.replace_audit_config(db, audit_config)
 
 
-# Read all databases
-def trigger_event_54003(admin_client):
+def trigger_get_all_databases(admin_client):
     admin_client.get_dbs()
 
 
-# Create document
-def trigger_event_55000(sg_client, sg_url, doc_id_prefix, auth):
+def trigger_create_document(sg_client, sg_url, doc_id_prefix, auth):
     sgdoc_bodies = document.create_docs(doc_id_prefix=doc_id_prefix, number=3, channels=channels)
     sg_client.add_bulk_docs(url=sg_url, db=sg_db, docs=sgdoc_bodies, auth=auth)
 
 
-# Read document
-def trigger_event_55001(sg_client, sg_url, doc_id, auth):
+def trigger_read_document(sg_client, sg_url, doc_id, auth):
     sg_client.get_doc(url=sg_url, db=sg_db, doc_id=doc_id, auth=auth)
 
 
-# Update document
-def trigger_event_55002(sg_client, sg_url, doc_id, auth):
+def trigger_update_document(sg_client, sg_url, doc_id, auth):
     sg_client.update_doc(url=sg_url, db=sg_db, doc_id=doc_id, auth=auth)
 
 
-# Delete document
-def trigger_event_55003(sg_client, sg_url, doc_id, auth):
+def trigger_delete_document(sg_client, sg_url, doc_id, auth):
     doc = sg_client.get_doc(url=sg_url, db=sg_db, doc_id=doc_id, auth=auth)
     sg_client.delete_doc(url=sg_url, db=sg_db, doc_id=doc_id, rev=doc['_rev'], auth=auth)
