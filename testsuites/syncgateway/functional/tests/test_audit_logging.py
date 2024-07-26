9ee5961a44ab4b4e793babaf8c50c3ca382b8fad
Gilad Kalchheim <gilkal1979@gmail.com>
2024-07-26 08:55:37 +0100

    update the config

--- testsuites/syncgateway/functional/tests/test_audit_logging.py
+++ testsuites/syncgateway/functional/tests/test_audit_logging.py
@@ -129,12 +129,12 @@ def test_audit_settings(params_from_base_test_setup, audit_logging_fixture, sett
     doc_id_prefix = "audit_logging_doc" + random_suffix
     tested_ids = DEFAULT_EVENTS_SETTINGS
     # randomise a selected filterable events in case we are not testing the default settings
+    audit_config = {"enabled": True}
     if settings_config != "default":
         for event in tested_ids.keys():
             tested_ids[event] = settings_config
-
-        audit_config = {"enabled": True, "events": tested_ids}
-        admin_client.replace_audit_config(sg_db, audit_config)
+        audit_config["events"] = tested_ids
+    admin_client.update_audit_config(sg_db, audit_config)
 
     print("The audit events configuration: " + str(admin_client.get_audit_logging_conf(sg_db)))
     # 1. Trigger the tested events
