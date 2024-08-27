2a0c782d31e14f2d5acfba0319a79badbaba8b3c
Gilad Kalchheim <gilkal1979@gmail.com>
2024-08-27 11:37:57 +0100

    Change to SGW url as the target

--- testsuites/CBLTester/topology_specific_tests/sync_gateway_proxy_enabled/test_replication.py
+++ testsuites/CBLTester/topology_specific_tests/sync_gateway_proxy_enabled/test_replication.py
@@ -150,15 +150,16 @@ def test_proxy_authentication(params_from_base_test_setup):
     print("**************************************toplogy=" + str(topology))
     sg_db = "db"
     channels = ["ABC"]
-    sgw_user = "proxy-auth-test1"
-    sgw_password = "password"
+    # sgw_user = "proxy-auth-test1"
+    # sgw_password = "password"
     # data = {"bucket": "data-bucket-1",  "num_index_replicas": 0}
 
     # c_cluster = cluster.Cluster(config=cluster_config)
     # admin_client = Admin(c_cluster.sync_gateways[0])
     # c.reset(sg_config_path=sg_config)
     # admin_client.create_db(sg_db, data)
-
+    sgw_user = NGINX_SGW_USER_NAME
+    sgw_password = NGINX_SGW_PASSWORD
     proxy_username = NGINX_SGW_USER_NAME
     proxy_password = NGINX_SGW_PASSWORD
 
