c4533ea046bfbfa0c6a4a10fa56239c4186dfcb8
Gilad Kalchheim <gilkal1979@gmail.com>
2024-08-29 11:05:01 +0100

    Enable vector search for all the platforms except iOS

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -112,7 +112,7 @@ def vector_search_test_fixture(params_from_base_test_setup):
     yield base_url, scope, dbv_col_name, st_col_name, iv_col_name, aw_col_name, cb_server, vsTestDatabase, sg_client, sg_username
     db.deleteDB(vsTestDatabase)
 
-
+@pytest.mark.skipif(pytest.config.getoption("--liteserv-platform").startswith("ios"))
 def test_vector_search_index_correctness(vector_search_test_fixture):
     '''
     @summary: Modifying and pulling documents leads to correct vector embeddings
