3093d837defe22065a6207fb671c28de23bfb24a
barkha06 <barkha.goyal@couchbase.com>
2024-10-21 17:51:00 +0530

    commented flush mem

--- testsuites/CBLTester/p2p_topology_specific_tests/conftest.py
+++ testsuites/CBLTester/p2p_topology_specific_tests/conftest.py
@@ -417,7 +417,7 @@ def params_from_base_suite_setup(request):
     # Flush all the memory contents on the server app
     log_info("Flushing server memory")
     utils_obj = Utils(base_url)
-    utils_obj.flushMemory()
+    # utils_obj.flushMemory()
     log_info("Stopping the test server")
     if not use_local_testserver:
         log_info("Stopping the test server per suite")
