f532c8241f2ed66633f114e9a46bf9ae6e5af5cf
DragosPetruTaraban <dragos.petru@couchbase.com>
2024-11-07 16:06:12 +0000

    Update install

--- setup.sh
+++ setup.sh
@@ -9,18 +9,20 @@
 alias python3=/opt/homebrew/bin/python3.8
 py3version=$(python3 -c 'import sys; print("{}.{}.{}".format(sys.version_info.major, sys.version_info.minor, sys.version_info.micro))')
 
-if [[ $py3version == 3.8.* ]]; then
-    printf "Using Python3 version: %s\n" $py3version
-    PYTHON=python3
-    PIP=pip3.8
-elif [[ $py3version == 3.7.* ]]; then
-    printf "Using Python3 version: %s\n" $py3version
-    PYTHON=python3
-    PIP=pip3.7
-else
-    echo "Exiting. Make sure Python version is 3.8 or 3.7"
-    return 1
-fi
+# if [[ $py3version == 3.8.* ]]; then
+#     printf "Using Python3 version: %s\n" $py3version
+#     PYTHON=python3
+#     PIP=pip3.8
+# elif [[ $py3version == 3.7.* ]]; then
+#     printf "Using Python3 version: %s\n" $py3version
+#     PYTHON=python3
+#     PIP=pip3.7
+# else
+#     echo "Exiting. Make sure Python version is 3.8 or 3.7"
+#     return 1
+# fi
+PYTHON=python3.8
+PIP=pip3.8
 
 $PYTHON -m virtualenv --version
 if [ $? -ne 0 ]; then
