c3d58dd6be2b5dd359f38b917c1921955093bd59
Sanjivani Patra <sanjivani.patra@couchbase.com>
2025-01-27 13:06:29 +0530

    Modified command to install couchbase

--- setup.sh
+++ setup.sh
@@ -46,6 +46,6 @@ export PYTHONPATH=$PYTHONPATH:$currentdir/
 export ANSIBLE_CONFIG=$currentdir/ansible.cfg
 
 pip install --upgrade pip==20.1.1
-pip install couchbase==3.2.7
+python3 -m pip install couchbase==3.2.7 --no-use-pep517
 pip install importlib-metadata==4.3.0
 pip install setuptools==68.0.0
