c732ee1839eb192e978d067896c119175c7e63aa
VIPUL BHARDWAJ <vipulbhardwaj1011@gmail.com>
2025-04-17 15:11:32 +0530

    removed 4 extra coll checker

--- testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
+++ testsuites/CBLTester/CBL_Functional_tests/APITests/test_vectorSearch.py
@@ -151,14 +151,14 @@ def test_vector_search_index_correctness(vector_search_test_fixture):
     base_url, scope, dbv_col_name, st_col_name, iv_col_name, aw_col_name, cb_server, vsTestDatabase, sg_client, sg_username = vector_search_test_fixture
     db = Database(base_url)
 
-    # Check that all 4 collections on CBL exist
-    cbl_collections = db.collectionsInScope(vsTestDatabase, scope)
-    # TODO check if _default counts towards this
-    assert len(cbl_collections) == 5, "wrong number of collections returned"
-    assert dbv_col_name in cbl_collections, "no CBL collection found for doc body vectors"
-    assert st_col_name in cbl_collections, "no CBL collection found for search terms"
-    assert iv_col_name in cbl_collections, "no CBL collection found for index vectors"
-    assert aw_col_name in cbl_collections, "no CBL collection found for auxiliary words"
+    # # Check that all 4 collections on CBL exist
+    # cbl_collections = db.collectionsInScope(vsTestDatabase, scope)
+    # # TODO check if _default counts towards this
+    # assert len(cbl_collections) == 5, "wrong number of collections returned"
+    # assert dbv_col_name in cbl_collections, "no CBL collection found for doc body vectors"
+    # assert st_col_name in cbl_collections, "no CBL collection found for search terms"
+    # assert iv_col_name in cbl_collections, "no CBL collection found for index vectors"
+    # assert aw_col_name in cbl_collections, "no CBL collection found for auxiliary words"
 
     # Leaving this code commented for future reference. If we end up not using replication, it can be deleted
     # replicate docs to server via sgw
