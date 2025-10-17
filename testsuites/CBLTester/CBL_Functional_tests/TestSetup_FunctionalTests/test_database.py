d432b3539b57e514611c699c1f52fb3c0b683097
Vipul Bhardwaj <vipul.bhardwaj@couchbase.com>
2025-10-17 20:58:48 +0530

    didn't work, passing a flag in config instead

--- testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_database.py
+++ testsuites/CBLTester/CBL_Functional_tests/TestSetup_FunctionalTests/test_database.py
@@ -569,7 +569,6 @@ def test_db_delete_on_active_replicators(params_from_base_test_setup):
     sg_db = "db"
     sg_admin_url = params_from_base_test_setup["sg_admin_url"]
     sg_blip_url = params_from_base_test_setup["target_url"]
-    params_from_base_test_setup["need_sgw_admin_auth"] = False
     need_sgw_admin_auth = params_from_base_test_setup["need_sgw_admin_auth"]
 
     if liteserv_version < "2.8.0":
